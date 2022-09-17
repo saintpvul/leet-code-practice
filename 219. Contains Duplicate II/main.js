cce81834da051a72377cb384c557b0196c98d917
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-17 23:05:40 +0600

    feat: complete 219 ex

--- 219. Contains Duplicate II/main.js
+++ 219. Contains Duplicate II/main.js
@@ -5,3 +5,27 @@ Given an integer array nums and an integer k, return true if there are two disti
 */
 
 //solution
+
+function containsNearbyDuplicate(nums, k) {
+  let length = nums.length;
+  if (!length) return false;
+  if (k > length) k = length - 1;
+  const object = {};
+  for (let i = 0; i <= k; i++) {
+    if (object[nums[i]]) return true;
+    object[nums[i]] = 1;
+  }
+  if (k !== length - 1) {
+    delete object[nums[0]];
+    let j = 1;
+    for (let i = k + 1; i < length; i++) {
+      if (object[nums[i]]) return true;
+      else {
+        object[nums[i]] = 1;
+        delete object[nums[j]];
+        j = j + 1;
+      }
+    }
+  }
+  return false;
+}
