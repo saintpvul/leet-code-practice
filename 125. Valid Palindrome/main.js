cb9b58d9ab1d45832443dc64c0429677e621c4e0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-18 00:31:10 +0600

    feat: complete 125 ex

--- 125. Valid Palindrome/main.js
+++ 125. Valid Palindrome/main.js
@@ -7,3 +7,31 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 */
 
 //solution
+
+let str = " A man, a plan, a canalino: Panama   ";
+let palStr = "race car";
+
+var isPalindrome = function (s) {
+  let notPal = s
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "");
+  console.log(notPal);
+  let pal = s
+    .trim()
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .split("")
+    .reverse()
+    .join("");
+  console.log(pal);
+  if (pal === notPal) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isPalindrome(str));
+
+console.log(isPalindrome(palStr));
