73e0fb0b0c26076f998bbe47aeff20fabf7ff236
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-19 20:31:53 +0600

    feat: re-complete 344ex

--- 344. Reverse String/main.js
+++ 344. Reverse String/main.js
@@ -9,6 +9,18 @@ You must do this by modifying the input array in-place with O(1) extra memory.
 
 // solution
 
+/*
+ * @param {character[]} s
+ */
+//! @return {void} Do not return anything, modify s in-place instead.
+
 var reverseString = function (s) {
-  return s.reverse();
+  let start = 0;
+  let end = s.length - 1;
+
+  while (end > start) {
+    let curr = s[end];
+    s[end--] = s[start];
+    s[start++] = curr;
+  }
 };
