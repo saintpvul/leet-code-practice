c86c81da8e8d00ebb4e786e9fd527a4878235a91
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-19 19:58:18 +0600

    feat: complete 680ex

--- 680. Valid Palindrome II/main.js
+++ 680. Valid Palindrome II/main.js
@@ -5,3 +5,27 @@ Given a string s, return true if the s can be palindrome after deleting at most 
 */
 
 // solution
+
+function checkPal(s) {
+  let i = 0;
+  let j = s.length - 1;
+  let count = 0;
+
+  while (i < j) {
+    if (s[i] !== s[j]) {
+      return isPal(s, i + 1, j) || isPal(s, i, j - 1);
+    }
+    i++;
+    j--;
+  }
+  return true;
+}
+
+function isPal(str, i, j) {
+  while (i < j) {
+    if (str[i] !== str[j]) return false;
+    i++;
+    j--;
+  }
+  return true;
+}
