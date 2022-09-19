e70c9352ef3839927e8eb6c596fef5b489642c83
saintpvul <103897555+saintpvul@users.noreply.github.com>
2022-09-19 22:25:55 +0600

    feat: complete 345ex

--- 345. Reverse Vowels of a String/main.js
+++ 345. Reverse Vowels of a String/main.js
@@ -9,3 +9,33 @@ The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
 */
 
 //solition
+
+let str = "hello";
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function reverseVowels(s) {
+  let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+  let foundedVowels = [];
+  for (let i = 0; i < s.length; i++) {
+    if (vowels.indexOf(s[i]) > -1) {
+      foundedVowels.push(s[i]);
+    }
+  }
+  console.log(foundedVowels);
+  let splittedArr = s.split("");
+  let lastIndex = foundedVowels.length - 1;
+  for (let i = 0; i < splittedArr.length; i++) {
+    if (vowels.indexOf(splittedArr[i]) > -1) {
+      splittedArr[i] = foundedVowels[lastIndex];
+      lastIndex--;
+    }
+  }
+  if (s !== splittedArr.join("")) {
+    return splittedArr.join("");
+  }
+  if (s !== null) {
+    return s;
+  }
+}
