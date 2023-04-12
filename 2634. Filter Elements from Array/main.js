c7e8e86056558df94f595a55c54665b0ace06b3f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-13 00:16:02 +0600

    feat: complete problem 2634. filter elements from array

--- 2634. Filter Elements from Array/main.js
+++ 2634. Filter Elements from Array/main.js
@@ -9,3 +9,13 @@ Please solve it without the built-in Array.filter method.
 */
 
 // solution
+
+var filter = function (arr, fn) {
+    let resultArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            resultArray.push(arr[i]);
+        }
+    }
+    return resultArray;
+};
