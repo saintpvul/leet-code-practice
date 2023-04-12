bb679b9b4a11d2c94625a1a0b5b6cd06d7218344
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-12 23:21:56 +0600

    feat: start problem 2620. counter

---
+++ 2620. Counter/main.js
@@ -0,0 +1,9 @@
+/*
+
+Write a function that accepts an integer n and returns a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+ 
+
+*/
+
+// solution
