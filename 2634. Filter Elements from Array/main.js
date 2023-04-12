8c4587a8e4fc185646b6e66a7e14b25c68c2de1c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-13 00:14:28 +0600

    feat: start problem 2634. filter elements from array

---
+++ 2634. Filter Elements from Array/main.js
@@ -0,0 +1,11 @@
+/*
+
+Write a function that takes an input array of integers arr and a filtering function fn and returns a new array with a fewer or equal number of elements.
+
+The returned array should only contain elements where fn(arr[i], i) evaluated to a truthy value.
+
+Please solve it without the built-in Array.filter method.
+
+*/
+
+// solution
