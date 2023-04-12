23cf857802c9e7c2f5dc03815913e9f912b1b177
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-12 23:42:55 +0600

    feat: complete problem 2621.sleep

--- 2621. Sleep/main.js
+++ 2621. Sleep/main.js
@@ -1,7 +1,13 @@
 /*
 
-
+Write an asyncronous function that accepts a positive integer millis and sleeps for that many milliseconds. It can resolve any value.
 
 */
 
 // solution
+
+async function sleep(millis) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(millis), millis);
+    });
+}
