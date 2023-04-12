aca9ed85a6d8673a5ba64210934bcec582f6dc56
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-13 00:12:54 +0600

    feat: start problem 2635. apply transform over each element in array.

---
+++ 2635. Apply Transform Over Each Element in Array/main.js
@@ -0,0 +1,11 @@
+/*
+
+Write a function that takes an input array of integers arr and mapping function fn and returns a new array.
+
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+
+*/
+
+// solution
