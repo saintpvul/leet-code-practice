b8839e2452fc8fa53ec0e645b777e270aaeeb172
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-14 21:08:16 +0600

    feat: complete 15. 3sum problem

--- 15. 3Sum/main.js
+++ 15. 3Sum/main.js
@@ -7,3 +7,41 @@ Notice that the solution set must not contain duplicate triplets.
 */
 
 // solution
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    if (nums.reduce((s, v) => (s += v), 0) === 0) return [nums];
+    let triplets = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (nums[i] + nums[j] + nums[k] === 0) {
+                    triplets.push(
+                        [nums[i], nums[j], nums[k]].sort((a, b) => a - b)
+                    );
+                    break;
+                }
+            }
+        }
+    }
+    let uniq1 = [],
+        uniq2 = [];
+
+    let res = [];
+    for (let i = 0; i < triplets.length - 1; i++) {
+        uniq1 = triplets[i].slice();
+        uniq2 = triplets[i + 1].slice();
+        for (let j = 0; j < triplets[i].length; j++) {
+            if (uniq1[j] === uniq2[j]) {
+                continue;
+            } else {
+                res.push(uniq2);
+                break;
+            }
+        }
+    }
+    return res;
+};
