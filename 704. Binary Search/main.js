efa2270c69e116fa780d52465a5f03d84b478cac
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-15 02:24:45 +0600

    feat: complete 704. binary search

--- 704. Binary Search/main.js
+++ 704. Binary Search/main.js
@@ -7,3 +7,22 @@ You must write an algorithm with O(log n) runtime complexity.
 */
 
 // solution
+
+var search = function (nums, target) {
+    let left = 0,
+        right = nums.length - 1;
+
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+};
