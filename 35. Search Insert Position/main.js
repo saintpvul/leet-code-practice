956de902c02ad58fcf2048655789f3da94444994
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-15 03:07:48 +0600

    feat: complete 35. search insert position

--- 35. Search Insert Position/main.js
+++ 35. Search Insert Position/main.js
@@ -7,3 +7,31 @@ You must write an algorithm with O(log n) runtime complexity.
 */
 
 // solution
+
+var searchInsert = function (nums, target) {
+    let min = 0;
+    let max = nums.length - 1;
+
+    let targetIdx;
+
+    if (target > nums[max]) return max + 1;
+
+    while (min <= max) {
+        let mid = Math.floor((min + max) / 2);
+        let midValue = nums[mid];
+        if (midValue === target) {
+            targetIdx = mid;
+            break;
+        } else if (midValue < target) {
+            min = mid + 1;
+            targetIdx = mid;
+        } else if (target > nums[min] && target < nums[max]) {
+            targetIdx = min + 1;
+            min++;
+        } else {
+            max = mid - 1;
+            targetIdx = mid;
+        }
+    }
+    return targetIdx;
+};
