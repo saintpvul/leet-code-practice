4f512e569900562a89b8f19fc0394f313686dec2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-14 23:13:34 +0600

    feat: complete 16. 3sum closest problem

--- 16. 3Sum Closest/main.js
+++ 16. 3Sum Closest/main.js
@@ -9,3 +9,24 @@ You may assume that each input would have exactly one solution.
 */
 
 // solution
+
+var threeSumClosest = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let closestSum = nums[0] + nums[1] + nums[2];
+    for (let i = 0; i < nums.length - 2; i++) {
+        let j = i + 1,
+            k = nums.length - 1;
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+                closestSum = sum;
+            }
+            if (sum > target) {
+                k--;
+            } else {
+                j++;
+            }
+        }
+    }
+    return closestSum;
+};
