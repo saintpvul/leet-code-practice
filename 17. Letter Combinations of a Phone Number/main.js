a771bb1b5146b294dbeaaf4bc9695e5f9f17ce8a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-15 00:12:33 +0600

    feat: complete 17. letter combination of a phone number problem

--- 17. Letter Combinations of a Phone Number/main.js
+++ 17. Letter Combinations of a Phone Number/main.js
@@ -7,3 +7,36 @@ A mapping of digits to letters (just like on the telephone buttons) is given bel
 */
 
 // solution
+
+var letterCombinations = function (digits) {
+    if (!digits) return [];
+    let keyboard = {
+        2: "abc",
+        3: "def",
+        4: "ghi",
+        5: "jkl",
+        6: "mno",
+        7: "pqrs",
+        8: "tuv",
+        9: "wxyz",
+    };
+    if (digits.length === 1) return keyboard[digits].split("");
+
+    let output = [];
+
+    for (let i = 0; i < digits.length; i++) {
+        let letters = keyboard[digits[i]];
+        if (output.length === 0) {
+            output = letters.split("");
+        } else {
+            let newOutput = [];
+            for (let j = 0; j < output.length; j++) {
+                for (let k = 0; k < letters.length; k++) {
+                    newOutput.push(output[j] + letters[k]);
+                }
+            }
+            output = newOutput;
+        }
+    }
+    return output;
+};
