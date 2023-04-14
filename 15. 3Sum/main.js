0870494ba0b5e077f84dda54238d13e80b34d846
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-14 22:11:50 +0600

    feat: complete 15. 3sum problem

--- 15. 3Sum/main.js
+++ 15. 3Sum/main.js
@@ -12,34 +12,65 @@ Notice that the solution set must not contain duplicate triplets.
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function (nums) {
-    if (nums.reduce((s, v) => (s += v), 0) === 0) return [nums];
-    let triplets = [];
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                if (nums[i] + nums[j] + nums[k] === 0) {
-                    triplets.push(
-                        [nums[i], nums[j], nums[k]].sort((a, b) => a - b)
-                    );
-                    break;
-                }
-            }
-        }
-    }
-    let uniq1 = [],
-        uniq2 = [];
 
+// passes only basic tests
+
+// var threeSum = function (nums) {
+//     if (nums.reduce((s, v) => (s += v), 0) === 0) return [nums];
+//     let triplets = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             for (let k = j + 1; k < nums.length; k++) {
+//                 if (nums[i] + nums[j] + nums[k] === 0) {
+//                     triplets.push(
+//                         [nums[i], nums[j], nums[k]].sort((a, b) => a - b)
+//                     );
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     let uniq1 = [],
+//         uniq2 = [];
+
+//     let res = [];
+//     for (let i = 0; i < triplets.length - 1; i++) {
+//         uniq1 = triplets[i].slice();
+//         uniq2 = triplets[i + 1].slice();
+//         for (let j = 0; j < triplets[i].length; j++) {
+//             if (uniq1[j] === uniq2[j]) {
+//                 continue;
+//             } else {
+//                 res.push(uniq2);
+//                 break;
+//             }
+//         }
+//     }
+//     return res;
+// };
+
+// modified solution
+
+var threeSum = function (nums) {
+    nums.sort((a, b) => a - b);
     let res = [];
-    for (let i = 0; i < triplets.length - 1; i++) {
-        uniq1 = triplets[i].slice();
-        uniq2 = triplets[i + 1].slice();
-        for (let j = 0; j < triplets[i].length; j++) {
-            if (uniq1[j] === uniq2[j]) {
-                continue;
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] > 0) break;
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        let j = i + 1,
+            k = nums.length - 1;
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            if (sum === 0) {
+                res.push([nums[i], nums[j], nums[k]]);
+                while (j < k && nums[j] === nums[j + 1]) j++;
+                while (j < k && nums[k] === nums[k - 1]) k--;
+                j++;
+                k--;
+            } else if (sum < 0) {
+                j++;
             } else {
-                res.push(uniq2);
-                break;
+                k--;
             }
         }
     }
