d25134df4e925da51ca9c0d3cfe2f8e7667fffa3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-17 00:03:06 +0600

    feat: complete 189. rotate array

--- 189. Rotate Array/main.js
+++ 189. Rotate Array/main.js
@@ -5,3 +5,8 @@ Given an integer array nums, rotate the array to the right by k steps, where k i
 */
 
 // solution
+
+var rotate = function (nums, k) {
+    k = k % nums.length;
+    nums.unshift(...nums.splice(-k));
+};
