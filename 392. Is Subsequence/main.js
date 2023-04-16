1331d1e6626bbaba7276c39815fbe9c909faaf5f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-16 23:39:36 +0600

    feat: complete 392. is subsequence

--- 392. Is Subsequence/main.js
+++ 392. Is Subsequence/main.js
@@ -7,3 +7,17 @@ A subsequence of a string is a new string that is formed from the original strin
 */
 
 // solution
+
+var isSubsequence = function (s, t) {
+    let i = 0,
+        j = 0;
+
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j]) {
+            i++;
+        }
+        j++;
+    }
+
+    return i === s.length;
+};
