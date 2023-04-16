bf19cc1b9d5d67caacb01b0e5d8ead014bc87e9c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-16 23:25:55 +0600

    feat: complete 205. isomorphic strings

--- 205. Isomorphic Strings/main.js
+++ 205. Isomorphic Strings/main.js
@@ -1,7 +1,38 @@
 /*
 
+Given two strings s and t, determine if they are isomorphic.
 
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
 
 */
 
 // solution
+
+var isIsomorphic = function (s, t) {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    let sMap = {};
+    let tMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        let charS = s[i];
+        let charT = t[i];
+
+        if (sMap[charS]) {
+            if (sMap[charS] !== charT) {
+                return false;
+            }
+        } else {
+            if (tMap[charT]) {
+                return false;
+            }
+            sMap[charS] = charT;
+            tMap[charT] = charS;
+        }
+    }
+    return true;
+};
