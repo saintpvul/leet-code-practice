9902072daa6dffc455a0ee7b99d8f442e4cea18c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-17 22:02:04 +0600

    feat: complete 557. reverse words in a string III

--- 557. Reverse Words in a String III/main.js
+++ 557. Reverse Words in a String III/main.js
@@ -5,3 +5,11 @@ Given a string s, reverse the order of characters in each word within a sentence
 */
 
 // solution
+
+var reverseWords = function (s) {
+    s = s.split(" ");
+    for (let i = 0; i < s.length; i++) {
+        s[i] = s[i].split("").reverse().join("");
+    }
+    return s.join(" ");
+};
