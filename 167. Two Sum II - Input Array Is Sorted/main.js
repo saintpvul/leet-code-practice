3276c5dd18c09160e62fd5f55b40ec7e5b05e6ab
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-17 21:46:03 +0600

    feat: complete 167. Two Sum II - input array is sorted

--- 167. Two Sum II - Input Array Is Sorted/main.js
+++ 167. Two Sum II - Input Array Is Sorted/main.js
@@ -11,3 +11,20 @@ Your solution must use only constant extra space.
 */
 
 // solution
+
+var twoSum = function (numbers, target) {
+    let left = 0,
+        right = numbers.length - 1;
+
+    while (left < right) {
+        let sum = numbers[left] + numbers[right];
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return null;
+};
