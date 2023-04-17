d197a9284c9ff8b41d7c3b81770051776d3d3a29
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-17 22:47:35 +0600

    feat: complete 142. linked list cycle ii

--- 142. Linked List Cycle II/main.js
+++ 142. Linked List Cycle II/main.js
@@ -9,3 +9,31 @@ Do not modify the linked list.
 */
 
 // solution
+
+var detectCycle = function (head) {
+    let slow = head,
+        fast = head,
+        hasCycle = false;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if (slow === fast) {
+            hasCycle = true;
+            break;
+        }
+    }
+
+    if (!hasCycle) {
+        return null;
+    }
+
+    slow = head;
+
+    while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    return slow;
+};
