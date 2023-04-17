2eafd264cb623d9f7e43f8e3f433efb19a76a4a9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-17 22:11:12 +0600

    feat: complete 876. middle of the linked list

--- 876. Middle of the Linked List/main.js
+++ 876. Middle of the Linked List/main.js
@@ -7,3 +7,14 @@ If there are two middle nodes, return the second middle node.
 */
 
 // solution
+
+var middleNode = function (head) {
+    let middle = head,
+        last = head;
+
+    while (last && last.next) {
+        middle = middle.next;
+        last = last.next.next;
+    }
+    return middle;
+};
