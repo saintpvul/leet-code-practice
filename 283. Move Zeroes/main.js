c02dc9238fa6fc042f51eca3ec9fef356aa76ef6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-17 21:44:45 +0600

    feat: complete 283. move zeroes

--- 283. Move Zeroes/main.js
+++ 283. Move Zeroes/main.js
@@ -7,3 +7,20 @@ Note that you must do this in-place without making a copy of the array.
 */
 
 // solution
+
+var moveZeroes = function (nums) {
+    let i = 0,
+        j = 0;
+
+    while (i < nums.length) {
+        if (nums[i] !== 0) {
+            nums[j] = nums[i];
+            j++;
+        }
+        i++;
+    }
+    while (j < nums.length) {
+        nums[j] = 0;
+        j++;
+    }
+};
