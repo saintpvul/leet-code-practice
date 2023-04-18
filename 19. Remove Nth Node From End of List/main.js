4e0d63de36d58ece878ab0da807a5e721ce0036c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-18 23:32:02 +0600

    feat: complete 19. remove nth node from end of lost

--- 19. Remove Nth Node From End of List/main.js
+++ 19. Remove Nth Node From End of List/main.js
@@ -5,3 +5,26 @@ Given the head of a linked list, remove the nth node from the end of the list an
 */
 
 // solution
+
+var removeNthFromEnd = function (head, n) {
+    let dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    let l = 0;
+    let curr = head;
+
+    while (curr) {
+        l++;
+        curr = curr.next;
+    }
+
+    let prev = dummyHead;
+    curr = head;
+    for (let i = 0; i < l - n; i++) {
+        prev = curr;
+        curr = curr.next;
+    }
+
+    prev.next = curr.next;
+
+    return dummyHead.next;
+};
