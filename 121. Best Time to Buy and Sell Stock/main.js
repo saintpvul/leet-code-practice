1ce6a6893ddd51459699f3079bb78a5e5915d367
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-18 21:51:17 +0600

    feat: complete 121. best time to buy and sell stock

--- 121. Best Time to Buy and Sell Stock/main.js
+++ 121. Best Time to Buy and Sell Stock/main.js
@@ -9,3 +9,17 @@ Return the maximum profit you can achieve from this transaction. If you cannot a
 */
 
 // solution
+
+var maxProfit = function (prices) {
+    let minPrice = prices[0],
+        maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        }
+    }
+    return maxProfit;
+};
