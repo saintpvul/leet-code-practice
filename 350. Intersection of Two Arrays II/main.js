675f449cbb66708e8b761d2f02b8a59c1b6c1fb7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-19 23:09:43 +0600

    feat: complete 350. intersection of two arrays ii

--- 350. Intersection of Two Arrays II/main.js
+++ 350. Intersection of Two Arrays II/main.js
@@ -7,3 +7,21 @@ Given two integer arrays nums1 and nums2, return an array of their intersection.
 */
 
 // solution
+
+var intersect = function (nums1, nums2) {
+    let shortest = nums1.length <= nums2.length ? nums1 : nums2;
+    let largest = shortest.length === nums1.length ? nums2 : nums1;
+    let counter = new Map();
+    let res = [];
+    for (let digit of shortest) {
+        !counter[digit] ? (counter[digit] = 1) : counter[digit]++;
+    }
+
+    for (let digit of largest) {
+        if (counter[digit] && counter[digit] > 0) {
+            res.push(digit);
+            counter[digit]--;
+        }
+    }
+    return res;
+};
