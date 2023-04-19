3a0cf2e02243fab8f2680f090c388f9911bf933c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-19 23:47:50 +0600

    feat: complete 102. binary tree level order traversal

--- 102. Binary Tree Level Order Traversal/main.js
+++ 102. Binary Tree Level Order Traversal/main.js
@@ -5,3 +5,21 @@ Given the root of a binary tree, return the level order traversal of its nodes' 
 */
 
 // solution
+
+var levelOrder = function (root) {
+    if (!root) return [];
+    let queue = [root];
+    let res = [];
+    while (queue.length) {
+        let levelSize = queue.length;
+        let level = [];
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        res.push(level);
+    }
+    return res;
+};
