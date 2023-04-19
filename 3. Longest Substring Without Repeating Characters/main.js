9926ff182cdc3f66d6c45b00ae2ecc51ada7fa05
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-19 21:25:26 +0600

    feat: complete 3. longest substring without repeating characters

--- 3. Longest Substring Without Repeating Characters/main.js
+++ 3. Longest Substring Without Repeating Characters/main.js
@@ -7,3 +7,21 @@ substring
 */
 
 // solution
+
+var lengthOfLongestSubstring = function (s) {
+    let end = 0;
+    let start = 0;
+    let maxLen = 0;
+    let chars = new Set();
+    while (end < s.length) {
+        if (!chars.has(s[end])) {
+            chars.add(s[end]);
+            end++;
+            maxLen = Math.max(maxLen, end - start);
+        } else {
+            chars.delete(s[start]);
+            start++;
+        }
+    }
+    return maxLen;
+};
