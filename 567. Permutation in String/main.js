797cdbf45d73ea8870cd84a02d091f68f4d3300e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-19 22:50:42 +0600

    feat: complete 567. permutation in string

--- 567. Permutation in String/main.js
+++ 567. Permutation in String/main.js
@@ -7,3 +7,26 @@ In other words, return true if one of s1's permutations is the substring of s2.
 */
 
 // solution
+
+var checkInclusion = function (s1, s2) {
+    const s1Counts = Array(26).fill(0);
+    const s2Counts = Array(26).fill(0);
+
+    for (let i = 0; i < s1.length; i++) {
+        s1Counts[s1.charCodeAt(i) - 97]++;
+    }
+
+    for (let i = 0; i < s1.length; i++) {
+        s2Counts[s2.charCodeAt(i) - 97]++;
+    }
+
+    for (let i = 0; i <= s2.length - s1.length; i++) {
+        if (s1Counts.every((count, index) => count === s2Counts[index])) {
+            return true;
+        }
+        s2Counts[s2.charCodeAt(i) - 97]--;
+        s2Counts[s2.charCodeAt(i + s1.length) - 97]++;
+    }
+
+    return false;
+};
