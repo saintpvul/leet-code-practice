56e4c51df134577b45f5d3342f735f8cf935a179
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-20 22:22:29 +0600

    feat: complete 118. pascal's triangle

--- 118. Pascal's Triangle/main.js
+++ 118. Pascal's Triangle/main.js
@@ -7,3 +7,20 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 */
 
 // solution
+
+var generate = function (numRows) {
+    let res = [[1]];
+    let curr = [];
+    for (let i = 1; i < numRows; i++) {
+        let k = 0;
+        let n = 1;
+        curr = Array(i + 1).fill(1);
+        for (let j = 1; j < curr.length - 1; j++) {
+            curr[j] = res[i - 1][k] + res[i - 1][n];
+            k++;
+            n++;
+        }
+        res.push(curr);
+    }
+    return res;
+};
