feaf74091c0f009817f185b19897408342268448
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-20 23:09:32 +0600

    feat: complete 695. max area of island

--- 695. Max Area of Island/main.js
+++ 695. Max Area of Island/main.js
@@ -9,3 +9,34 @@ Return the maximum area of an island in grid. If there is no island, return 0.
 */
 
 // solution
+
+var maxAreaOfIsland = function (grid) {
+    let maxArea = 0;
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === 1) {
+                let area = exploreIsland(grid, i, j);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+    }
+    return maxArea;
+};
+const exploreIsland = (grid, i, j) => {
+    if (
+        i < 0 ||
+        i >= grid.length ||
+        j < 0 ||
+        j >= grid[0].length ||
+        grid[i][j] === 0
+    ) {
+        return 0;
+    }
+    grid[i][j] = 0;
+    let area = 1;
+    area += exploreIsland(grid, i + 1, j);
+    area += exploreIsland(grid, i - 1, j);
+    area += exploreIsland(grid, i, j + 1);
+    area += exploreIsland(grid, i, j - 1);
+    return area;
+};
