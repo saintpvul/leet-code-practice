49a8f378b01af24e82f9ab03d1de2220e878862e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-21 19:45:03 +0600

    feat: complete 1512. number of good pairs

--- 1512. Number of Good Pairs/main.js
+++ 1512. Number of Good Pairs/main.js
@@ -7,3 +7,15 @@ A pair (i, j) is called good if nums[i] == nums[j] and i < j.
 */
 
 // solution
+
+var numIdenticalPairs = function (nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
