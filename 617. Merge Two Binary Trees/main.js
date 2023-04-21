f49e86e4e643b0aab77f2573c9f8f0174d565721
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-21 19:11:55 +0600

    feat: complete 617. merge two binary trees

--- 617. Merge Two Binary Trees/main.js
+++ 617. Merge Two Binary Trees/main.js
@@ -11,3 +11,12 @@ Note: The merging process must start from the root nodes of both trees.
 */
 
 // solution
+
+var mergeTrees = function (root1, root2) {
+    if (!root1) return root2;
+    if (!root2) return root1;
+    const newRoot = new TreeNode(root1.val + root2.val);
+    newRoot.left = mergeTrees(root1.left, root2.left);
+    newRoot.right = mergeTrees(root1.right, root2.right);
+    return newRoot;
+};
