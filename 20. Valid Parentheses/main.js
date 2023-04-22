c816d4e87bd511693cb4490c4ed822b09688341f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-22 22:52:46 +0600

    feat: complete 20. valid parentheses

--- 20. Valid Parentheses/main.js
+++ 20. Valid Parentheses/main.js
@@ -12,3 +12,26 @@ Every close bracket has a corresponding open bracket of the same type.
 */
 
 // solution
+
+var isValid = function (s) {
+    s = s.split("");
+    let par = [];
+    for (let p of s) {
+        if (p === "(" || p === "[" || p === "{") {
+            par.push(p);
+        } else {
+            if (!par.length) return false;
+            let last = par[par.length - 1];
+            if (p === ")" && last === "(") {
+                par.pop();
+            } else if (p === "]" && last === "[") {
+                par.pop();
+            } else if (p === "}" && last === "{") {
+                par.pop();
+            } else {
+                break;
+            }
+        }
+    }
+    return par.length === 0;
+};
