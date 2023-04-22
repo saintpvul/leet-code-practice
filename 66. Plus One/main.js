13250b196db484bfaec0e0a33a6b8c768a7eb11a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-22 21:36:43 +0600

    feat: complete 66. plus one

--- 66. Plus One/main.js
+++ 66. Plus One/main.js
@@ -7,3 +7,18 @@ Increment the large integer by one and return the resulting array of digits.
 */
 
 // solution
+
+var plusOne = function (digits) {
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0;
+        } else {
+            digits[i]++;
+            break;
+        }
+    }
+    if (digits[0] === 0) {
+        digits.unshift(1);
+    }
+    return digits;
+};
