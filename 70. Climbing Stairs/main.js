a836f563e307f87c104d0958387eb744d515057f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-23 23:35:45 +0600

    feat: complete 70. climbing stairs

--- 70. Climbing Stairs/main.js
+++ 70. Climbing Stairs/main.js
@@ -7,3 +7,11 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 */
 
 // solution
+
+var climbStairs = function (n) {
+    let fb = [1, 1];
+    for (let i = 2; i <= n; i++) {
+        fb[i] = fb[i - 1] + fb[i - 2];
+    }
+    return fb[n];
+};
