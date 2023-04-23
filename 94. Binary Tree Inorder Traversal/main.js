c5609e152d90d0f1806749bff7e05a60333c7402
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-23 23:16:50 +0600

    feat: complete 94. binary tree inorder traversal

--- 94. Binary Tree Inorder Traversal/main.js
+++ 94. Binary Tree Inorder Traversal/main.js
@@ -5,3 +5,15 @@ Given the root of a binary tree, return the inorder traversal of its nodes' valu
 */
 
 // solution
+
+var inorderTraversal = function (root) {
+    let res = [];
+    const traverse = (node) => {
+        if (!node) return;
+        traverse(node.left);
+        res.push(node.val);
+        traverse(node.right);
+    };
+    traverse(root);
+    return res;
+};
