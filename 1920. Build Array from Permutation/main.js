31d6f525d702e8d52fde7109e2a41b9eed84b56d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-24 21:25:52 +0600

    feat: complete 1920. build array from permutation

--- 1920. Build Array from Permutation/main.js
+++ 1920. Build Array from Permutation/main.js
@@ -7,3 +7,11 @@ A zero-based permutation nums is an array of distinct integers from 0 to nums.le
 */
 
 // solution
+
+var buildArray = function (nums) {
+    let ans = [];
+    for (let i = 0; i < nums.length; i++) {
+        ans.push(nums[nums[i]]);
+    }
+    return ans;
+};
