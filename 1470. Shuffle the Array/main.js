67a8e0dd16c7d100d8e684a0adb9cd5f7665fa2c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-24 21:31:33 +0600

    feat: complete 1470. shuffle the array

--- 1470. Shuffle the Array/main.js
+++ 1470. Shuffle the Array/main.js
@@ -7,3 +7,11 @@ Return the array in the form [x1,y1,x2,y2,...,xn,yn].
 */
 
 // solution
+
+var shuffle = function (nums, n) {
+    let res = [];
+    for (let x = 0, y = n; x < n; x++, y++) {
+        res.push(nums[x], nums[y]);
+    }
+    return res;
+};
