72e6d5a69ae0b7d8d36dfb37e88985af7eb6f143
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-25 19:46:30 +0600

    feat: complete 1603. design parking system

--- 1603. Design Parking System/main.js
+++ 1603. Design Parking System/main.js
@@ -10,3 +10,16 @@ bool addCar(int carType) Checks whether there is a parking space of carType for 
 */
 
 // solution
+
+var ParkingSystem = function (big, medium, small) {
+    this.slots = [big, medium, small];
+};
+
+ParkingSystem.prototype.addCar = function (carType) {
+    if (this.slots[carType - 1] > 0) {
+        this.slots[carType - 1]--;
+        return true;
+    } else {
+        return false;
+    }
+};
