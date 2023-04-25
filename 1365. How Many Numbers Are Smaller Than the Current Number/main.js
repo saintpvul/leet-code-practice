00a1c6966a0e47e9d133d8274f976c0ac2872e64
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-25 21:02:38 +0600

    feat: complete 1365. how many numbers are smaller than the current number

--- 1365. How Many Numbers Are Smaller Than the Current Number/main.js
+++ 1365. How Many Numbers Are Smaller Than the Current Number/main.js
@@ -7,3 +7,21 @@ Return the answer in an array.
 */
 
 // solution
+
+var smallerNumbersThanCurrent = function (nums) {
+    let res = [];
+    for (let i = 0; i < nums.length; i++) {
+        let counter = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (i === j) {
+                continue;
+            } else {
+                if (nums[i] > nums[j]) {
+                    counter++;
+                }
+            }
+        }
+        res.push(counter);
+    }
+    return res;
+};
