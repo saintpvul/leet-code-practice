56076f3e276a1bfa89e4e3e44a318b7c4ba6d07f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-25 22:34:37 +0600

    feat: complete 1281. subtract the product and sum of digits of an integer

--- 1281. Subtract the Product and Sum of Digits of an Integer/main.js
+++ 1281. Subtract the Product and Sum of Digits of an Integer/main.js
@@ -5,3 +5,21 @@ Given an integer number n, return the difference between the product of its digi
 */
 
 // solution
+
+// var subtractProductAndSum = function (n) {
+//     n = n.toString().split('').map(Number)
+//     let prod = n.reduce((prod, num) => (prod *= num), 1);
+//     let sum = n.reduce((sum, num) => (sum += num), 0);
+//     return prod - sum;
+// };
+
+var subtractProductAndSum = function (n) {
+    let prod = 1,
+        sum = 0;
+    n = n.toString();
+    for (let i = 0; i < n.length; i++) {
+        prod *= +n[i];
+        sum += +n[i];
+    }
+    return prod - sum;
+};
