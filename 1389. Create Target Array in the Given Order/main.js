f523458bd07291fbd305baa7c6ccf4a712c460c3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-26 22:04:35 +0600

    feat: complete 1389. create target array in the given order

--- 1389. Create Target Array in the Given Order/main.js
+++ 1389. Create Target Array in the Given Order/main.js
@@ -12,3 +12,13 @@ It is guaranteed that the insertion operations will be valid.
 */
 
 // solution
+
+function createTargetArray(nums, index) {
+    const target = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        target.splice(index[i], 0, nums[i]);
+    }
+
+    return target;
+}
