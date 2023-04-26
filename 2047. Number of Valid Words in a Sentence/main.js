7dcf08aab4f6d638b0d2aa1aa874621d53bbff28
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-26 22:40:10 +0600

    feat: complete 2047. number of valid words in a sentence

--- 2047. Number of Valid Words in a Sentence/main.js
+++ 2047. Number of Valid Words in a Sentence/main.js
@@ -14,3 +14,42 @@ Given a string sentence, return the number of valid words in sentence.
 */
 
 // solution
+
+var countValidWords = function (sentence) {
+    let str = sentence.split(" ");
+    let res = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== "" && isValid(str[i])) {
+            res++;
+        }
+    }
+    return res;
+};
+
+function isValid(s) {
+    let h = 0,
+        m = 0;
+    let ch = s.split("");
+    for (let i = 0; i < ch.length; i++) {
+        if (/\d|\s/.test(ch[i])) {
+            return false;
+        }
+        if (ch[i] == "-") {
+            h++;
+            if (h >= 2) return false;
+            else if (
+                i == 0 ||
+                i == ch.length - 1 ||
+                !/[a-zA-Z]/.test(ch[i - 1]) ||
+                !/[a-zA-Z]/.test(ch[i + 1])
+            )
+                return false;
+        }
+        if (ch[i] == "!" || ch[i] == "." || ch[i] == ",") {
+            m++;
+            if (m >= 2) return false;
+            if (i != ch.length - 1) return false;
+        }
+    }
+    return true;
+}
