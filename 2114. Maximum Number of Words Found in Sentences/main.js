5246b25ef81cefdbe86c32971df6dbc07bfb2aa3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-26 21:02:42 +0600

    feat: complete 2114. maximum number of words found in sentences

--- 2114. Maximum Number of Words Found in Sentences/main.js
+++ 2114. Maximum Number of Words Found in Sentences/main.js
@@ -9,3 +9,22 @@ Return the maximum number of words that appear in a single sentence.
 */
 
 // solution
+
+// var mostWordsFound = function (sentences) {
+//     return Math.max(...sentences.map((e) => e.split(" ").length));
+// };
+
+var mostWordsFound = function (sentences) {
+    let longest = 0;
+    for (let i = 0; i < sentences.length; i++) {
+        sentences[i] = sentences[i].split(" ");
+        let current = 0;
+        for (let j = 0; j < sentences[i].length; j++) {
+            current++;
+        }
+        if (longest < current) {
+            longest = current;
+        }
+    }
+    return longest;
+};
