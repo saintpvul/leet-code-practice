5815f0828b29d4b5961ef13d76eaed6d2ea882dd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-28 23:20:00 +0600

    feat: complete 500. keyboard row

--- 500. Keyboard Row/main.js
+++ 500. Keyboard Row/main.js
@@ -11,3 +11,28 @@ the third row consists of the characters "zxcvbnm".
 */
 
 // solution
+
+var findWords = function (words) {
+    const keyboard = ["qwertyuiop", "asdfghjkl", "zxcvbnm"];
+    const results = [];
+    for (let word of words) {
+        let row = -1;
+        let isOneRow = true;
+        for (let i = 0; i < keyboard.length; i++) {
+            if (keyboard[i].indexOf(word[0].toLowerCase()) !== -1) {
+                row = i;
+                break;
+            }
+        }
+        for (let char of word) {
+            if (keyboard[row].indexOf(char.toLowerCase()) === -1) {
+                isOneRow = false;
+                break;
+            }
+        }
+        if (isOneRow) {
+            results.push(word);
+        }
+    }
+    return results;
+};
