2d5d7c70b58ab7f54671247c3182a00d987516ed
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-29 01:44:31 +0600

    feat: start 2399. check distance between same letters

---
+++ 2399. Check Distances Between Same Letters/main.js
@@ -0,0 +1,13 @@
+/*
+
+You are given a 0-indexed string s consisting of only lowercase English letters, where each letter in s appears exactly twice. You are also given a 0-indexed integer array distance of length 26.
+
+Each letter in the alphabet is numbered from 0 to 25 (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, ... , 'z' -> 25).
+
+In a well-spaced string, the number of letters between the two occurrences of the ith letter is distance[i]. If the ith letter does not appear in s, then distance[i] can be ignored.
+
+Return true if s is a well-spaced string, otherwise return false.
+
+*/
+
+// solution
