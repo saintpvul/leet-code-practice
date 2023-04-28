aba06be1dc27ee53f4ef8b8deb959b7273a59d47
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-04-28 23:43:35 +0600

    feat: complete 2085. count common words with one occurence

--- 2085. Count Common Words With One Occurrence/main.js
+++ 2085. Count Common Words With One Occurrence/main.js
@@ -5,3 +5,24 @@ Given two string arrays words1 and words2, return the number of strings that app
 */
 
 // solution
+
+var countWords = function (words1, words2) {
+    let count = 0;
+    let wordsCount = {};
+    for (let word of words1) {
+        wordsCount[word] = (wordsCount[word] || 0) + 1;
+    }
+    for (let word of words2) {
+        wordsCount[word] = (wordsCount[word] || 0) + 1;
+    }
+    for (let word in wordsCount) {
+        if (
+            wordsCount[word] === 2 &&
+            words1.includes(word) &&
+            words2.includes(word)
+        ) {
+            count++;
+        }
+    }
+    return count;
+};
