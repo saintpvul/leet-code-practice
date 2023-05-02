45af1f287a3c809ea8fae251b112d74fc8c78f38
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-02 23:11:30 +0600

    feat: complete 2535. difference between element sum and digit sum of an array

--- 2535. Difference Between Element Sum and Digit Sum of an Array/main.js
+++ 2535. Difference Between Element Sum and Digit Sum of an Array/main.js
@@ -13,3 +13,16 @@ Note that the absolute difference between two integers x and y is defined as |x 
 */
 
 // solution
+
+var differenceOfSum = function (nums) {
+    let sum = 0;
+    let digitsSum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+        digitsSum += nums[i]
+            .toString()
+            .split("")
+            .reduce((s, v) => (s += +v), 0);
+    }
+    return Math.abs(sum - digitsSum);
+};
