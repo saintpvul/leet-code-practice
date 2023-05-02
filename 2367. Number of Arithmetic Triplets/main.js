51495acf7f8f624d18705227f2dce6d8ff0930a5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-02 23:28:16 +0600

    feat: complete 2367. number of arithmetic triplets

--- 2367. Number of Arithmetic Triplets/main.js
+++ 2367. Number of Arithmetic Triplets/main.js
@@ -10,3 +10,16 @@ Return the number of unique arithmetic triplets.
 */
 
 // solution
+
+var arithmeticTriplets = function (nums, diff) {
+    let counter = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (nums[j] - nums[i] === diff && nums[k] - nums[j] === diff)
+                    counter++;
+            }
+        }
+    }
+    return counter;
+};
