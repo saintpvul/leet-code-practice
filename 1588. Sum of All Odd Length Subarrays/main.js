f89f2b828e28565ea670487e670b110b2d4a5688
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-02 23:57:56 +0600

    feat: complete 1588. sum of all odd length subarrays

--- 1588. Sum of All Odd Length Subarrays/main.js
+++ 1588. Sum of All Odd Length Subarrays/main.js
@@ -7,3 +7,16 @@ A subarray is a contiguous subsequence of the array.
 */
 
 // solution
+
+var sumOddLengthSubarrays = function (arr) {
+    let sum = 0;
+    const l = arr.length;
+    for (let i = 0; i < l; i++) {
+        const lOdd = Math.floor((i + 1) / 2);
+        const lEven = Math.floor(i / 2) + 1;
+        const rOdd = Math.floor((l - i) / 2);
+        const rEven = Math.floor((l - i - 1) / 2) + 1;
+        sum += arr[i] * (lOdd * rOdd + lEven * rEven);
+    }
+    return sum;
+};
