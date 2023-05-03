85b517b6b36d6951c19d17472ed8d0b1f7c9c4e9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-03 23:49:28 +0600

    feat: complete 2215. find the difference of two arrays

--- 2215. Find the Difference of Two Arrays/main.js
+++ 2215. Find the Difference of Two Arrays/main.js
@@ -9,3 +9,19 @@ Note that the integers in the lists may be returned in any order.
 */
 
 // solution
+
+var findDifference = function (nums1, nums2) {
+    let res = [[], []];
+    for (let i = 0; i < nums1.length; i++) {
+        if (!nums2.includes(nums1[i]) && !res[0].includes(nums1[i])) {
+            res[0].push(nums1[i]);
+        }
+    }
+
+    for (let i = 0; i < nums2.length; i++) {
+        if (!nums1.includes(nums2[i]) && !res[1].includes(nums2[i])) {
+            res[1].push(nums2[i]);
+        }
+    }
+    return res;
+};
