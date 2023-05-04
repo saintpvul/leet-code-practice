7389178f6d0d2339b2e5789c7c30a5ed55138ffb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-04 23:15:46 +0600

    feat: complete 2006. count number of pairs with absolute difference k

--- 2006. Count Number of Pairs With Absolute Difference K/main.js
+++ 2006. Count Number of Pairs With Absolute Difference K/main.js
@@ -10,3 +10,15 @@ x if x >= 0.
 */
 
 // solution
+
+var countKDifference = function (nums, k) {
+    let count = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (i < j && Math.abs(nums[i] - nums[j]) === k) {
+                count++;
+            }
+        }
+    }
+    return count;
+};
