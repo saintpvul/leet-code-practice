050e38f3d5e8cc81be261d68f2e9f1a3806e27f4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-04 22:54:26 +0600

    feat: complete 2373. largest local values in a matrix

--- 2373. Largest Local Values in a Matrix/main.js
+++ 2373. Largest Local Values in a Matrix/main.js
@@ -12,3 +12,23 @@ Return the generated matrix.
 */
 
 // solution
+
+var largestLocal = function (grid) {
+    const n = grid.length - 2;
+    const maxLocal = new Array(n);
+    for (let i = 0; i < n; i++) {
+        maxLocal[i] = new Array(n).fill(0);
+        for (let j = 0; j < n; j++) {
+            let maxVal = Number.MIN_SAFE_INTEGER;
+            for (let k = i; k < i + 3; k++) {
+                for (let l = j; l < j + 3; l++) {
+                    if (grid[k][l] > maxVal) {
+                        maxVal = grid[k][l];
+                    }
+                }
+            }
+            maxLocal[i][j] = maxVal;
+        }
+    }
+    return maxLocal;
+};
