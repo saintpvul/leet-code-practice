2695252d4569e5c3d9891f19b704696d15f954d1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-06 00:11:41 +0600

    feat: complete 1636. sort array by increasing frequency

--- 1636. Sort Array by Increasing Frequency/main.js
+++ 1636. Sort Array by Increasing Frequency/main.js
@@ -7,3 +7,22 @@ Return the sorted array.
 */
 
 // solution
+
+var frequencySort = function (nums) {
+    let count = {};
+    for (const num of nums) {
+        count[num] ? count[num]++ : (count[num] = 1);
+    }
+
+    let res = [];
+    count = Object.entries(count).sort((a, b) =>
+        a[1] === b[1] ? b[0] - a[0] : a[1] - b[1]
+    );
+    for (const val in count) {
+        while (count[val][1] > 0) {
+            res.push(+count[val][0]);
+            count[val][1]--;
+        }
+    }
+    return res;
+};
