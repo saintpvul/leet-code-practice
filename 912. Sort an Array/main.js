d21923e33cdae9b0f6a92a7e9dc246e1f9c03556
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-05 23:56:09 +0600

    feat: complete 912. sort an array

--- 912. Sort an Array/main.js
+++ 912. Sort an Array/main.js
@@ -7,3 +7,24 @@ You must solve the problem without using any built-in functions in O(nlog(n)) ti
 */
 
 // solution
+
+var sortArray = function (nums) {
+    const min = Math.min(...nums);
+    const max = Math.max(...nums);
+    const temp = Array.from({ length: max - min + 1 }, () => 0);
+
+    for (const num of nums) {
+        temp[num - min]++;
+    }
+
+    let curr = 0;
+    for (let i = 0; i < temp.length; i++) {
+        while (temp[i]) {
+            nums[curr] = min + i;
+            curr++;
+            temp[i]--;
+        }
+    }
+
+    return nums;
+};
