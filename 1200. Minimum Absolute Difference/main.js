d538aafdc109c426c9d4b1aeb2bd02b2b7230326
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-05 22:17:31 +0600

    feat: complete 1200. minimum absolute difference

--- 1200. Minimum Absolute Difference/main.js
+++ 1200. Minimum Absolute Difference/main.js
@@ -11,3 +11,18 @@ b - a equals to the minimum absolute difference of any two elements in arr
 */
 
 // solution
+
+var minimumAbsDifference = function (arr) {
+    arr.sort((a, b) => a - b);
+    let diff = arr[arr.length - 1];
+    for (let i = 0, j = i + 1; i < arr.length - 1; i++, j++) {
+        if (Math.abs(arr[i] - arr[j]) <= diff) diff = Math.abs(arr[i] - arr[j]);
+    }
+    let res = [];
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (Math.abs(arr[i] - arr[i + 1]) === diff) {
+            res.push([arr[i], arr[i + 1]]);
+        }
+    }
+    return res;
+};
