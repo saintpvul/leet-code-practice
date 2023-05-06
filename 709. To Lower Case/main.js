bb96dda610dbbbf60d2c36046b3b548a97d501c5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-06 23:22:47 +0600

    feat: complete 709. to lower case

--- 709. To Lower Case/main.js
+++ 709. To Lower Case/main.js
@@ -1,7 +1,11 @@
 /*
 
-
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
 
 */
 
 // solution
+
+var toLowerCase = function (s) {
+    return s.toLowerCase();
+};
