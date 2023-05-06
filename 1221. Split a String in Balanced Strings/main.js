e9b9738b1563812bc690d4b36fb661c73c4f4410
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-06 23:58:56 +0600

    feat: complete 1221. split a string in balanced strings

--- 1221. Split a String in Balanced Strings/main.js
+++ 1221. Split a String in Balanced Strings/main.js
@@ -10,3 +10,20 @@ Return the maximum number of balanced strings you can obtain.
 */
 
 // solution
+
+var balancedStringSplit = function (s) {
+    let count = 0;
+    let balance = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] == "L") {
+            balance++;
+        } else {
+            balance--;
+        }
+        if (balance == 0) {
+            count++;
+        }
+    }
+    return count;
+};
