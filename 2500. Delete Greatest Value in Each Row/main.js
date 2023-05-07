e1091964efd0808c8b77145c19682047735b7f31
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-07 18:15:03 +0600

    feat: complete 2500. delete greatest value in each row

--- 2500. Delete Greatest Value in Each Row/main.js
+++ 2500. Delete Greatest Value in Each Row/main.js
@@ -13,3 +13,20 @@ Return the answer after performing the operations described above.
 */
 
 // solution
+
+var deleteGreatestValue = function (grid) {
+    let deleted = 0;
+
+    while (grid[0].length) {
+        let currentMax = 0;
+        let maxVals = [];
+        for (let i = 0; i < grid.length; i++) {
+            let max = Math.max(...grid[i]);
+            maxVals.push(max);
+            grid[i].splice(grid[i].indexOf(max), 1);
+        }
+        currentMax = Math.max(...maxVals);
+        deleted += currentMax;
+    }
+    return deleted;
+};
