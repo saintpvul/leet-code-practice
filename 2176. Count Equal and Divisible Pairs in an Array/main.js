89d859334bc9a2a52a5946fdff1633f47bde6a65
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-07 18:34:01 +0600

    feat: complete 2176. count equal and divisible pairs in an array

--- 2176. Count Equal and Divisible Pairs in an Array/main.js
+++ 2176. Count Equal and Divisible Pairs in an Array/main.js
@@ -5,3 +5,15 @@ Given a 0-indexed integer array nums of length n and an integer k, return the nu
 */
 
 // solution
+
+var countPairs = function (nums, k) {
+    let pairs = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && !((i * j) % k)) {
+                pairs++;
+            }
+        }
+    }
+    return pairs;
+};
