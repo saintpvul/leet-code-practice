9d72ab5b3699b1b553acfa7dca07f8a0424f73a5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-07 18:24:18 +0600

    feat: complete 1464. maximum product of two elements in an array

--- 1464. Maximum Product of Two Elements in an Array/main.js
+++ 1464. Maximum Product of Two Elements in an Array/main.js
@@ -5,3 +5,16 @@ Given the array of integers nums, you will choose two different indices i and j 
 */
 
 // solution
+
+var maxProduct = function (nums) {
+    let max = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            let product = (nums[i] - 1) * (nums[j] - 1);
+            if (product >= max) {
+                max = product;
+            }
+        }
+    }
+    return max;
+};
