2503b6d22234d4cd4f57070fd7c5f7440e7cf60c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-08 23:59:23 +0600

    feat: complete 832. flipping an image

--- 832. Flipping an Image/main.js
+++ 832. Flipping an Image/main.js
@@ -12,3 +12,15 @@ For example, inverting [0,1,1] results in [1,0,0].
 */
 
 // solution
+
+var flipAndInvertImage = function (image) {
+    const n = image.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < Math.floor((n + 1) / 2); j++) {
+            const temp = image[i][j] ^ 1;
+            image[i][j] = image[i][n - 1 - j] ^ 1;
+            image[i][n - 1 - j] = temp;
+        }
+    }
+    return image;
+};
