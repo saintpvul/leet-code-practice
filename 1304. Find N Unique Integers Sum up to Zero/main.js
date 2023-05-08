cbc0ec571a73ca226872502e7f1231ebbbbdc626
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-09 01:30:42 +0600

    feat: complete 1304. find n unique integers sum up to zero

--- 1304. Find N Unique Integers Sum up to Zero/main.js
+++ 1304. Find N Unique Integers Sum up to Zero/main.js
@@ -5,3 +5,16 @@ Given an integer n, return any array containing n unique integers such that they
 */
 
 // solution
+
+var sumZero = function (n) {
+    let summed = [];
+    if (n % 2) {
+        n -= 1;
+        summed.push(0);
+    }
+    let count = n / 2;
+    for (let i = 1; i <= count; i++) {
+        summed.push(i, -i);
+    }
+    return summed;
+};
