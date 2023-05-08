35a418b81b9abf3fbe186365b7fd41df70610d5f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-08 23:50:48 +0600

    feat: complete 942. di string match

--- 942. DI String Match/main.js
+++ 942. DI String Match/main.js
@@ -9,3 +9,32 @@ Given a string s, reconstruct the permutation perm and return it. If there are m
 */
 
 // solution
+
+var diStringMatch = function (s) {
+    let n = s.length;
+    let perm = Array(n + 1).fill(0);
+
+    const reverse = (arr, start, end) => {
+        while (start < end) {
+            [arr[start], arr[end]] = [arr[end], arr[start]];
+            start++;
+            end--;
+        }
+    };
+
+    for (let i = 0; i <= n; i++) {
+        perm[i] = i;
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (s[i] === "D") {
+            let j = i;
+            while (i < n && s[i] === "D") {
+                i++;
+            }
+            reverse(perm, j, i);
+        }
+    }
+
+    return perm;
+};
