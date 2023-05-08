5d2ed9a18e42ea714e5e550bbbc3155dbaff065a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-09 00:26:38 +0600

    feat: complete 1295. find numbers with even numver of digits

--- 1295. Find Numbers with Even Number of Digits/main.js
+++ 1295. Find Numbers with Even Number of Digits/main.js
@@ -5,3 +5,7 @@ Given an array nums of integers, return how many of them contain an even number 
 */
 
 // solution
+
+var findNumbers = function (nums) {
+    return nums.filter((e) => !(e.toString().length % 2)).length;
+};
