b9fa8a6c20175951416d135c3c05f0d943c1704f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-09 00:27:40 +0600

    feat: start 1979. find greatest common divisor of array

---
+++ 1979. Find Greatest Common Divisor of Array/main.js
@@ -0,0 +1,9 @@
+/*
+
+Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+*/
+
+// solution
