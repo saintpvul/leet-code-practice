b1cd8626202d8d35e4d9d457f5599637570172b2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-11 02:22:58 +0600

    feat: complete 2670. find the distinct difference array

--- 2670. Find the Distinct Difference Array/main.js
+++ 2670. Find the Distinct Difference Array/main.js
@@ -11,3 +11,36 @@ Note that nums[i, ..., j] denotes the subarray of nums starting at index i and e
 */
 
 // solution
+
+//simple and slow ( O(n^2))
+
+// var distinctDifferenceArray = function (nums) {
+//     let diff = [];
+//     for (let i = 0; i < nums.length - 0; i++) {
+//         let prefix = Array.from(new Set([...nums].slice(0, i + 1))).length;
+//         let suffix = Array.from(new Set([...nums].slice(i + 1))).length;
+//         diff.push(prefix - suffix);
+//     }
+//     return diff;
+// };
+
+var distinctDifferenceArray = function (nums) {
+    let n = nums.length;
+    let prefix = Array.from({ length: n });
+    let suffix = Array.from({ length: n });
+    let uniq = new Set();
+
+    for (let i = 0; i < n; i++) {
+        uniq.add(nums[i]);
+        prefix[i] = uniq.size;
+    }
+
+    uniq = new Set();
+
+    for (let i = n - 1; i >= 0; i--) {
+        suffix[i] = uniq.size;
+        uniq.add(nums[i]);
+    }
+
+    return prefix.map((pre, i) => pre - suffix[i]);
+};
