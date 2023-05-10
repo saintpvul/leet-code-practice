0ae5020c347854f18b43eda926c5b5ea53e78259
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-10 23:40:20 +0600

    feat: complete 1450. number of students doing homework at a given time

--- 1450. Number of Students Doing Homework at a Given Time/main.js
+++ 1450. Number of Students Doing Homework at a Given Time/main.js
@@ -9,3 +9,13 @@ Return the number of students doing their homework at time queryTime. More forma
 */
 
 // solution
+
+var busyStudent = function (startTime, endTime, queryTime) {
+    let count = 0;
+    for (let i = 0; i < startTime.length; i++) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+            count++;
+        }
+    }
+    return count;
+};
