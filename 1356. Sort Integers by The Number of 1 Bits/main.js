f292e40ac78073d26b842a03db256a5bd89d60b5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-11 02:52:41 +0600

    feat: complete 1356. sort integers by the number of 1 bits

--- 1356. Sort Integers by The Number of 1 Bits/main.js
+++ 1356. Sort Integers by The Number of 1 Bits/main.js
@@ -8,16 +8,35 @@ Return the array after sorting it.
 
 // solution
 
-var sortByBits = function (arr) {
-    let bin = new Map();
+// var sortByBits = function (arr) {
+//     let bin = new Map();
 
-    for (let val of arr) {
-        bin.set(val, val.toString(2).replace(/[0]/gi, "").length);
-    }
+//     for (let val of arr) {
+//         bin.set(val, val.toString(2).replace(/[0]/gi, "").length);
+//     }
+
+//     return arr.sort((a, b) => {
+//         let countA = bin.get(a);
+//         let countB = bin.get(b);
+//         return countA === countB ? a - b : countA - countB;
+//     });
+// };
+
+//using bitwise operators
+
+var sortByBits = function (arr) {
+    const countBits = (num) => {
+        let count = 0;
+        while (num) {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    };
 
     return arr.sort((a, b) => {
-        let countA = bin.get(a);
-        let countB = bin.get(b);
+        const countA = countBits(a);
+        const countB = countBits(b);
         return countA === countB ? a - b : countA - countB;
     });
 };
