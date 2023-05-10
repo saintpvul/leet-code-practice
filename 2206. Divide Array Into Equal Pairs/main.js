305277a8d3d6b4084c475eff6b374f1f19140563
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-11 02:42:25 +0600

    feat: complete 2206. divide array into equal pairs

--- 2206. Divide Array Into Equal Pairs/main.js
+++ 2206. Divide Array Into Equal Pairs/main.js
@@ -12,13 +12,26 @@ Return true if nums can be divided into n pairs, otherwise return false.
 
 // solution
 
+// bit slower
+
+// var divideArray = function (nums) {
+//     let keeper = {};
+//     for (let i = 0; i < nums.length; i++) {
+//         keeper[nums[i]] ? keeper[nums[i]]++ : (keeper[nums[i]] = 1);
+//         if (keeper[nums[i]] % 2 && nums.lastIndexOf(nums[i]) === i) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
 var divideArray = function (nums) {
-    let keeper = {};
-    for (let i = 0; i < nums.length; i++) {
-        keeper[nums[i]] ? keeper[nums[i]]++ : (keeper[nums[i]] = 1);
-        if (keeper[nums[i]] % 2 && nums.lastIndexOf(nums[i]) === i) {
-            return false;
-        }
-    }
-    return true;
+    return Object.values(
+        nums.reduce(
+            (count, val) => (
+                count[val] ? count[val]++ : (count[val] = 1), count
+            ),
+            {}
+        )
+    ).every((val) => !(val % 2));
 };
