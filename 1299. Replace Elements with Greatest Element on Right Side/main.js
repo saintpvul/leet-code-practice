e89462a5a242d117c53818ab8ce9c7fe72d0fa1c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-13 02:33:12 +0600

    feat: complete 1299. replace elements with greatest element on right side

--- 1299. Replace Elements with Greatest Element on Right Side/main.js
+++ 1299. Replace Elements with Greatest Element on Right Side/main.js
@@ -7,3 +7,13 @@ After doing so, return the array.
 */
 
 // solution
+
+var replaceElements = function (arr) {
+    let greatest = -1;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const current = arr[i];
+        arr[i] = greatest;
+        greatest = Math.max(greatest, current);
+    }
+    return arr;
+};
