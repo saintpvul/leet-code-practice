1511460b8d0a804dd76f36bc227899cd9e31986e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-12 23:34:31 +0600

    feat: complete 961. n-repeated element in size 2n array

--- 961. N-Repeated Element in Size 2N Array/main.js
+++ 961. N-Repeated Element in Size 2N Array/main.js
@@ -11,6 +11,20 @@ Return the element that is repeated n times.
 
 // solution
 
+// slower
+
+/*
+var repeatedNTimes = function (nums) {
+    let n = nums.length / 2;
+    let counter = {};
+    for (let i = 0; i < nums.length; i++) {
+        const element = nums[i];
+        counter[element] ? counter[element]++ : (counter[element] = 1);
+        if (counter[element] === n) return element;
+    }
+};
+*/
+
 var repeatedNTimes = function (nums) {
     let elements = {};
     for (let element of nums) {
