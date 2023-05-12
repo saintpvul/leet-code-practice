0115dd2027069cf92b696cd14e52fd9c23574d71
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-12 23:23:36 +0600

    feat: complete 475. final prices with a special discount in a shop

--- 475. Final Prices With a Special Discount in a Shop/main.js
+++ 475. Final Prices With a Special Discount in a Shop/main.js
@@ -9,3 +9,15 @@ Return an integer array answer where answer[i] is the final price you will pay f
 */
 
 // solution
+
+var finalPrices = function (prices) {
+    for (let i = 0; i < prices.length - 1; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            if (prices[j] <= prices[i]) {
+                prices[i] -= prices[j];
+                break;
+            }
+        }
+    }
+    return prices;
+};
