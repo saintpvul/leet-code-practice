6ee22bd79a0b95190346cee08782d5f0816589d7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-15 01:08:25 +0600

    feat: complete 2389. longest subsequence with limited sum

--- 2389. Longest Subsequence With Limited Sum/main.js
+++ 2389. Longest Subsequence With Limited Sum/main.js
@@ -9,3 +9,39 @@ A subsequence is an array that can be derived from another array by deleting som
 */
 
 // solution
+
+var answerQueries = function (nums, queries) {
+    const sortedNums = nums.sort((a, b) => a - b);
+    const prefixSums = [0];
+
+    for (let i = 0; i < sortedNums.length; i++) {
+        prefixSums[i + 1] = prefixSums[i] + sortedNums[i];
+    }
+
+    const answer = [];
+
+    for (let i = 0; i < queries.length; i++) {
+        const query = queries[i];
+        const index = binarySearch(prefixSums, query);
+        answer.push(index);
+    }
+
+    return answer;
+};
+
+const binarySearch = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] <= target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return right;
+};
