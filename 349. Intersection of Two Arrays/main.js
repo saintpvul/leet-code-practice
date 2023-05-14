8461b6bfa3f58777c655749b81deb502cac4dc7b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-14 23:17:30 +0600

    feat: complete 349.intersection of two arrays

--- 349. Intersection of Two Arrays/main.js
+++ 349. Intersection of Two Arrays/main.js
@@ -5,3 +5,13 @@ Given two integer arrays nums1 and nums2, return an array of their intersection.
 */
 
 // solution
+
+var intersection = function (nums1, nums2) {
+    let res = new Set();
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.includes(nums1[i])) {
+            res.add(nums1[i]);
+        }
+    }
+    return Array.from(res);
+};
