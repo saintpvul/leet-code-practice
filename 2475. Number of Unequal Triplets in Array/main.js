b6f4ec8fda4af0529289a33eacaebdb6f7daab9b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-14 22:57:43 +0600

    feat: complete 2475. number of unequal triplets in array

--- 2475. Number of Unequal Triplets in Array/main.js
+++ 2475. Number of Unequal Triplets in Array/main.js
@@ -10,3 +10,21 @@ Return the number of triplets that meet the conditions.
 */
 
 // solution
+
+var unequalTriplets = function (nums) {
+    let count = 0;
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                if (
+                    nums[i] != nums[j] &&
+                    nums[i] != nums[k] &&
+                    nums[j] != nums[k]
+                ) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+};
