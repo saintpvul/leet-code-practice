07b07e0fc7a6510c2e2da0e069fa8a529c162205
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-15 00:32:08 +0600

    feat: complete 1460. make two arrays equal by reversing subarrays

--- 1460. Make Two Arrays Equal by Reversing Subarrays/main.js
+++ 1460. Make Two Arrays Equal by Reversing Subarrays/main.js
@@ -7,3 +7,40 @@ Return true if you can make arr equal to target or false otherwise.
 */
 
 // solution
+
+// var canBeEqual = function (target, arr) {
+//     let countTarget = {};
+//     let countArr = {};
+
+//     for (let i = 0; i < target.length; i++) {
+//         countTarget[target[i]]
+//             ? countTarget[target[i]]++
+//             : (countTarget[target[i]] = 1);
+//     }
+
+//     for (let i = 0; i < arr.length; i++) {
+//         countArr[arr[i]] ? countArr[arr[i]]++ : (countArr[arr[i]] = 1);
+//     }
+
+//     const targetString = JSON.stringify(countTarget);
+//     const arrString = JSON.stringify(countArr);
+
+//     return targetString === arrString;
+// };
+
+var canBeEqual = function (target, arr) {
+    const count = {};
+
+    for (let i = 0; i < target.length; i++) {
+        count[target[i]] = (count[target[i]] || 0) + 1;
+        count[arr[i]] = (count[arr[i]] || 0) - 1;
+    }
+
+    for (const key in count) {
+        if (count[key] !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
