e3602b8bd3690a14a5ca7076ea6a060b51ebba2d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-17 00:48:49 +0600

    feat: complete 345. reverse vowels of a string

--- 345. Reverse Vowels of a String/main.js
+++ 345. Reverse Vowels of a String/main.js
@@ -15,27 +15,54 @@ let str = "hello";
  * @param {string} s
  * @return {string}
  */
-function reverseVowels(s) {
-  let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  let foundedVowels = [];
-  for (let i = 0; i < s.length; i++) {
-    if (vowels.indexOf(s[i]) > -1) {
-      foundedVowels.push(s[i]);
+
+// function reverseVowels(s) {
+//   let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+//   let foundedVowels = [];
+//   for (let i = 0; i < s.length; i++) {
+//     if (vowels.indexOf(s[i]) > -1) {
+//       foundedVowels.push(s[i]);
+//     }
+//   }
+//   console.log(foundedVowels);
+//   let splittedArr = s.split("");
+//   let lastIndex = foundedVowels.length - 1;
+//   for (let i = 0; i < splittedArr.length; i++) {
+//     if (vowels.indexOf(splittedArr[i]) > -1) {
+//       splittedArr[i] = foundedVowels[lastIndex];
+//       lastIndex--;
+//     }
+//   }
+//   if (s !== splittedArr.join("")) {
+//     return splittedArr.join("");
+//   }
+//   if (s !== null) {
+//     return s;
+//   }
+// }
+
+// faster
+
+var reverseVowels = function (s) {
+    s = s.split("");
+    let vowels = new Set(["a", "e", "i", "o", "u"]);
+    let start = 0;
+    let end = s.length - 1;
+    while (start < end) {
+        while (start < end && !vowels.has(s[start].toLowerCase())) {
+            start++;
+        }
+        while (start < end && !vowels.has(s[end].toLowerCase())) {
+            end--;
+        }
+        if (start < end) {
+            let temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
     }
-  }
-  console.log(foundedVowels);
-  let splittedArr = s.split("");
-  let lastIndex = foundedVowels.length - 1;
-  for (let i = 0; i < splittedArr.length; i++) {
-    if (vowels.indexOf(splittedArr[i]) > -1) {
-      splittedArr[i] = foundedVowels[lastIndex];
-      lastIndex--;
-    }
-  }
-  if (s !== splittedArr.join("")) {
-    return splittedArr.join("");
-  }
-  if (s !== null) {
+    s = s.join("");
     return s;
-  }
-}
+};
