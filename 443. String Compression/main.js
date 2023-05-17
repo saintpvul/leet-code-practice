92322b9624d989bdbc8a97b93cf781719b4d63f3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-18 00:13:31 +0600

    feat: complete 443. string compression

--- 443. String Compression/main.js
+++ 443. String Compression/main.js
@@ -15,3 +15,27 @@ You must write an algorithm that uses only constant extra space.
 */
 
 // solution
+
+var compress = function (chars) {
+    let write = 0;
+    let count = 1;
+
+    for (let read = 1; read <= chars.length; read++) {
+        if (read < chars.length && chars[read] === chars[read - 1]) {
+            count++;
+        } else {
+            chars[write++] = chars[read - 1];
+
+            if (count > 1) {
+                const countStr = count.toString();
+                for (let i = 0; i < countStr.length; i++) {
+                    chars[write++] = countStr[i];
+                }
+            }
+
+            count = 1;
+        }
+    }
+
+    return write;
+};
