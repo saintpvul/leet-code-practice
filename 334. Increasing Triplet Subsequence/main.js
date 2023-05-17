a522b1902f08bafe3f200d09bfe2171b0934ec0c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-18 00:00:26 +0600

    feat: complete 334. increasing triplet subsequence

--- 334. Increasing Triplet Subsequence/main.js
+++ 334. Increasing Triplet Subsequence/main.js
@@ -5,3 +5,20 @@ Given an integer array nums, return true if there exists a triple of indices (i,
 */
 
 // solution
+
+var increasingTriplet = function (nums) {
+    let firstSmallest = Infinity;
+    let secondSmallest = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] <= firstSmallest) {
+            firstSmallest = nums[i];
+        } else if (nums[i] <= secondSmallest) {
+            secondSmallest = nums[i];
+        } else {
+            return true;
+        }
+    }
+
+    return false;
+};
