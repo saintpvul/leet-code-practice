6100f081112c322dd40b4016cf5153748c0a9ba9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-17 23:57:24 +0600

    feat: complete 238. product of array except self

--- 238. Product of Array Except Self/main.js
+++ 238. Product of Array Except Self/main.js
@@ -9,3 +9,21 @@ You must write an algorithm that runs in O(n) time and without using the divisio
 */
 
 // solution
+
+var productExceptSelf = function (nums) {
+    let n = nums.length;
+    let answer = new Array(n);
+
+    answer[0] = 1;
+    for (let i = 1; i < n; i++) {
+        answer[i] = answer[i - 1] * nums[i - 1];
+    }
+
+    let suffixProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        answer[i] *= suffixProduct;
+        suffixProduct *= nums[i];
+    }
+
+    return answer;
+};
