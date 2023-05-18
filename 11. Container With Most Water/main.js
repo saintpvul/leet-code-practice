29e610922cf544fcc2ef4aa1ac81b4b97059b8b1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-18 21:47:11 +0600

    feat: complete 11. container with most water

--- 11. Container With Most Water/main.js
+++ 11. Container With Most Water/main.js
@@ -11,3 +11,22 @@ Notice that you may not slant the container.
 */
 
 // solution
+
+var maxArea = function (height) {
+    let l = 0;
+    let r = height.length - 1;
+    let max = 0;
+    while (l < r) {
+        let vol = Math.min(height[l], height[r]) * (r - l);
+        if (max < vol) max = vol;
+        if (height[l] < height[r]) {
+            l++;
+        } else if (height[l] > height[r]) {
+            r--;
+        } else {
+            l++;
+            r--;
+        }
+    }
+    return max;
+};
