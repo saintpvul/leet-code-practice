37dde6936de12823589397d4ba933992af55b301
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-18 22:10:16 +0600

    feat: complete 1679. max number of k-sum pairs

--- 1679. Max Number of K-Sum Pairs/main.js
+++ 1679. Max Number of K-Sum Pairs/main.js
@@ -9,3 +9,36 @@ Return the maximum number of operations you can perform on the array.
 */
 
 // solution
+
+// var maxOperations = function (nums, k) {
+//     let map = {};
+//     let count = 0;
+
+//     for (const num of nums) {
+//         const exp = k - num;
+//         if (map[exp] && map[exp] > 0) {
+//             count++;
+//             map[exp]--;
+//         } else {
+//             map[num] ? map[num]++ : (map[num] = 1);
+//         }
+//     }
+//     return count;
+// };
+
+var maxOperations = function (nums, k) {
+    let map = new Map();
+    let count = 0;
+
+    for (const num of nums) {
+        const exp = k - num;
+        if (map.has(exp) && map.get(exp) > 0) {
+            count++;
+            map.set(exp, map.get(exp) - 1);
+        } else {
+            map.set(num, (map.get(num) || 0) + 1);
+        }
+    }
+
+    return count;
+};
