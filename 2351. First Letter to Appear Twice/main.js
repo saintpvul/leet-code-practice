23972b4a3140c56683dc0b7bba3b1d3e477d1431
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-19 23:02:32 +0600

    feat: complete 2351. first letter to appear twice

--- 2351. First Letter to Appear Twice/main.js
+++ 2351. First Letter to Appear Twice/main.js
@@ -10,3 +10,15 @@ s will contain at least one letter that appears twice.
 */
 
 // solution
+
+var repeatedCharacter = function (s) {
+    let map = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
+            return s[i];
+        } else {
+            map.set(s[i], 1);
+        }
+    }
+};
