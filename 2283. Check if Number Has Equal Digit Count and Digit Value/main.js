8a5ac0ad16c0d84f4fadef0f7ac93128b2c5397e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-19 23:27:37 +0600

    feat: complete 2283. check if number has equal digit count and digit value

--- 2283. Check if Number Has Equal Digit Count and Digit Value/main.js
+++ 2283. Check if Number Has Equal Digit Count and Digit Value/main.js
@@ -7,3 +7,22 @@ Return true if for every index i in the range 0 <= i < n, the digit i occurs num
 */
 
 // solution
+
+var digitCount = function (num) {
+    for (let i = 0; i < num.length; i++) {
+        const count = Number(num[i]);
+        let digitCount = 0;
+
+        for (let j = 0; j < num.length; j++) {
+            if (Number(num[j]) === i) {
+                digitCount++;
+            }
+        }
+
+        if (digitCount !== count) {
+            return false;
+        }
+    }
+
+    return true;
+};
