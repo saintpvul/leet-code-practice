63a2515de92e068b1312f2347854c096403a85fe
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-19 22:59:17 +0600

    feat: complete 1941. check if all characters have equal number of occurences

--- 1941. Check if All Characters Have Equal Number of Occurrences/main.js
+++ 1941. Check if All Characters Have Equal Number of Occurrences/main.js
@@ -7,3 +7,45 @@ A string s is good if all the characters that appear in s have the same number o
 */
 
 // solution
+
+// var areOccurrencesEqual = function (s) {
+//     let map = new Map();
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (map.has(s[i])) {
+//             map.set(s[i], map.get(s[i]) + 1);
+//         } else {
+//             map.set(s[i], 1);
+//         }
+//     }
+//     const values = Array.from(map.values());
+//     const firstValue = values[0];
+
+//     for (let i = 0; i < values.length; i++) {
+//         if (values[i] !== firstValue) {
+//             return false;
+//         }
+//     }
+//     return true;
+// };
+
+var areOccurrencesEqual = function (s) {
+    let map = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
+            map.set(s[i], map.get(s[i]) + 1);
+        } else {
+            map.set(s[i], 1);
+        }
+    }
+    const expectedValue = map.values().next().value;
+
+    for (const value of map.values()) {
+        if (value !== expectedValue) {
+            return false;
+        }
+    }
+
+    return true;
+};
