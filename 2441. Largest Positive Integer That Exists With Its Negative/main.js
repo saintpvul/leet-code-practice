04ffdef51ae9fe0f931780ec5436f9805d41e51d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-20 23:24:52 +0600

    feat: complete 2441. largest positive integer that exists with its negative

--- 2441. Largest Positive Integer That Exists With Its Negative/main.js
+++ 2441. Largest Positive Integer That Exists With Its Negative/main.js
@@ -7,3 +7,23 @@ Return the positive integer k. If there is no such integer, return -1.
 */
 
 // solution
+
+var findMaxK = function (nums) {
+    let ints = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === Math.abs(nums[i]) && !ints.has(nums[i])) {
+            ints.set(nums[i], true);
+        }
+    }
+    let max = -1;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === Math.abs(nums[i])) {
+            continue;
+        }
+        if (ints.has(Math.abs(nums[i])) && Math.abs(nums[i]) > max) {
+            max = Math.abs(nums[i]);
+        }
+    }
+    return max;
+};
