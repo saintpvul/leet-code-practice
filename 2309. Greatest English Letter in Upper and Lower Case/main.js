c82ccdd165b27d6ad7babe5ac59fb65f6a78bb04
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-20 21:54:37 +0600

    feat: complete 2309. greatest english letter in upper and lower case

--- 2309. Greatest English Letter in Upper and Lower Case/main.js
+++ 2309. Greatest English Letter in Upper and Lower Case/main.js
@@ -7,3 +7,26 @@ An English letter b is greater than another letter a if b appears after a in the
 */
 
 // solution
+
+var greatestLetter = function (s) {
+    let map = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (!map.has(char)) {
+            map.set(char, true);
+        }
+    }
+
+    for (let i = 25; i >= 0; i--) {
+        let ch1 = String.fromCharCode(97 + i);
+        let ch2 = String.fromCharCode(65 + i);
+
+        if (map.has(ch1) && map.has(ch2)) {
+            return ch2;
+        }
+    }
+
+    return "";
+};
