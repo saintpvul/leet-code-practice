22a4eff8da7217710e3e28b576e326a9c6d2b78a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-20 23:31:50 +0600

    feat: complete 1160. find words that can be formed by characters

--- 1160. Find Words That Can Be Formed by Characters/main.js
+++ 1160. Find Words That Can Be Formed by Characters/main.js
@@ -10,3 +10,35 @@ Return the sum of lengths of all good strings in words.
 */
 
 // solution
+
+var countCharacters = function (words, chars) {
+    const charsCount = new Map();
+
+    for (const char of chars) {
+        charsCount.set(char, (charsCount.get(char) || 0) + 1);
+    }
+
+    let counter = 0;
+
+    for (const word of words) {
+        const wordCount = new Map();
+        let isValid = true;
+
+        for (const char of word) {
+            wordCount.set(char, (wordCount.get(char) || 0) + 1);
+            if (
+                !charsCount.has(char) ||
+                wordCount.get(char) > charsCount.get(char)
+            ) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid) {
+            counter += word.length;
+        }
+    }
+
+    return counter;
+};
