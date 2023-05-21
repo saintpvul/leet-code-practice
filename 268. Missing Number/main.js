2ae2c5cffae8c55652b98422c05611d296fbebc5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-22 00:46:04 +0600

    feat: complete 268. missing number

--- 268. Missing Number/main.js
+++ 268. Missing Number/main.js
@@ -5,3 +5,35 @@ Given an array nums containing n distinct numbers in the range [0, n], return th
 */
 
 // solution
+
+// using hashMap
+
+// var missingNumber = function (nums) {
+//     const numbers = new Set(
+//         Array.from({ length: nums.length + 1 }, (_, idx) => idx)
+//     );
+//     for (const num of nums) {
+//         numbers.delete(num);
+//     }
+//     return numbers.keys().next().value;
+// };
+//or
+
+var missingNumber = function (nums) {
+    const numbers = new Set(nums);
+
+    for (let i = 0; i <= nums.length; i++) {
+        if (!numbers.has(i)) {
+            return i;
+        }
+    }
+};
+
+//the fastes way
+
+// var missingNumber = function (nums) {
+//     const n = nums.length;
+//     const expected = (n * (n + 1)) / 2;
+//     const actual = nums.reduce((sum, num) => sum + num, 0);
+//     return expected - actual;
+// };
