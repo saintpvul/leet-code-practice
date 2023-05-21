d6eca9f3546f9ded7dbc51fb6e88b8998472110a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-21 22:38:21 +0600

    feat: complete 884. uncommon words from two sentences

--- 884. Uncommon Words from Two Sentences/main.js
+++ 884. Uncommon Words from Two Sentences/main.js
@@ -9,3 +9,24 @@ Given two sentences s1 and s2, return a list of all the uncommon words. You may 
 */
 
 // solution
+
+var uncommonFromSentences = function (s1, s2) {
+    const words = new Map();
+    const uncommon = [];
+
+    s1.split(" ").forEach((word) => {
+        words.set(word, (words.get(word) || 0) + 1);
+    });
+
+    s2.split(" ").forEach((word) => {
+        words.set(word, (words.get(word) || 0) + 1);
+    });
+
+    for (const [word, count] of words.entries()) {
+        if (count === 1) {
+            uncommon.push(word);
+        }
+    }
+
+    return uncommon;
+};
