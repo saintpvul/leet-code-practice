8b341a8ffa575659fd911863f16e0ad29edfd1a6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-22 00:33:40 +0600

    feat: complete 1763. longest nice substring

--- 1763. Longest Nice Substring/main.js
+++ 1763. Longest Nice Substring/main.js
@@ -7,3 +7,38 @@ Given a string s, return the longest substring of s that is nice. If there are m
 */
 
 // solution
+
+var longestNiceSubstring = function (s) {
+    let longestNice = "";
+
+    const isNice = (charSet) => {
+        for (char of charSet) {
+            if (
+                !charSet.has(char.toLowerCase()) ||
+                !charSet.has(char.toUpperCase())
+            ) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const charSet = new Set();
+        for (let j = i; j < s.length; j++) {
+            const char = s[j];
+
+            charSet.add(char);
+
+            if (isNice(charSet)) {
+                const substring = s.substring(i, j + 1);
+                if (substring.length > longestNice.length) {
+                    longestNice = substring;
+                }
+            }
+        }
+    }
+
+    return longestNice;
+};
+Console;
