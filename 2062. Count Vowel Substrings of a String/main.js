f626ce5c6069a8fbb2b21060ea762bd0cdab4e16
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-21 22:59:08 +0600

    feat: complete 2062. count vowel substrings of a string

--- 2062. Count Vowel Substrings of a String/main.js
+++ 2062. Count Vowel Substrings of a String/main.js
@@ -9,3 +9,25 @@ Given a string word, return the number of vowel substrings in word.
 */
 
 // solution
+
+var countVowelSubstrings = function (word) {
+    const isVowel = (char) => ["a", "e", "i", "o", "u"].includes(char);
+    let count = 0;
+    for (let i = 0; i < word.length; i++) {
+        let vowels = new Set();
+
+        for (let j = i; j < word.length; j++) {
+            const char = word[j];
+
+            if (isVowel(char)) {
+                vowels.add(char);
+                if (vowels.size >= 5) {
+                    count++;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    return count;
+};
