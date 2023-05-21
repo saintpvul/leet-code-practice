f9df3890930e4d7d896808a0fa17b5c3242c6b29
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-22 01:26:35 +0600

    feat: complete 242. valid anagram

--- 242. Valid Anagram/main.js
+++ 242. Valid Anagram/main.js
@@ -7,3 +7,17 @@ An Anagram is a word or phrase formed by rearranging the letters of a different 
 */
 
 // solution
+
+var isAnagram = function (s, t) {
+    const letters = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        letters.set(s[i], (letters.get(s[i]) || 0) + 1);
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        letters.set(t[i], (letters.get(t[i]) || 0) - 1);
+    }
+
+    return Array.from(letters.entries()).every((e) => e[1] === 0);
+};
