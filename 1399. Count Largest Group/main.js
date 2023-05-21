caac1b08b5e6d9b4759cd6ac315e87c0110ff0dc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-21 21:46:23 +0600

    feat: complete 1399. count largest group

--- 1399. Count Largest Group/main.js
+++ 1399. Count Largest Group/main.js
@@ -10,15 +10,41 @@ Return the number of groups that have the largest size.
 
 // solution
 
+// var countLargestGroup = function (n) {
+//     let groups = {};
+//     for (let i = 1; i <= n; i++) {
+//         let sum = i
+//             .toString()
+//             .split("")
+//             .reduce((s, v) => (s += +v), 0);
+//         groups[sum] ? groups[sum]++ : (groups[sum] = 1);
+//     }
+//     const largest = Math.max(...Object.values(groups));
+//     return Object.values(groups).filter((e) => e === largest).length;
+// };
+
+// less memory usage
+
 var countLargestGroup = function (n) {
     let groups = {};
+    let maxCount = 0;
+
     for (let i = 1; i <= n; i++) {
         let sum = i
             .toString()
             .split("")
             .reduce((s, v) => (s += +v), 0);
-        groups[sum] ? groups[sum]++ : (groups[sum] = 1);
+        groups[sum] = (groups[sum] || 0) + 1;
+        maxCount = Math.max(maxCount, groups[sum]);
     }
-    const largest = Math.max(...Object.values(groups));
-    return Object.values(groups).filter((e) => e === largest).length;
+
+    let count = 0;
+    for (let key in groups) {
+        if (groups[key] === maxCount) {
+            count++;
+        }
+    }
+
+    return count;
 };
+Console;
