238da52d99abacf2e21fb6311388dcd0cbb13c4f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-21 23:39:23 +0600

    feat: complete 1394. find lucky integer in an array

--- 1394. Find Lucky Integer in an Array/main.js
+++ 1394. Find Lucky Integer in an Array/main.js
@@ -7,3 +7,23 @@ Return the largest lucky integer in the array. If there is no lucky integer retu
 */
 
 // solution
+
+var findLucky = function (arr) {
+    let freq = new Map();
+
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+
+        freq.set(num, (freq.get(num) || 0) + 1);
+    }
+
+    let largest = -1;
+
+    for (const [key, value] of freq.entries()) {
+        if (key === value) {
+            largest = Math.max(largest, key);
+        }
+    }
+
+    return largest;
+};
