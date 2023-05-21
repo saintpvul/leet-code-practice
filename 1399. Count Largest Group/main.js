a53ecf68341c0bf21b56a4a869d4d95ecb9e7b04
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-21 21:34:30 +0600

    feat: start 1399. count largest group

---
+++ 1399. Count Largest Group/main.js
@@ -0,0 +1,11 @@
+/*
+
+You are given an integer n.
+
+Each number from 1 to n is grouped according to the sum of its digits.
+
+Return the number of groups that have the largest size.
+
+*/
+
+// solution
