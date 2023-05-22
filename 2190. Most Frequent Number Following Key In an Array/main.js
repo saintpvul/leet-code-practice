213eafc15c10ad77fbcf921ac46a92f56327dae0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-23 00:38:59 +0600

    feat: complete 2190. most frequent number following key in an array

--- 2190. Most Frequent Number Following Key In an Array/main.js
+++ 2190. Most Frequent Number Following Key In an Array/main.js
@@ -14,3 +14,51 @@ Return the target with the maximum count. The test cases will be generated such 
 */
 
 // solution
+
+// two innerloops is too slow
+
+// var mostFrequent = function (nums, key) {
+//     const freq = new Map();
+//     const targets = new Set(nums);
+
+//     for (const target of targets) {
+//         for (let i = 0; i < nums.length - 1; i++) {
+//             if (nums[i] === key && nums[i + 1] === target) {
+//                 freq.set(target, (freq.get(target) || 0) + 1);
+//             }
+//         }
+//     }
+//     let maxCount = 0;
+//     let outputTarget = 0;
+//     for (const [target, counter] of freq.entries()) {
+//         if (counter > maxCount) {
+//             maxCount = counter;
+//             outputTarget = target;
+//         }
+//     }
+
+//     return outputTarget;
+// };
+
+//fastes way
+
+var mostFrequent = function (nums, key) {
+    const freq = new Map();
+    let maxCount = 0;
+    let maxTarget = 0;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === key) {
+            const target = nums[i + 1];
+            const count = (freq.get(target) || 0) + 1;
+            freq.set(target, count);
+
+            if (count > maxCount) {
+                maxCount = count;
+                maxTarget = target;
+            }
+        }
+    }
+
+    return maxTarget;
+};
