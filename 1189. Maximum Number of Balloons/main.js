38983f5bb1ada274242b4cd68f0c5360659900ac
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-23 01:03:34 +0600

    feat: complete 1189. maximum number of balloons

--- 1189. Maximum Number of Balloons/main.js
+++ 1189. Maximum Number of Balloons/main.js
@@ -7,3 +7,25 @@ You can use each character in text at most once. Return the maximum number of in
 */
 
 // solution
+
+var maxNumberOfBalloons = function (text) {
+    const balloon = ["b", "a", "l", "o", "n"];
+    const countBalloons = new Map(balloon.map((key) => [key, 0]));
+
+    for (const char of text) {
+        if (countBalloons.has(char)) {
+            countBalloons.set(char, countBalloons.get(char) + 1);
+        }
+    }
+
+    let maxInstances = Infinity;
+    for (const [key, count] of countBalloons.entries()) {
+        if (key === "l" || key === "o") {
+            maxInstances = Math.min(maxInstances, Math.floor(count / 2));
+        } else {
+            maxInstances = Math.min(maxInstances, count);
+        }
+    }
+
+    return maxInstances;
+};
