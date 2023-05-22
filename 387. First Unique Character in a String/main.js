cb2adb89589a174df6dce907324991f02ae73c5f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-22 23:56:46 +0600

    feat: complete 387. first unique character in a string

--- 387. First Unique Character in a String/main.js
+++ 387. First Unique Character in a String/main.js
@@ -5,3 +5,43 @@ Given a string s, find the first non-repeating character in it and return its in
 */
 
 // solution
+
+/*
+
+//faster way
+
+var firstUniqChar = function(s) {
+    const chars = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        chars[char] = (chars[char] || 0) + 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (chars[char] === 1) {
+            return i;
+        }
+    }
+
+    return -1;
+};
+
+*/
+
+//using hashmap
+
+var firstUniqChar = function (s) {
+    const chars = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        chars.has(char) ? chars.get(char).push(i) : chars.set(char, [i]);
+    }
+
+    for (const [letter, idx] of chars.entries()) {
+        if (idx.length === 1) return idx[0];
+    }
+    return -1;
+};
