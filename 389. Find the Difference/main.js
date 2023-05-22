569f39c89f1bf4b5f67095f9d152eaa162b7042b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-22 23:38:50 +0600

    feat: complete 389. find the difference

--- 389. Find the Difference/main.js
+++ 389. Find the Difference/main.js
@@ -9,3 +9,19 @@ Return the letter that was added to t.
 */
 
 // solution
+
+var findTheDifference = function (s, t) {
+    const chars = new Map();
+    for (let i = 0; i < t.length; i++) {
+        const char = t[i];
+        chars.set(char, (chars.get(char) || 0) + 1);
+    }
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        chars.set(char, chars.get(char) - 1);
+        if (chars.get(char) === 0) {
+            chars.delete(char);
+        }
+    }
+    return chars.keys().next().value;
+};
