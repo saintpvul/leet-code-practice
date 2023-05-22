8dfadc6d15ec6294286c2fadaf7b02916c5d9620
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-23 00:47:15 +0600

    feat: complete 448. find all numbers disappeared in an array

--- 448. Find All Numbers Disappeared in an Array/main.js
+++ 448. Find All Numbers Disappeared in an Array/main.js
@@ -5,3 +5,15 @@ Given an array nums of n integers where nums[i] is in the range [1, n], return a
 */
 
 // solution
+
+var findDisappearedNumbers = function (nums) {
+    const appeared = new Set(nums);
+    const disappeared = [];
+
+    for (let i = 1; i < nums.length + 1; i++) {
+        if (!appeared.has(i)) {
+            disappeared.push(i);
+        }
+    }
+    return disappeared;
+};
