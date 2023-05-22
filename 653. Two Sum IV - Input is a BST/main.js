ac7228f470b31379f1db122b7d4511af52682829
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-23 01:05:12 +0600

    feat: start 653. two sum iv - input is a bst

---
+++ 653. Two Sum IV - Input is a BST/main.js
@@ -0,0 +1,7 @@
+/*
+
+Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
+
+*/
+
+// solution
