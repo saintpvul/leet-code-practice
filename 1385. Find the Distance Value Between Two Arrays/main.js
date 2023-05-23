7655b0bb35268b87623c00399aeee88d6bef3b06
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-23 22:59:02 +0600

    feat: complete 1385. find the distance value between two arrays

--- 1385. Find the Distance Value Between Two Arrays/main.js
+++ 1385. Find the Distance Value Between Two Arrays/main.js
@@ -7,3 +7,56 @@ The distance value is defined as the number of elements arr1[i] such that there 
 */
 
 // solution
+
+// using treeNode
+
+var findTheDistanceValue = function (arr1, arr2, d) {
+    class TreeNode {
+        constructor(val) {
+            this.val = val;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    const insertNode = (root, val) => {
+        if (root === null) return new TreeNode(val);
+
+        if (val < root.val) {
+            root.left = insertNode(root.left, val);
+        } else {
+            root.right = insertNode(root.right, val);
+        }
+
+        return root;
+    };
+
+    const findMin = (root, target) => {
+        let min = Infinity;
+
+        while (root !== null) {
+            if (root.val >= target) {
+                min = Math.min(min, root.val);
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return min;
+    };
+
+    let distance = 0;
+    let bst = null;
+
+    for (let i = 0; i < arr2.length; i++) {
+        bst = insertNode(bst, arr2[i]);
+    }
+
+    for (let i = 0; i < arr1.length; i++) {
+        const min = findMin(bst, arr1[i] - d);
+        if (min === Infinity || min > arr1[i] + d) {
+            distance++;
+        }
+    }
+    return distance;
+};
