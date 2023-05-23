281f01eb9aef928b5d92c9ec0594bfabd5fcfe37
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-24 01:18:18 +0600

    feat: complete 374. guess number higher or lower

--- 374. Guess Number Higher or Lower/main.js
+++ 374. Guess Number Higher or Lower/main.js
@@ -16,3 +16,39 @@ Return the number that I picked.
 */
 
 // solution
+
+// var guessNumber = function (n) {
+//     let currentGuess = n;
+//     let min = 1;
+//     let max = n;
+//     while (min <= max) {
+//         currentGuess = Math.floor(min + (max - min) / 2);
+//         if (guess(currentGuess) === 0) {
+//             return currentGuess;
+//         }
+//         if (guess(currentGuess) < 0) {
+//             max = currentGuess - 1;
+//         } else {
+//             min = currentGuess + 1;
+//         }
+//     }
+// };
+// optimized
+
+var guessNumber = function (n) {
+    let min = 1;
+    let max = n;
+    while (min <= max) {
+        let pivot = Math.floor(min + (max - min) / 2);
+        let current = guess(pivot);
+        if (current === 0) {
+            return pivot;
+        }
+        if (current < 0) {
+            max = pivot - 1;
+        } else {
+            min = pivot + 1;
+        }
+    }
+    return -1;
+};
