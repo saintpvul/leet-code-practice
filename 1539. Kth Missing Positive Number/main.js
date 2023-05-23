96d1a3e7f35ac2c93cedbb9f77a1016070c03e7e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-24 00:08:04 +0600

    feat: complete 1539. kth missing positive number

--- 1539. Kth Missing Positive Number/main.js
+++ 1539. Kth Missing Positive Number/main.js
@@ -7,3 +7,18 @@ Return the kth positive integer that is missing from this array.
 */
 
 // solution
+
+var findKthPositive = function (arr, k) {
+    let left = 0,
+        right = arr.length - 1;
+    while (left <= right) {
+        const pivot = Math.floor(left + (right - left) / 2);
+        if (arr[pivot] - pivot - 1 < k) {
+            left = pivot + 1;
+        } else {
+            right = pivot - 1;
+        }
+    }
+
+    return left + k;
+};
