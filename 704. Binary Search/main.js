6b9ce47b21d6ad29c120f0528a8e410b53a90d78
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-24 01:04:32 +0600

    update: complete 704. binary search

--- 704. Binary Search/main.js
+++ 704. Binary Search/main.js
@@ -8,20 +8,38 @@ You must write an algorithm with O(log n) runtime complexity.
 
 // solution
 
+// var search = function (nums, target) {
+//     let left = 0,
+//         right = nums.length - 1;
+
+//     while (left <= right) {
+//         mid = Math.floor((left + right) / 2);
+
+//         if (nums[mid] === target) {
+//             return mid;
+//         }
+//         if (nums[mid] < target) {
+//             left = mid + 1;
+//         } else {
+//             right = mid - 1;
+//         }
+//     }
+//     return -1;
+// };
+//a bit faster way
+
 var search = function (nums, target) {
-    let left = 0,
-        right = nums.length - 1;
+    let left = 0;
+    let right = nums.length - 1;
 
     while (left <= right) {
-        mid = Math.floor((left + right) / 2);
-
-        if (nums[mid] === target) {
-            return mid;
-        }
-        if (nums[mid] < target) {
-            left = mid + 1;
+        let pivot = Math.floor(left + (right - left) / 2);
+        if (nums[pivot] === target) {
+            return pivot;
+        } else if (nums[pivot] < target) {
+            left = pivot + 1;
         } else {
-            right = mid - 1;
+            right = pivot - 1;
         }
     }
     return -1;
