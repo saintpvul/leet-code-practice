18841a8622b85a5e21c2d8f34fb80ecff7430950
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-23 23:50:35 +0600

    feat: start 1539. kth missing positive number

---
+++ 1539. Kth Missing Positive Number/main.js
@@ -0,0 +1,9 @@
+/*
+
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+
+Return the kth positive integer that is missing from this array.
+
+*/
+
+// solution
