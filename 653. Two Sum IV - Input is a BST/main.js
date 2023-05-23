08e761cf4956fe1d995d5c6d57b2efad27fbda6e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-23 22:36:38 +0600

    feat: complete 653. two sum iv - input is a bst

--- 653. Two Sum IV - Input is a BST/main.js
+++ 653. Two Sum IV - Input is a BST/main.js
@@ -5,3 +5,20 @@ Given the root of a binary search tree and an integer k, return true if there ex
 */
 
 // solution
+
+var findTarget = function (root, k) {
+    const visited = new Set();
+
+    const findSum = (node, target) => {
+        if (node === null) return false;
+
+        const diff = target - node.val;
+
+        if (visited.has(diff)) return true;
+
+        visited.add(node.val);
+
+        return findSum(node.left, target) || findSum(node.right, target);
+    };
+    return findSum(root, k);
+};
