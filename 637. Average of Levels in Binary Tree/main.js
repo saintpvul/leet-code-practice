e73f40bcd3a5b46b62e45fd876fc2050cefee11a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-26 01:26:10 +0600

    feat: complete 637. averange of levels in binary tree

--- 637. Average of Levels in Binary Tree/main.js
+++ 637. Average of Levels in Binary Tree/main.js
@@ -5,3 +5,32 @@ Given the root of a binary tree, return the average value of the nodes on each l
 */
 
 // solution
+
+var averageOfLevels = function (root) {
+    const res = [];
+    const queue = [root];
+    let index = 0;
+
+    while (index < queue.length) {
+        const queueLength = queue.length - index;
+        let levelSum = 0;
+
+        for (let i = 0; i < queueLength; i++) {
+            const currNode = queue[index + i];
+            levelSum += currNode.val;
+
+            if (currNode.left) {
+                queue.push(currNode.left);
+            }
+
+            if (currNode.right) {
+                queue.push(currNode.right);
+            }
+        }
+
+        res.push(levelSum / queueLength);
+        index += queueLength;
+    }
+
+    return res;
+};
