ab4ba5293cffb317dbb8ec5942177fba7b34fbb7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-25 23:18:35 +0600

    feat: complete 367. valid perfect square

--- 367. Valid Perfect Square/main.js
+++ 367. Valid Perfect Square/main.js
@@ -9,3 +9,29 @@ You must not use any built-in library function, such as sqrt.
 */
 
 // solution
+
+// banned but also works
+
+/*
+var isPerfectSquare = function(num) {
+    return Math.sqrt(num) === parseInt(Math.sqrt(num))
+};
+*/
+
+var isPerfectSquare = function (num) {
+    if (num < 2) return true;
+    let min = 2;
+    let max = Math.floor(num / 2);
+
+    while (min <= max) {
+        const pivot = min + Math.floor((max - min) / 2);
+        const sqr = pivot ** 2;
+        if (sqr === num) return pivot;
+        if (sqr > num) {
+            max = pivot - 1;
+        } else {
+            min = pivot + 1;
+        }
+    }
+    return false;
+};
