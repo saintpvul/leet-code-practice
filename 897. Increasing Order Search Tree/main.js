837dcf3a0eac1383d7f3fb9499c9cc983e29060d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-26 00:16:46 +0600

    feat: complete 897. increasing order search tree

--- 897. Increasing Order Search Tree/main.js
+++ 897. Increasing Order Search Tree/main.js
@@ -5,3 +5,23 @@ Given the root of a binary search tree, rearrange the tree in in-order so that t
 */
 
 // solution
+
+var increasingBST = function (root) {
+    const newNode = new TreeNode();
+    let current = newNode;
+
+    function inorder(node) {
+        if (!node) {
+            return;
+        }
+        inorder(node.left);
+
+        current.right = new TreeNode(node.val);
+        current = current.right;
+
+        inorder(node.right);
+    }
+
+    inorder(root);
+    return newNode.right;
+};
