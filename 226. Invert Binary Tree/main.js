dc81957da2d194fd7adf39c338fe1bcceb584f3a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-26 01:14:22 +0600

    feat: complete 226. invert binary tree

--- 226. Invert Binary Tree/main.js
+++ 226. Invert Binary Tree/main.js
@@ -5,3 +5,18 @@ Given the root of a binary tree, invert the tree, and return its root.
 */
 
 // solution
+
+var invertTree = function (root) {
+    if (!root) {
+        return null;
+    }
+
+    let temp = root.left;
+    root.left = root.right;
+    root.right = temp;
+
+    invertTree(root.left);
+    invertTree(root.right);
+
+    return root;
+};
