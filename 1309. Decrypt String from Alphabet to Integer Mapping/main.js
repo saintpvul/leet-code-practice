0f4b0b4aff6c691636e4cf86dc2a1bc918194537
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-27 22:03:07 +0600

    feat: start 1309. decrypt string from alphabet to integer mapping

---
+++ 1309. Decrypt String from Alphabet to Integer Mapping/main.js
@@ -0,0 +1,13 @@
+/*
+
+You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+Return the string formed after mapping.
+
+The test cases are generated so that a unique mapping will always exist.
+
+*/
+
+// solution
