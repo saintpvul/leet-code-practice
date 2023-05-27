32dc0e862df623ce0988b5c17915442b2b2644ca
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-27 23:22:40 +0600

    feat: complete 2678. number of senior citizens

--- 2678. Number of Senior Citizens/main.js
+++ 2678. Number of Senior Citizens/main.js
@@ -11,3 +11,12 @@ Return the number of passengers who are strictly more than 60 years old.
 */
 
 // solution
+
+var countSeniors = function (details) {
+    let count = 0;
+    for (let i = 0; i < details.length; i++) {
+        const currentAge = details[i].slice(-4, -2);
+        if (currentAge > 60) count++;
+    }
+    return count;
+};
