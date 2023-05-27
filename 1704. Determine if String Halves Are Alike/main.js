24a1e21cccf3e561b6f36a309ae16fe9baf4d8c2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-27 23:44:20 +0600

    feat: complete 1704. determine if string halves are alike

--- 1704. Determine if String Halves Are Alike/main.js
+++ 1704. Determine if String Halves Are Alike/main.js
@@ -9,3 +9,28 @@ Return true if a and b are alike. Otherwise, return false.
 */
 
 // solution
+
+var halvesAreAlike = function (s) {
+    const half = s.length / 2;
+    const first = s.slice(0, half);
+    const second = s.slice(half);
+
+    const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+    let counter = [0, 0];
+
+    for (let i = 0; i < first.length; i++) {
+        const char = first[i];
+        if (vowels.includes(char)) {
+            counter[0]++;
+        }
+    }
+
+    for (let i = 0; i < second.length; i++) {
+        const char = second[i];
+        if (vowels.includes(char)) {
+            counter[1]++;
+        }
+    }
+    return counter[0] === counter[1];
+};
