bd526b2cca47f96373d77cd7e38226ebdc3b302a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-27 21:54:09 +0600

    feat: complete 1967. number of strings that appear as substrings in word

--- 1967. Number of Strings That Appear as Substrings in Word/main.js
+++ 1967. Number of Strings That Appear as Substrings in Word/main.js
@@ -7,3 +7,20 @@ A substring is a contiguous sequence of characters within a string.
 */
 
 // solution
+
+// var numOfStrings = function (patterns, word) {
+//     return patterns.reduce(
+//         (count, element) =>
+//             word.includes(element) ? (count += 1) : (count += 0),
+//         0
+//     );
+// };
+
+var numOfStrings = function (patterns, word) {
+    let count = 0;
+    for (let i = 0; i < patterns.length; i++) {
+        const pattern = patterns[i];
+        if (word.includes(pattern)) count++;
+    }
+    return count;
+};
