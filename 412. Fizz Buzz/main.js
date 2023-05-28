51e2dcb2a074126cc581a5320a2392b2e39e28e4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-28 22:46:41 +0600

    feat: complete 412. fizz buzz

--- 412. Fizz Buzz/main.js
+++ 412. Fizz Buzz/main.js
@@ -10,3 +10,19 @@ answer[i] == i (as a string) if none of the above conditions are true.
 */
 
 // solution
+
+var fizzBuzz = function (n) {
+    let res = [];
+    for (let i = 1; i <= n; i++) {
+        if (!(i % 5) && !(i % 3)) {
+            res.push("FizzBuzz");
+        } else if (!(i % 3)) {
+            res.push("Fizz");
+        } else if (!(i % 5)) {
+            res.push("Buzz");
+        } else {
+            res.push(i.toString());
+        }
+    }
+    return res;
+};
