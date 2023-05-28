32e4383eb7bef7dab8adad1da956641f4bbdcc4c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-29 00:47:44 +0600

    feat: start 1974. minimum time to type word using special typewriter

---
+++ 1974. Minimum Time to Type Word Using Special Typewriter/main.js
@@ -0,0 +1,14 @@
+/*
+
+There is a special typewriter with lowercase English letters 'a' to 'z' arranged in a circle with a pointer. A character can only be typed if the pointer is pointing to that character. The pointer is initially pointing to the character 'a'.
+
+
+Each second, you may perform one of the following operations:
+
+Move the pointer one character counterclockwise or clockwise.
+Type the character the pointer is currently on.
+Given a string word, return the minimum number of seconds to type out the characters in word.
+
+*/
+
+// solution
