212895866c4ea7e217ddd648f37e320fb4de19b6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-29 01:24:05 +0600

    feat: complete 1047. remove all adjecent duplicates in string

--- 1047. Remove All Adjacent Duplicates In String/main.js
+++ 1047. Remove All Adjacent Duplicates In String/main.js
@@ -9,3 +9,18 @@ Return the final string after all such duplicate removals have been made. It can
 */
 
 // solution
+
+var removeDuplicates = function (s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join("");
+};
