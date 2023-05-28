feafbb8502c3c80b52c4c724430ea450d04abc34
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-28 22:56:33 +0600

    feat: complete 2124. check if all a's appears before all b's

--- 2124. Check if All A's Appears Before All B's/main.js
+++ 2124. Check if All A's Appears Before All B's/main.js
@@ -5,3 +5,25 @@ Given a string s consisting of only the characters 'a' and 'b', return true if e
 */
 
 // solution
+
+// var checkString = function (s) {
+//     const a = s.lastIndexOf("a");
+//     const b = s.indexOf("b");
+//     if (a >= 0 && b < 0) {
+//         return true;
+//     }
+//     return a < b;
+// };
+
+var checkString = function (s) {
+    let appears = false;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "b") {
+            appears = true;
+        }
+        if (s[i] === "a" && appears) {
+            return false;
+        }
+    }
+    return true;
+};
