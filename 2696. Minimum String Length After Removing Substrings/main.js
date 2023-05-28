18b14c1b557f1ac15c2c2f1a5998e1d320243557
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-29 01:37:42 +0600

    feat: complete 2696. minimum string length after removing substrings

--- 2696. Minimum String Length After Removing Substrings/main.js
+++ 2696. Minimum String Length After Removing Substrings/main.js
@@ -11,3 +11,19 @@ Note that the string concatenates after removing the substring and could produce
 */
 
 // solution
+
+var minLength = function (s) {
+    let stack = [];
+    for (let i = 0; i < s.length; i++) {
+        if (
+            stack.length > 0 &&
+            ((stack[stack.length - 1] === "A" && s[i] === "B") ||
+                (stack[stack.length - 1] === "C" && s[i] === "D"))
+        ) {
+            stack.pop();
+        } else {
+            stack.push(s[i]);
+        }
+    }
+    return stack.length;
+};
