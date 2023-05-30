60517fd7ecb3d7b94cc2a87d5b62f191bfd1b0cb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-31 01:30:17 +0600

    feat: complete 144. binary tree preorder traversal

--- 144. Binary Tree Preorder Traversal/main.js
+++ 144. Binary Tree Preorder Traversal/main.js
@@ -5,3 +5,15 @@ Given the root of a binary tree, return the preorder traversal of its nodes' val
 */
 
 // solution
+
+var preorderTraversal = function (root) {
+    let res = [];
+    const check = (node) => {
+        if (!node) return;
+        res.push(node.val);
+        check(node.left);
+        check(node.right);
+    };
+    check(root);
+    return res;
+};
