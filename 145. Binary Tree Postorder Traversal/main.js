642cd8b7622c364d18980a70ec6ce8e77df7e71a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-31 01:19:04 +0600

    feat: complete 145. binary tree postorder traversal

--- 145. Binary Tree Postorder Traversal/main.js
+++ 145. Binary Tree Postorder Traversal/main.js
@@ -5,3 +5,15 @@ Given the root of a binary tree, return the postorder traversal of its nodes' va
 */
 
 // solution
+
+var postorderTraversal = function (root) {
+    let res = [];
+    const check = (node) => {
+        if (!node) return;
+        check(node.left);
+        check(node.right);
+        res.push(node.val);
+    };
+    check(root);
+    return res;
+};
