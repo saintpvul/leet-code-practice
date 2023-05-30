dad532c398f9cfa4394a035001931e69e2af6eae
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-31 01:06:26 +0600

    feat: complete 965. univalued binary tree

--- 965. Univalued Binary Tree/main.js
+++ 965. Univalued Binary Tree/main.js
@@ -7,3 +7,12 @@ Given the root of a binary tree, return true if the given tree is uni-valued, or
 */
 
 // solution
+
+var isUnivalTree = function (root) {
+    const check = (node, v) => {
+        if (!node) return true;
+        if (node.val != v) return false;
+        return check(node.left, v) && check(node.right, v);
+    };
+    return check(root, root.val);
+};
