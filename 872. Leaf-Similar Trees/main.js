028daa35f7ec4736b68420ed5eeb3cfa9a4f123c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-05-31 01:46:16 +0600

    feat: complete 872. leaf-similar trees

--- 872. Leaf-Similar Trees/main.js
+++ 872. Leaf-Similar Trees/main.js
@@ -13,3 +13,26 @@ Return true if and only if the two given trees with head nodes root1 and root2 a
 */
 
 // solution
+
+var leafSimilar = function (root1, root2) {
+    const getLeaves = (node, leaves) => {
+        if (!node) return;
+        if (!node.left && !node.right) leaves.push(node.val);
+        getLeaves(node.left, leaves);
+        getLeaves(node.right, leaves);
+    };
+
+    let leaves1 = [];
+    let leaves2 = [];
+
+    getLeaves(root1, leaves1);
+    getLeaves(root2, leaves2);
+
+    if (leaves1.length != leaves2.length) return false;
+
+    for (let i = 0; i < leaves1.length; i++) {
+        if (leaves1[i] != leaves2[i]) return false;
+    }
+
+    return true;
+};
