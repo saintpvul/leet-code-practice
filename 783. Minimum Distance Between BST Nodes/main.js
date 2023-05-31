4d8a09b6b054ec08fc7034f03da9187d57772095
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-01 02:05:18 +0600

    feat: complete 783. minimum distance between bst nodes

--- 783. Minimum Distance Between BST Nodes/main.js
+++ 783. Minimum Distance Between BST Nodes/main.js
@@ -5,3 +5,50 @@ Given the root of a Binary Search Tree (BST), return the minimum difference betw
 */
 
 // solution
+
+// var minDiffInBST = function (root) {
+//     let values = [];
+//     const getVals = (node, arr) => {
+//         if (!node) return;
+
+//         arr.push(node.val);
+//         getVals(node.left, arr);
+//         getVals(node.right, arr);
+//     };
+//     getVals(root, values);
+
+//     values = values.sort((a, b) => a - b);
+
+//     let minDist = Infinity;
+
+//     for (let i = 0; i < values.length; i++) {
+//         let currentDist = Math.abs(values[i] - values[i + 1]);
+//         if (currentDist < minDist) {
+//             minDist = currentDist;
+//         }
+//     }
+//     return minDist;
+// };
+
+// optimized
+var minDiffInBST = function (root) {
+    let prev = null;
+    let minDist = Infinity;
+
+    const getVals = (node) => {
+        if (!node) return;
+
+        getVals(node.left);
+
+        if (prev !== null) {
+            minDist = Math.min(minDist, node.val - prev);
+        }
+        prev = node.val;
+
+        getVals(node.right);
+    };
+
+    getVals(root);
+
+    return minDist;
+};
