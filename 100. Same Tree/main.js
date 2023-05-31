fe39f37608f7857ccf671672208180e1573d8af3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-01 01:57:51 +0600

    feat: complete 100. same tree

--- 100. Same Tree/main.js
+++ 100. Same Tree/main.js
@@ -7,3 +7,28 @@ Two binary trees are considered the same if they are structurally identical, and
 */
 
 // solution
+
+var isSameTree = function (p, q) {
+    let pVals = [];
+    let qVals = [];
+
+    const check = (node, arr) => {
+        if (!node) {
+            arr.push(null);
+            return;
+        }
+
+        arr.push(node.val);
+        check(node.left, arr);
+        check(node.right, arr);
+    };
+
+    check(p, pVals);
+    check(q, qVals);
+    if (pVals.length !== qVals.length) return false;
+
+    for (let i = 0; i < pVals.length; i++) {
+        if (pVals[i] !== qVals[i]) return false;
+    }
+    return true;
+};
