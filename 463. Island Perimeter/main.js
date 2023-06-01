da6ad3adddf095dba3322aa333cedacdecc65f7f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-02 00:39:01 +0600

    feat: start 463. island perimeter

--- 463. Island Perimeter/main.js
+++ 463. Island Perimeter/main.js
@@ -6,6 +6,6 @@ Grid cells are connected horizontally/vertically (not diagonally). The grid is c
 
 The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
 
-/*
+*/
 
 // solution
