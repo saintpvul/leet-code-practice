c7228fae4fb6dae58f4258c173e07cf0308fc03a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-02 00:34:42 +0600

    feat: complete 530. minimum absolute difference in bst

--- 530. Minimum Absolute Difference in BST/main.js
+++ 530. Minimum Absolute Difference in BST/main.js
@@ -5,3 +5,26 @@ Given the root of a Binary Search Tree (BST), return the minimum absolute differ
 */
 
 // solution
+
+var getMinimumDifference = function (root) {
+    const inorderTraversal = (node, values) => {
+        if (!node) return;
+
+        inorderTraversal(node.left, values);
+        values.push(node.val);
+        inorderTraversal(node.right, values);
+    };
+
+    const values = [];
+    inorderTraversal(root, values);
+
+    values.sort((a, b) => a - b);
+
+    let minDiff = Infinity;
+    for (let i = 1; i < values.length; i++) {
+        const diff = values[i] - values[i - 1];
+        minDiff = Math.min(minDiff, diff);
+    }
+
+    return minDiff;
+};
