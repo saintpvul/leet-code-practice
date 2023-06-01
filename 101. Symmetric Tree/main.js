f2f162b680fbffa530918c5e9b1ebd61ccb26329
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-02 00:24:52 +0600

    feat: complete 101. symmetric tree

--- 101. Symmetric Tree/main.js
+++ 101. Symmetric Tree/main.js
@@ -5,3 +5,19 @@ Given the root of a binary tree, check whether it is a mirror of itself (i.e., s
 */
 
 // solution
+
+var isSymmetric = function (root) {
+    const mirrorCheck = (node1, node2) => {
+        if (!node1 && !node2) return true;
+
+        if (!node1 || !node2 || node1.val !== node2.val) {
+            return false;
+        }
+        return (
+            mirrorCheck(node1.left, node2.right) &&
+            mirrorCheck(node1.right, node2.left)
+        );
+    };
+
+    return mirrorCheck(root, root);
+};
