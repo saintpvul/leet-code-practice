a0cd4a8a9f2f3f62dbc7667718dc2ee8d197d261
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-01 23:59:49 +0600

    feat: complete 404. sum of left leaves

--- 404. Sum of Left Leaves/main.js
+++ 404. Sum of Left Leaves/main.js
@@ -7,3 +7,20 @@ A leaf is a node with no children. A left leaf is a leaf that is the left child 
 */
 
 // solution
+
+var sumOfLeftLeaves = function (root) {
+    let sum = 0;
+
+    let checkLeft = (node) => {
+        if (!node) return;
+
+        if (node.left && !node.left.left && !node.left.right) {
+            sum += node.left.val;
+        }
+
+        checkLeft(node.left);
+        checkLeft(node.right);
+    };
+    checkLeft(root);
+    return sum;
+};
