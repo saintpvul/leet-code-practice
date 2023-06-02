8e38ab43a11686eefc27f70476c8fe672f7e2b8f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-02 22:54:48 +0600

    feat: complete 1380. lucky numbers in a matrix

--- 1380. Lucky Numbers in a Matrix/main.js
+++ 1380. Lucky Numbers in a Matrix/main.js
@@ -7,3 +7,32 @@ A lucky number is an element of the matrix such that it is the minimum element i
 */
 
 // solution
+
+var luckyNumbers = function (matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+
+    const luckyNumbers = [];
+    for (let i = 0; i < m; i++) {
+        let minIndex = 0;
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] < matrix[i][minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        let maxInColumn = true;
+        for (let k = 0; k < m; k++) {
+            if (matrix[k][minIndex] > matrix[i][minIndex]) {
+                maxInColumn = false;
+                break;
+            }
+        }
+
+        if (maxInColumn) {
+            luckyNumbers.push(matrix[i][minIndex]);
+        }
+    }
+
+    return luckyNumbers;
+};
