5d91d299a06b75ef9dd419e58ef26063498c82b3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-02 22:23:55 +0600

    feat: complete 922. sort array by parity ii

--- 922. Sort Array By Parity II/main.js
+++ 922. Sort Array By Parity II/main.js
@@ -9,3 +9,25 @@ Return any answer array that satisfies this condition.
 */
 
 // solution
+
+var sortArrayByParityII = function (nums) {
+    let i = 0;
+    let j = 1;
+
+    while (i < nums.length && j < nums.length) {
+        if (nums[i] % 2 === 0) {
+            i += 2;
+        } else if (nums[j] % 2 === 1) {
+            j += 2;
+        } else {
+            let temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+
+            i += 2;
+            j += 2;
+        }
+    }
+
+    return nums;
+};
