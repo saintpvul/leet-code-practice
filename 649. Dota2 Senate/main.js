44073c14df933ec47ce185890821d75c531c7fee
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-04 00:39:45 +0600

    feat: complete 649. dota2 senate

--- 649. Dota2 Senate/main.js
+++ 649. Dota2 Senate/main.js
@@ -15,3 +15,29 @@ Suppose every senator is smart enough and will play the best strategy for his ow
 */
 
 // solution
+
+var predictPartyVictory = function (senate) {
+    let radiant = [];
+    let dire = [];
+
+    for (let i = 0; i < senate.length; i++) {
+        if (senate[i] === "R") {
+            radiant.push(i);
+        } else {
+            dire.push(i);
+        }
+    }
+
+    while (radiant.length > 0 && dire.length > 0) {
+        if (radiant[0] < dire[0]) {
+            radiant.push(radiant[0] + senate.length);
+        } else {
+            dire.push(dire[0] + senate.length);
+        }
+
+        radiant.shift();
+        dire.shift();
+    }
+
+    return radiant.length > 0 ? "Radiant" : "Dire";
+};
