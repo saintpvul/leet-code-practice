04eaf4a157bdc33388bd6bc67d389a8f45e4e3b5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-04 01:06:38 +0600

    feat: complete 1498. number of subsequence that satisfy the given sum condition

--- 1498. Number of Subsequences That Satisfy the Given Sum Condition/main.js
+++ 1498. Number of Subsequences That Satisfy the Given Sum Condition/main.js
@@ -7,3 +7,25 @@ Return the number of non-empty subsequences of nums such that the sum of the min
 */
 
 // solution
+
+var numSubseq = function (nums, target) {
+    const kMod = 1_000_000_007;
+    const n = nums.length;
+    let ans = 0;
+    const pows = new Array(n).fill(1);
+
+    for (let i = 1; i < n; ++i) pows[i] = (pows[i - 1] * 2) % kMod;
+
+    nums.sort((a, b) => a - b);
+
+    for (let l = 0, r = n - 1; l <= r; ) {
+        if (nums[l] + nums[r] <= target) {
+            ans = (ans + pows[r - l]) % kMod;
+            ++l;
+        } else {
+            --r;
+        }
+    }
+
+    return ans;
+};
