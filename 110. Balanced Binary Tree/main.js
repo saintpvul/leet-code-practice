8571d244fe3ed9a8aed196e410d0086d6fa573a0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-04 01:26:55 +0600

    feat: complete 110. balanced binary tree

--- 110. Balanced Binary Tree/main.js
+++ 110. Balanced Binary Tree/main.js
@@ -5,3 +5,29 @@ Given a binary tree, determine if it is height-balanced.
 */
 
 // solution
+
+const isBalanced = function (root) {
+    const getHeight = function (node) {
+        if (node === null) {
+            return 0;
+        }
+
+        const leftHeight = getHeight(node.left);
+        if (leftHeight === -1) {
+            return -1;
+        }
+
+        const rightHeight = getHeight(node.right);
+        if (rightHeight === -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    };
+
+    return getHeight(root) !== -1;
+};
