548b8f22666368b033634cab1b1ae40622fd52b1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-03 23:01:38 +0600

    feat: complete 463. island perimeter

--- 463. Island Perimeter/main.js
+++ 463. Island Perimeter/main.js
@@ -9,3 +9,24 @@ The island doesn't have "lakes", meaning the water inside isn't connected to the
 */
 
 // solution
+
+var islandPerimeter = function (grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    let per = 0;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] === 1) {
+                per += 4;
+                if (i > 0 && grid[i - 1][j] === 1) {
+                    per -= 2;
+                }
+                if (j > 0 && grid[i][j - 1] === 1) {
+                    per -= 2;
+                }
+            }
+        }
+    }
+    return per;
+};
