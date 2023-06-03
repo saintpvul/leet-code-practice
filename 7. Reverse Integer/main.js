ac62e320cb85d7f4fbb50685d7586ee318982e3b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-04 00:12:14 +0600

    feat: complete 7. reverse integer

--- 7. Reverse Integer/main.js
+++ 7. Reverse Integer/main.js
@@ -7,3 +7,19 @@ Assume the environment does not allow you to store 64-bit integers (signed or un
 */
 
 // solution
+
+var reverse = function (x) {
+    let res = 0;
+    let num = Math.abs(x);
+
+    while (num > 0) {
+        res = res * 10 + (num % 10);
+        num = Math.floor(num / 10);
+    }
+
+    if (res > Math.pow(2, 31) - 1 || res < Math.pow(-2, 31)) {
+        return 0;
+    }
+
+    return x < 0 ? -res : res;
+};
