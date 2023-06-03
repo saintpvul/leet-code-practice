539e80331ad546971dda6ade44a33b01709b8a2d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-04 00:35:22 +0600

    feat: complete 1456. maximum number of vowels in a substring of given length

--- 1456. Maximum Number of Vowels in a Substring of Given Length/main.js
+++ 1456. Maximum Number of Vowels in a Substring of Given Length/main.js
@@ -7,3 +7,21 @@ Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
 */
 
 // solution
+
+var maxVowels = function (s, k) {
+    let count = 0;
+    let maxCount = 0;
+    const vowels = new Set(["a", "e", "i", "o", "u"]);
+
+    for (let i = 0; i < s.length; i++) {
+        if (i >= k && vowels.has(s[i - k])) {
+            count--;
+        }
+        if (vowels.has(s[i])) {
+            count++;
+        }
+        maxCount = Math.max(maxCount, count);
+    }
+
+    return maxCount;
+};
