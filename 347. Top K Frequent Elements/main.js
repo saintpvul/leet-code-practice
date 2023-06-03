cb0ccc7856ea6c0e43646dbb3e89b3cadc13c15f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-03 23:42:17 +0600

    feat: complete 347. top k frequent elements

--- 347. Top K Frequent Elements/main.js
+++ 347. Top K Frequent Elements/main.js
@@ -5,3 +5,21 @@ Given an integer array nums and an integer k, return the k most frequent element
 */
 
 // solution
+
+var topKFrequent = function (nums, k) {
+    const counter = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const value = nums[i];
+        counter.set(value, (counter.get(value) || 1) + 1);
+    }
+
+    const sortedCounter = [...counter.entries()].sort((a, b) => b[1] - a[1]);
+
+    const result = [];
+    for (let i = 0; i < k; i++) {
+        result.push(sortedCounter[i][0]);
+    }
+
+    return result;
+};
