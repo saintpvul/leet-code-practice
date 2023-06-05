29945d24471dc09e0e8e3080358abb82549c6033
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-05 23:12:52 +0600

    feat: complete 326. power of three

--- 326. Power of Three/main.js
+++ 326. Power of Three/main.js
@@ -7,3 +7,17 @@ An integer n is a power of three, if there exists an integer x such that n == 3x
 */
 
 // solution
+
+var isPowerOfThree = function (n) {
+    if (n <= 0) {
+        return false;
+    }
+    while (n > 1) {
+        if (n % 3 !== 0) {
+            return false;
+        }
+        n = n / 3;
+    }
+
+    return true;
+};
