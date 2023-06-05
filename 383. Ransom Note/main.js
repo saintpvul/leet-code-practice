24fade68e3ea418eed3f86a092615d52b3e888c0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-05 23:00:50 +0600

    feat: complete 383. ransom note

--- 383. Ransom Note/main.js
+++ 383. Ransom Note/main.js
@@ -7,3 +7,15 @@ Each letter in magazine can only be used once in ransomNote.
 */
 
 // solution
+
+var canConstruct = function (ransomNote, magazine) {
+    magazine = magazine.split("");
+    for (let i = 0; i < ransomNote.length; i++) {
+        const curr = magazine.indexOf(ransomNote[i]);
+        if (curr < 0) {
+            return false;
+        }
+        magazine.splice(curr, 1);
+    }
+    return true;
+};
