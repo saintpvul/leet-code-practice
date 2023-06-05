366bc9def8e52537b85050a45103e66066364952
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-05 23:14:19 +0600

    feat: complete 342. power of four

--- 342. Power of Four/main.js
+++ 342. Power of Four/main.js
@@ -7,3 +7,18 @@ An integer n is a power of four, if there exists an integer x such that n == 4x.
 */
 
 // solution
+
+var isPowerOfFour = function (n) {
+    if (n <= 0) {
+        return false;
+    }
+
+    while (n > 1) {
+        if (n % 4 !== 0) {
+            return false;
+        }
+        n = n / 4;
+    }
+
+    return true;
+};
