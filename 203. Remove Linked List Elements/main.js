e3428d01aed9d78083e216117f23f4318e56f6f6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-06 21:14:21 +0600

    feat: complete 203. remove linked list elements

--- 203. Remove Linked List Elements/main.js
+++ 203. Remove Linked List Elements/main.js
@@ -5,3 +5,18 @@ Given the head of a linked list and an integer val, remove all the nodes of the 
 */
 
 //  solution
+
+var removeElements = function (head, val) {
+    let newHead = new ListNode(-1);
+    let current = newHead;
+
+    while (head) {
+        if (head.val !== val) {
+            current.next = new ListNode(head.val);
+            current = current.next;
+        }
+        head = head.next;
+    }
+
+    return newHead.next;
+};
