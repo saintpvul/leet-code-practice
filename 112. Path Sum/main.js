50f5eeb3d32e7caf5bfdf1e03a329bb10b2a6d4c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-06 20:09:31 +0600

    feat: complete 112. path sum

--- 112. Path Sum/main.js
+++ 112. Path Sum/main.js
@@ -7,3 +7,15 @@ A leaf is a node with no children.
 */
 
 // solution
+
+var hasPathSum = function (root, targetSum) {
+    if (!root) return false;
+    if (!root.left && !root.right) {
+        return root.val === targetSum;
+    }
+
+    return (
+        hasPathSum(root.left, targetSum - root.val) ||
+        hasPathSum(root.right, targetSum - root.val)
+    );
+};
