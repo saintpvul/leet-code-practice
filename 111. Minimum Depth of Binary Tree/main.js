fb10a211dd7101cba5950df0ca518ef63e827067
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-06 20:04:50 +0600

    feat: complete 111. minimum depth of binary tree

--- 111. Minimum Depth of Binary Tree/main.js
+++ 111. Minimum Depth of Binary Tree/main.js
@@ -9,3 +9,49 @@ Note: A leaf is a node with no children.
 */
 
 // solution
+
+// var minDepth = function (root) {
+//     if (!root) {
+//         return 0;
+//     }
+
+//     if (!root.left && !root.right) {
+//         return 1;
+//     }
+
+//     if (!root.left) {
+//         return 1 + minDepth(root.right);
+//     }
+
+//     if (!root.right) {
+//         return 1 + minDepth(root.left);
+//     }
+
+//     return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+// };
+
+var minDepth = function (root) {
+    if (!root) {
+        return 0;
+    }
+
+    let min = Infinity;
+
+    const dive = (node, depth) => {
+        if (!node.left && !node.right) {
+            min = Math.min(min, depth);
+            return;
+        }
+
+        if (node.left) {
+            dive(node.left, depth + 1);
+        }
+
+        if (node.right) {
+            dive(node.right, depth + 1);
+        }
+    };
+
+    dive(root, 1);
+    return min;
+};
