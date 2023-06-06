d02f0b59e96a5feafa7aa22cba7f978f848ff494
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-06 20:20:52 +0600

    feat: complete 119. pascal's triangle ii

--- 119. Pascal's Triangle II/main.js
+++ 119. Pascal's Triangle II/main.js
@@ -7,3 +7,16 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 */
 
 // solution
+
+var getRow = function (rowIndex) {
+    let row = [1];
+    for (let i = 1; i <= rowIndex; i++) {
+        let current = [1];
+        for (let j = 1; j < i; j++) {
+            current[j] = row[j - 1] + row[j];
+        }
+        current.push(1);
+        row = current;
+    }
+    return row;
+};
