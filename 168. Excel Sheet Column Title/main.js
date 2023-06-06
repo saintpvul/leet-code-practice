bae30b4f4e36371a613952b29fa6f2a489e8f645
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-06 20:28:17 +0600

    feat: complete 168. excel sheet column title

--- 168. Excel Sheet Column Title/main.js
+++ 168. Excel Sheet Column Title/main.js
@@ -16,3 +16,17 @@ AB -> 28
 */
 
 // solution
+
+var convertToTitle = function (columnNumber) {
+    let result = "";
+
+    while (columnNumber > 0) {
+        let remainder = (columnNumber - 1) % 26;
+        let char = String.fromCharCode(65 + remainder);
+
+        result = char + result;
+        columnNumber = Math.floor((columnNumber - 1) / 26);
+    }
+
+    return result;
+};
