b587870514155d5c06ce073b98063dee7dd8d1c0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-07 23:09:09 +0600

    feat: complete 2396. strictly palindromic number

--- 2396. Strictly Palindromic Number/main.js
+++ 2396. Strictly Palindromic Number/main.js
@@ -9,3 +9,20 @@ A string is palindromic if it reads the same forward and backward.
 */
 
 // solution
+
+var isStrictlyPalindromic = function (n) {
+    if (n < 0 || n < 2 || n > 36) {
+        return false;
+    }
+    let base = n - 2;
+    const isPal = (num) => {
+        return num === num.split("").reverse().join("");
+    };
+    while (base >= 2) {
+        if (!isPal(n.toString(base))) {
+            return false;
+        }
+        base--;
+    }
+    return true;
+};
