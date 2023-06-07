8f29027316856ec4cf377b7981d9d2d66e08e298
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-07 23:33:33 +0600

    feat: complete 1302. deepest leaves sum

--- 1302. Deepest Leaves Sum/main.js
+++ 1302. Deepest Leaves Sum/main.js
@@ -5,3 +5,25 @@ Given the root of a binary tree, return the sum of values of its deepest leaves.
 */
 
 // solution
+
+var deepestLeavesSum = function (root) {
+    let values = 0;
+    let maxDepth = 0;
+    const getLeaf = (node, depth) => {
+        if (!node) return;
+        if (!node.left && !node.right) {
+            if (depth === maxDepth) {
+                values += node.val;
+            } else if (depth > maxDepth) {
+                values = node.val;
+                maxDepth = depth;
+            }
+        }
+        getLeaf(node.left, depth + 1);
+        getLeaf(node.right, depth + 1);
+    };
+
+    getLeaf(root, 0);
+
+    return values;
+};
