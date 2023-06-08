1814e31fa14638b49a92c4227ce0eff39a856e44
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-09 00:41:06 +0600

    feat: complete 303. range sum query

--- 303. Range Sum Query - Immutable/main.js
+++ 303. Range Sum Query - Immutable/main.js
@@ -12,16 +12,33 @@ int sumRange(int left, int right) Returns the sum of the elements of nums betwee
 
 // solution
 
+// class NumArray {
+//     constructor(nums) {
+//         this.arr = nums;
+//     }
+
+//     sumRange(left, right) {
+//         let sum = 0;
+//         for (let i = left; i <= right; i++) {
+//             sum += this.arr[i];
+//         }
+//         return sum;
+//     }
+// }
+
+// a bit faster
+
 class NumArray {
     constructor(nums) {
-        this.arr = nums;
+        this.prefixSum = [];
+        this.prefixSum[0] = 0;
+
+        for (let i = 1; i <= nums.length; i++) {
+            this.prefixSum[i] = this.prefixSum[i - 1] + nums[i - 1];
+        }
     }
 
     sumRange(left, right) {
-        let sum = 0;
-        for (let i = left; i <= right; i++) {
-            sum += this.arr[i];
-        }
-        return sum;
+        return this.prefixSum[right + 1] - this.prefixSum[left];
     }
 }
