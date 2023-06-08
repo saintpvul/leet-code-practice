8b73f3c6bb0fe856c4018d49259d31249a2834ba
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-08 23:59:05 +0600

    feat: complete 263. ugly number

--- 263. Ugly Number/main.js
+++ 263. Ugly Number/main.js
@@ -7,3 +7,20 @@ Given an integer n, return true if n is an ugly number.
 */
 
 // solution
+
+var isUgly = function (n) {
+    if (n <= 0) return false;
+
+    while (!(n % 2)) {
+        n /= 2;
+    }
+    while (!(n % 3)) {
+        n /= 3;
+    }
+
+    while (!(n % 5)) {
+        n /= 5;
+    }
+
+    return n === 1;
+};
