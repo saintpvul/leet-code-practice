15fe436f9722bd98e46ebb1f484ee958b6675fbc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-09 00:30:45 +0600

    feat: complete 290. word pattern

--- 290. Word Pattern/main.js
+++ 290. Word Pattern/main.js
@@ -7,3 +7,29 @@ Here follow means a full match, such that there is a bijection between a letter 
 */
 
 // solution
+
+var wordPattern = function (pattern, s) {
+    const words = s.split(" ");
+
+    if (pattern.length !== words.length) {
+        return false;
+    }
+
+    const map = new Map();
+
+    for (let i = 0; i < pattern.length; i++) {
+        const char = pattern[i];
+        const word = words[i];
+
+        if (!map.has(char)) {
+            if ([...map.values()].includes(word)) {
+                return false;
+            }
+            map.set(char, word);
+        } else if (map.get(char) !== word) {
+            return false;
+        }
+    }
+
+    return true;
+};
