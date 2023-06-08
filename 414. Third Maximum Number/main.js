83bce54cd15248cc8188ec95bcc823db3ca25ca7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-09 01:05:35 +0600

    feat: complete 414. third maximum number

--- 414. Third Maximum Number/main.js
+++ 414. Third Maximum Number/main.js
@@ -5,3 +5,32 @@ Given an integer array nums, return the third distinct maximum number in this ar
 */
 
 // solution
+
+var thirdMax = function (nums) {
+    let first = -Infinity;
+    let second = -Infinity;
+    let third = -Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === first || nums[i] === second || nums[i] === third) {
+            continue;
+        }
+
+        if (nums[i] > first) {
+            third = second;
+            second = first;
+            first = nums[i];
+        } else if (nums[i] > second) {
+            third = second;
+            second = nums[i];
+        } else if (nums[i] > third) {
+            third = nums[i];
+        }
+    }
+    return third === -Infinity ? first : third;
+};
+
+//  var thirdMax = function (nums) {
+//      let uniq = new Set(nums);
+//      let sorted = Array.from(uniq).sort((a, b) => b - a);
+//      return sorted.length > 2 ? sorted[2] : sorted[0];
+//  };
