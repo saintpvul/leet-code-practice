89e2d6ddf9c5718a37db8bd06ac958526a05d347
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-09 00:30:29 +0600

    feat: *

--- 257. Binary Tree Paths/main.js
+++ 257. Binary Tree Paths/main.js
@@ -8,3 +8,28 @@ A leaf is a node with no children.
 */
 
 // solution
+
+var binaryTreePaths = function (root) {
+    const paths = [];
+
+    const dfs = (node, path) => {
+        if (node === null) {
+            return;
+        }
+
+        path.push(node.val);
+
+        if (node.left === null && node.right === null) {
+            paths.push(path.join("->"));
+        } else {
+            dfs(node.left, path);
+            dfs(node.right, path);
+        }
+
+        path.pop();
+    };
+
+    dfs(root, []);
+
+    return paths;
+};
