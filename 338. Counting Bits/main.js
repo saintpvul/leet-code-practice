c02ca8b1edeadaaa9b34bd256eae16a7fa59469f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-09 00:49:17 +0600

    feat: complete 338. counting bits

--- 338. Counting Bits/main.js
+++ 338. Counting Bits/main.js
@@ -5,3 +5,23 @@ Given an integer n, return an array ans of length n + 1 such that for each i (0 
 */
 
 // solution
+
+// var countBits = function (n) {
+//     let ans = Array.from({ length: n + 1 });
+//     for (let i = 0; i <= n; i++) {
+//         const count = i.toString(2).replace(/[0]/gi, "").length;
+//         ans[i] = count;
+//     }
+//     return ans;
+// };
+
+//using bitwise operator is more efficient
+var countBits = function (n) {
+    const bitsCount = new Array(n + 1).fill(0);
+
+    for (let i = 1; i <= n; i++) {
+        bitsCount[i] = bitsCount[i >> 1] + (i & 1);
+    }
+
+    return bitsCount;
+};
