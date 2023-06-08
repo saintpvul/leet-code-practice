50fd4736762886b1bc4154967d30c1254034ad30
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-08 23:52:57 +0600

    feat: complete 228. summary ranges

--- 228. Summary Ranges/main.js
+++ 228. Summary Ranges/main.js
@@ -14,3 +14,28 @@ Each range [a,b] in the list should be output as:
 */
 
 // solution
+
+var summaryRanges = function (nums) {
+    const res = [];
+
+    if (nums.length === 0) {
+        return res;
+    }
+
+    let start = nums[0];
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (i === nums.length || nums[i] !== nums[i - 1] + 1) {
+            if (start === nums[i - 1]) {
+                res.push(start.toString());
+            } else {
+                res.push(`${start}->${nums[i - 1]}`);
+            }
+            if (i < nums.length) {
+                start = nums[i];
+            }
+        }
+    }
+
+    return res;
+};
