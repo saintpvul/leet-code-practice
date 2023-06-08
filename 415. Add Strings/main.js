c675936059b5c7da52db97593b054b9cd5ed5d2a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-09 01:12:28 +0600

    feat: complete 415. add strings

--- 415. Add Strings/main.js
+++ 415. Add Strings/main.js
@@ -7,3 +7,22 @@ You must solve the problem without using any built-in library for handling large
 */
 
 // solution
+
+var addStrings = function (num1, num2) {
+    let res = "";
+    let carry = 0;
+    let i = num1.length - 1;
+    let j = num2.length - 1;
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        const int1 = i >= 0 ? +num1[i] : 0;
+        const int2 = j >= 0 ? +num2[j] : 0;
+        const sum = int1 + int2 + carry;
+
+        res = (sum % 10) + res;
+        carry = Math.floor(sum / 10);
+        i--;
+        j--;
+    }
+    return res;
+};
