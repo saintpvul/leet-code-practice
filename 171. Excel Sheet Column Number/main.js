f3d49ec55f674ffdc80afeac12ac2e3d67ba8ac3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-08 22:08:14 +0600

    feat: complete 171. excel sheet column number

--- 171. Excel Sheet Column Number/main.js
+++ 171. Excel Sheet Column Number/main.js
@@ -16,3 +16,18 @@ AB -> 28
 */
 
 // solution
+
+var titleToNumber = function (columnTitle) {
+    let res = 0;
+
+    while (columnTitle.length > 0) {
+        const firstLetter = columnTitle.slice(0, 1);
+
+        const number = firstLetter.charCodeAt(0) - 65 + 1;
+        res = res * 26 + number;
+
+        columnTitle = columnTitle.slice(1);
+    }
+
+    return res;
+};
