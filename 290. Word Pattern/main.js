23eaf02ed6dc0ad1a66201d8f1eea15fcfd98eb5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-08 23:57:21 +0600

    feat: complete 257. binary tree paths

--- 290. Word Pattern/main.js
+++ 290. Word Pattern/main.js
@@ -7,3 +7,28 @@ Here follow means a full match, such that there is a bijection between a letter 
 */
 
 // solution
+
+var binaryTreePaths = function (root) {
+    const paths = [];
+
+    const dfs = (node, path) => {
+        if (node === null) {
+            return;
+        }
+
+        path.push(node.val);
+
+        if (node.left === null && node.right === null) {
+            paths.push(path.join("->"));
+        } else {
+            dfs(node.left, path);
+            dfs(node.right, path);
+        }
+
+        path.pop();
+    };
+
+    dfs(root, []);
+
+    return paths;
+};
