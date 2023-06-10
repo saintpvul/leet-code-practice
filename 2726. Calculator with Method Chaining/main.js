9445f458614cbfb6b1450d8bb0500cf1919f5e17
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-10 16:23:31 +0600

    feat: complete 2726. calculator with method chaining

--- 2726. Calculator with Method Chaining/main.js
+++ 2726. Calculator with Method Chaining/main.js
@@ -15,3 +15,68 @@ Solutions within 10-5 of the actual result are considered correct.
 */
 
 // solution
+
+class Calculator {
+    /**
+     * @param {number} value
+     */
+    constructor(value) {
+        this.result = value;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value) {
+        this.result += value;
+        return this.result;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value) {
+        this.result -= value;
+        return this.result;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    multiply(value) {
+        this.result *= value;
+        return this.result;
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if (value === 0) {
+            throw new Error("Division by zero is not allowed");
+        } else {
+            this.result /= value;
+            return this.result;
+        }
+    }
+
+    /**
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.result **= value;
+        return this.result;
+    }
+
+    /**
+     * @return {number}
+     */
+    getResult() {
+        return this.result;
+    }
+}
