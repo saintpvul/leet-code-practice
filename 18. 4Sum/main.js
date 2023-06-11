7751f6fde9d0ad18bbaae697012b0ea9c09b59cf
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-11 22:35:35 +0600

    feat: complete 18. 4sum

--- 18. 4Sum/main.js
+++ 18. 4Sum/main.js
@@ -10,3 +10,40 @@ You may return the answer in any order.
 */
 
 // solution
+
+var fourSum = function (nums, target) {
+    const res = [];
+    const n = nums.length;
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < n - 3; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        for (let j = i + 1; j < n - 2; j++) {
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+
+            let left = j + 1;
+            let right = n - 1;
+            while (left < right) {
+                let total = nums[i] + nums[j] + nums[left] + nums[right];
+                if (total === target) {
+                    res.push([nums[i], nums[j], nums[left], nums[right]]);
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] === nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] === nums[right + 1]) {
+                        right--;
+                    }
+                } else if (total < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+    return res;
+};
