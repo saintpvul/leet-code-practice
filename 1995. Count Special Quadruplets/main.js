46ce4633850b6d98f04a9b826a2dd1469e41f6ba
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-11 22:53:20 +0600

    feat: complete 1995. count special quadruplets

--- 1995. Count Special Quadruplets/main.js
+++ 1995. Count Special Quadruplets/main.js
@@ -8,3 +8,23 @@ a < b < c < d
 */
 
 // solution
+
+var countQuadruplets = function (nums) {
+    let count = 0;
+    const n = nums.length;
+
+    for (let a = 0; a < n - 3; a++) {
+        for (let b = a + 1; b < n - 2; b++) {
+            for (let c = b + 1; c < n - 1; c++) {
+                for (let d = c + 1; d < n; d++) {
+                    let sum = nums[a] + nums[b] + nums[c];
+                    if (sum === nums[d]) {
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+
+    return count;
+};
