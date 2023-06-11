39edb80912cf3cd558c435f68f58db097db97926
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-11 23:36:03 +0600

    feat: complete 34 find first and last position of element in sorted array

--- 34. Find First and Last Position of Element in Sorted Array/main.js
+++ 34. Find First and Last Position of Element in Sorted Array/main.js
@@ -9,3 +9,36 @@ You must write an algorithm with O(log n) runtime complexity.
 */
 
 // solution
+
+var searchRange = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    let res = [-1, -1];
+
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] === target) {
+            res[0] = mid;
+            res[1] = mid;
+
+            let i = mid - 1;
+            while (i >= left && nums[i] === target) {
+                res[0] = i;
+                i--;
+            }
+            let j = mid + 1;
+
+            while (j <= right && nums[j] === target) {
+                res[1] = j;
+                j++;
+            }
+            break;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return res;
+};
