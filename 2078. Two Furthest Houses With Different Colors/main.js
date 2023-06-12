9519f1f73070bd4e4f4623def2f5fc39aed9a650
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-13 00:19:18 +0600

    feat: start 2078. two furthest houses with different colors

---
+++ 2078. Two Furthest Houses With Different Colors/main.js
@@ -0,0 +1,11 @@
+/*
+
+There are n houses evenly lined up on the street, and each house is beautifully painted. You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
+
+Return the maximum distance between two houses with different colors.
+
+The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+
+*/
+
+// solution
