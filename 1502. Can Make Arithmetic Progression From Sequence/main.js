1aca34edf2bb0201990f473a3b73d6d4c02a3d09
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-12 22:25:29 +0600

    feat: complete 1502. can make arithmetic progression from sequence

--- 1502. Can Make Arithmetic Progression From Sequence/main.js
+++ 1502. Can Make Arithmetic Progression From Sequence/main.js
@@ -7,3 +7,13 @@ Given an array of numbers arr, return true if the array can be rearranged to for
 */
 
 // solution
+
+var canMakeArithmeticProgression = function (arr) {
+    arr.sort((a, b) => a - b);
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        const diff = Math.abs(arr[0] - arr[1]);
+        if (diff !== Math.abs(arr[i] - arr[i + 1])) return false;
+    }
+    return true;
+};
