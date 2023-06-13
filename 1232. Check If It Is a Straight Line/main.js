b3967ca69c1fd150e0ec5a202d2b5fbb5b3bc13d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-13 23:57:07 +0600

    feat: complete 1232. check if it is a straight line

--- 1232. Check If It Is a Straight Line/main.js
+++ 1232. Check If It Is a Straight Line/main.js
@@ -5,3 +5,20 @@ You are given an array coordinates, coordinates[i] = [x, y], where [x, y] repres
 */
 
 // solution
+
+var checkStraightLine = function (coordinates) {
+    let x0 = coordinates[0][0];
+    let y0 = coordinates[0][1];
+    let x1 = coordinates[1][0];
+    let y1 = coordinates[1][1];
+    let dx = x1 - x0;
+    let dy = y1 - y0;
+
+    for (let i = 2; i < coordinates.length; ++i) {
+        let x = coordinates[i][0];
+        let y = coordinates[i][1];
+        if ((x - x0) * dy !== (y - y0) * dx) return false;
+    }
+
+    return true;
+};
