16079e0e5ba883376cb713d5e2d186c3747b115a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-13 23:51:16 +0600

    feat: complete 2558. take gifts from the richest pile

--- 2558. Take Gifts From the Richest Pile/main.js
+++ 2558. Take Gifts From the Richest Pile/main.js
@@ -10,3 +10,14 @@ Return the number of gifts remaining after k seconds.
 */
 
 // solution
+
+var pickGifts = function (gifts, k) {
+    while (k) {
+        let max = Math.max(...gifts);
+        let idx = gifts.indexOf(max);
+        let sqrt = Math.floor(Math.sqrt(max));
+        gifts[idx] = sqrt;
+        k--;
+    }
+    return gifts.reduce((s, v) => s + v, 0);
+};
