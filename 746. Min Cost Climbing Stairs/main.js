8e1363c017e4f6edbc02c52a6eb3bbeac7d0fba8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-14 00:04:05 +0600

    feat: complete 746. min cost climbing stairs

--- 746. Min Cost Climbing Stairs/main.js
+++ 746. Min Cost Climbing Stairs/main.js
@@ -9,3 +9,17 @@ Return the minimum cost to reach the top of the floor.
 */
 
 // solution
+
+var minCostClimbingStairs = function (cost) {
+    let n = cost.length;
+
+    let price = new Array(n);
+
+    price[0] = cost[0];
+    price[1] = cost[1];
+
+    for (let i = 2; i < n; i++) {
+        price[i] = cost[i] + Math.min(price[i - 1], price[i - 2]);
+    }
+    return Math.min(price[n - 1], price[n - 2]);
+};
