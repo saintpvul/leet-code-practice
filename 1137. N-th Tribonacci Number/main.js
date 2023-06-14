521f5ce7a0a27d5c13208e2bd026b56a34daa951
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-14 23:59:26 +0600

    feat: complete 1137. n-th tribonacci number

--- 1137. N-th Tribonacci Number/main.js
+++ 1137. N-th Tribonacci Number/main.js
@@ -9,3 +9,22 @@ Given n, return the value of Tn.
 */
 
 // solution
+
+var tribonacci = function (n) {
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
+
+    let tnMinus3 = 0;
+    let tnMinus2 = 1;
+    let tnMinus1 = 1;
+    let tn = 0;
+
+    for (let i = 3; i <= n; i++) {
+        tn = tnMinus3 + tnMinus2 + tnMinus1;
+        tnMinus3 = tnMinus2;
+        tnMinus2 = tnMinus1;
+        tnMinus1 = tn;
+    }
+
+    return tn;
+};
