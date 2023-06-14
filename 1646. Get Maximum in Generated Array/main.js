c25d0bb8a5b7b8fec19ae3b2e597bc42a7298edc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-14 23:44:45 +0600

    feat: start 1646. get maximum in generated array

---
+++ 1646. Get Maximum in Generated Array/main.js
@@ -0,0 +1,13 @@
+/*
+
+You are given an integer n. A 0-indexed integer array nums of length n + 1 is generated in the following way:
+
+nums[0] = 0
+nums[1] = 1
+nums[2 * i] = nums[i] when 2 <= 2 * i <= n
+nums[2 * i + 1] = nums[i] + nums[i + 1] when 2 <= 2 * i + 1 <= n
+Return the maximum integer in the array nums​​​.
+
+*/
+
+// solution
