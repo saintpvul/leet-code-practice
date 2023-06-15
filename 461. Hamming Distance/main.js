9a85bd4c4a3b9b2d8d073ee399511765d62924ae
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-15 23:18:51 +0600

    feat: complete 461. hamming distance

--- 461. Hamming Distance/main.js
+++ 461. Hamming Distance/main.js
@@ -7,3 +7,18 @@ Given two integers x and y, return the Hamming distance between them.
 */
 
 // solution
+
+var hammingDistance = function (x, y) {
+    x = x.toString(2);
+    y = y.toString(2);
+    let n = Math.max(x.length, y.length);
+    x = ("0".repeat(n) + x).slice(-n);
+    y = ("0".repeat(n) + y).slice(-n);
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+        if (x[i] !== y[i]) {
+            count++;
+        }
+    }
+    return count;
+};
