e0cbe95e22432b72f3e3be7caa20e312d8fd7591
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-15 22:10:55 +0600

    feat: complete 434. number of segments in a string

--- 434. Number of Segments in a String/main.js
+++ 434. Number of Segments in a String/main.js
@@ -7,3 +7,28 @@ A segment is defined to be a contiguous sequence of non-space characters.
 */
 
 // solution
+
+// var countSegments = function (s) {
+//     s = s.split(" ");
+//     let count = 0;
+//     for (let i = 0; i < s.length; i++) {
+//         if (/^[^\s]/.test(s[i])) {
+//             count++;
+//         }
+//     }
+//     return count;
+// };
+
+var countSegments = function (s) {
+    s = s.split(" ");
+
+    let count = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "") {
+            continue;
+        } else {
+            count++;
+        }
+    }
+    return count;
+};
