e880a426151a4463393aac4a08a583cc511b5d34
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-16 22:16:27 +0600

    feat: complete 485. max consecutive ones

--- 485. Max Consecutive Ones/main.js
+++ 485. Max Consecutive Ones/main.js
@@ -5,3 +5,19 @@ Given a binary array nums, return the maximum number of consecutive 1's in the a
 */
 
 // solution
+
+var findMaxConsecutiveOnes = function (nums) {
+    let count = 0;
+    let currentCount = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            currentCount++;
+            count = Math.max(count, currentCount);
+        } else {
+            currentCount = 0;
+        }
+    }
+
+    return count;
+};
