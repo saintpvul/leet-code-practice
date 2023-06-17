4e7691b6c7feb5d1a41f7c1166629f1d0a613c41
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-17 20:52:05 +0600

    feat: complete 520. detect capital

--- 520. Detect Capital/main.js
+++ 520. Detect Capital/main.js
@@ -10,3 +10,33 @@ Given a string word, return true if the usage of capitals in it is right.
 */
 
 // solution
+
+var detectCapitalUse = function (word) {
+    if (word.length === 1) {
+        return true;
+    }
+
+    const isFirstLetterCapital = word[0] === word[0].toUpperCase();
+    const isSecondLetterLowerCase = word[1] === word[1].toLowerCase();
+
+    if (!isFirstLetterCapital && !isSecondLetterLowerCase) {
+        return false;
+    }
+
+    for (let i = 2; i < word.length; i++) {
+        if (isFirstLetterCapital) {
+            if (isSecondLetterLowerCase && word[i] !== word[i].toLowerCase()) {
+                return false;
+            }
+            if (!isSecondLetterLowerCase && word[i] !== word[i].toUpperCase()) {
+                return false;
+            }
+        } else {
+            if (word[i] !== word[i].toLowerCase()) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
