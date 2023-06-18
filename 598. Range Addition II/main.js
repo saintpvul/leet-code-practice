9a8ff55a1294714b8c4577fe9294a191338ad492
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-18 23:47:23 +0600

    feat: complete 598. range addition ii

--- 598. Range Addition II/main.js
+++ 598. Range Addition II/main.js
@@ -7,3 +7,11 @@ Count and return the number of maximum integers in the matrix after performing a
 */
 
 // solution
+
+var maxCount = function (m, n, ops) {
+    for (let i = 0; i < ops.length; i++) {
+        m = Math.min(m, ops[i][0]);
+        n = Math.min(n, ops[i][1]);
+    }
+    return m * n;
+};
