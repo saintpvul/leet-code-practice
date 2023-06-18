39faefc320b96c61231d030b78c334d15457e992
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-18 23:57:02 +0600

    feat: complete 572. subtree of another tree

--- 572. Subtree of Another Tree/main.js
+++ 572. Subtree of Another Tree/main.js
@@ -7,3 +7,31 @@ A subtree of a binary tree tree is a tree that consists of a node in tree and al
 */
 
 // solution
+
+const isSubtree = function (root, subRoot) {
+    if (root === null) {
+        return false;
+    }
+
+    if (isSameTree(root, subRoot)) {
+        return true;
+    }
+
+    return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
+const isSameTree = function (p, q) {
+    if (p === null && q === null) {
+        return true;
+    }
+
+    if (p === null || q === null) {
+        return false;
+    }
+
+    if (p.val !== q.val) {
+        return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
