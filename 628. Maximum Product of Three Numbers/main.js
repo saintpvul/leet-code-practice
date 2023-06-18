0a82a3e9735a871b83a8cba639983b9289843d53
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-18 23:04:36 +0600

    feat: complete 628. maximum product of tree numbers

--- 628. Maximum Product of Three Numbers/main.js
+++ 628. Maximum Product of Three Numbers/main.js
@@ -1,7 +1,16 @@
 /*
 
-
+Given an integer array nums, find three numbers whose product is maximum and return the maximum product.
 
 */
 
 // solution
+
+var maximumProduct = function (nums) {
+    nums = nums.sort((a, b) => a - b);
+    const n = nums.length;
+    const prod1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+    const prod2 = nums[0] * nums[1] * nums[n - 1];
+
+    return Math.max(prod1, prod2);
+};
