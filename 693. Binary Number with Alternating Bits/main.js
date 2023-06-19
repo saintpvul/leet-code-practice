b9c08ffa16e965f0132c749c28ecf8fd74e1a7b9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-19 18:26:08 +0600

    feat: complete 693. binary number with alternating bits

--- 693. Binary Number with Alternating Bits/main.js
+++ 693. Binary Number with Alternating Bits/main.js
@@ -5,3 +5,13 @@ Given a positive integer, check whether it has alternating bits: namely, if two 
 */
 
 // solution
+
+var hasAlternatingBits = function (n) {
+    let bit = n.toString(2);
+    for (let i = 0; i < bit.length - 1; i++) {
+        if (bit[i] === bit[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+};
