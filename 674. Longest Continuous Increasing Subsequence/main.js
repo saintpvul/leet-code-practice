b38b3b2fe6f03538c0684452f2a4cecfda177955
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-19 18:33:20 +0600

    feat: complete 674. longest continuous increasing subsequence

--- 674. Longest Continuous Increasing Subsequence/main.js
+++ 674. Longest Continuous Increasing Subsequence/main.js
@@ -9,3 +9,21 @@ A continuous increasing subsequence is defined by two indices l and r (l < r) su
 */
 
 // solution
+
+var findLengthOfLCIS = function (nums) {
+    if (nums.length === 0) return 0;
+
+    let maxLength = 1;
+    let currentLength = 1;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[i - 1]) {
+            currentLength++;
+            maxLength = Math.max(maxLength, currentLength);
+        } else {
+            currentLength = 1;
+        }
+    }
+
+    return maxLength;
+};
