c8936af04cea4dda46bdb70e85afe84b773bf2e2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-19 17:57:57 +0600

    feat: start complete. degree of an array

--- 697. Degree of an Array/main.js
+++ 697. Degree of an Array/main.js
@@ -7,3 +7,33 @@ Your task is to find the smallest possible length of a (contiguous) subarray of 
 */
 
 // solution
+
+var findShortestSubArray = function (nums) {
+    const frequency = {};
+    const indices = {};
+    let maxFreq = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+
+        if (!(num in indices)) {
+            indices[num] = { start: i, end: i };
+        } else {
+            indices[num].end = i;
+        }
+
+        frequency[num] = (frequency[num] || 0) + 1;
+        maxFreq = Math.max(maxFreq, frequency[num]);
+    }
+
+    let minLength = Infinity;
+
+    for (const num in frequency) {
+        if (frequency[num] === maxFreq) {
+            const length = indices[num].end - indices[num].start + 1;
+            minLength = Math.min(minLength, length);
+        }
+    }
+
+    return minLength;
+};
