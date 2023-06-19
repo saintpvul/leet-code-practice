575819e61c10de839313865953d05559d161323d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-19 18:42:53 +0600

    feat: complete 671. second minimum node in a binary tree

--- 671. Second Minimum Node In a Binary Tree/main.js
+++ 671. Second Minimum Node In a Binary Tree/main.js
@@ -9,3 +9,43 @@ If no such second minimum value exists, output -1 instead.
 */
 
 // solution
+
+// var findSecondMinimumValue = function (root) {
+//     let vals = new Set();
+
+//     const traverse = (node) => {
+//         if (node === null) return;
+
+//         if (!node.left && !node.right && !vals.has(node.val)) {
+//             vals.add(node.val);
+//         }
+
+//         traverse(node.left);
+//         traverse(node.right);
+//     };
+//     traverse(root);
+//     console.log(vals);
+//     return Array.from(vals).sort((a, b) => a - b)[1] || -1;
+// };
+
+//
+
+var findSecondMinimumValue = function (root) {
+    let min = root.val;
+    let secondMin = Infinity;
+
+    const traverse = (node) => {
+        if (node === null) return;
+
+        if (node.val > min && node.val < secondMin) {
+            secondMin = node.val;
+        }
+
+        traverse(node.left);
+        traverse(node.right);
+    };
+
+    traverse(root);
+
+    return secondMin !== Infinity ? secondMin : -1;
+};
