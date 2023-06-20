dddffb52bf4ceab387fc5c6a2d36efd00817bff6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-21 01:20:56 +0600

    feat: complete 812. largest triangle area

--- 812. Largest Triangle Area/main.js
+++ 812. Largest Triangle Area/main.js
@@ -5,3 +5,25 @@ Given an array of points on the X-Y plane points where points[i] = [xi, yi], ret
 */
 
 // solution
+
+var largestTriangleArea = function (points) {
+    let maxArea = 0;
+
+    for (let i = 0; i < points.length - 2; i++) {
+        for (let j = i + 1; j < points.length - 1; j++) {
+            for (let k = j + 1; k < points.length; k++) {
+                const [x1, y1] = points[i];
+                const [x2, y2] = points[j];
+                const [x3, y3] = points[k];
+
+                const area =
+                    0.5 *
+                    Math.abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2));
+
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+    }
+
+    return maxArea;
+};
