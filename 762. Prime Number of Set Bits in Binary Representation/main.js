a652e3e237dc1f20a32fd334b2992345e638db2c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-20 23:17:01 +0600

    feat: complete 762. prime number of set bits in binary representation

--- 762. Prime Number of Set Bits in Binary Representation/main.js
+++ 762. Prime Number of Set Bits in Binary Representation/main.js
@@ -9,3 +9,39 @@ For example, 21 written in binary is 10101, which has 3 set bits.
 */
 
 // solution
+
+var countPrimeSetBits = function (left, right) {
+    let primeCount = 0;
+
+    for (let i = left; i <= right; i++) {
+        let countBits = countSetBits(i);
+        if (isPrime(countBits)) {
+            primeCount++;
+        }
+    }
+
+    return primeCount;
+};
+
+function countSetBits(number) {
+    let count = 0;
+    while (number) {
+        count += number & 1;
+        number >>= 1;
+    }
+    return count;
+}
+
+function isPrime(number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
