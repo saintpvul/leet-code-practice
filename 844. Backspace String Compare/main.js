6822d32b7f3b1ca992706435a5cf63a64604d8f5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-21 22:35:56 +0600

    feat: complete 844. backspace string compare

--- 844. Backspace String Compare/main.js
+++ 844. Backspace String Compare/main.js
@@ -7,3 +7,22 @@ Note that after backspacing an empty text, the text will continue empty.
 */
 
 // solution
+
+var backspaceCompare = function (s, t) {
+    const stacked = (str) => {
+        let stack = [];
+        for (let i = 0; i < str.length; i++) {
+            if (str[i] === "#") {
+                stack.pop();
+            } else {
+                stack.push(str[i]);
+            }
+        }
+        return stack.join("");
+    };
+
+    const stackedS = stacked(s);
+    const stackedT = stacked(t);
+
+    return stackedS === stackedT;
+};
