39ae90819dafcc2fabda7242b69b0f1ab07efa9c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-21 22:35:06 +0600

    feat: complete 859. buddy strings

--- 859. Buddy Strings/main.js
+++ 859. Buddy Strings/main.js
@@ -9,3 +9,25 @@ For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
 */
 
 // solution
+
+var buddyStrings = function (s, goal) {
+    if (s.length != goal.length) return false;
+
+    if (s === goal) {
+        const chars = new Set(s).size;
+        return chars < s.length;
+    }
+
+    let indicies = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != goal[i]) {
+            indicies.push(i);
+        }
+    }
+    if (indicies.length > 2) return false;
+
+    return (
+        s[indicies[0]] === goal[indicies[1]] &&
+        s[indicies[1]] === goal[indicies[0]]
+    );
+};
