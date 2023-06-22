f0929d77ac4b1716c16a401df1f21f70b1364f21
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-22 21:48:04 +0600

    feat: start 976. largest perimeter triangle

---
+++ 976. Largest Perimeter Triangle/main.js
@@ -0,0 +1,7 @@
+/*
+
+Given an integer array nums, return the largest perimeter of a triangle with a non-zero area, formed from three of these lengths. If it is impossible to form any triangle of a non-zero area, return 0.
+
+*/
+
+// solution
