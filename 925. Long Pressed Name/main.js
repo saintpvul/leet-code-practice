66baf2908bfe74296ced8ef5a79691b38fca2c3b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-22 22:41:41 +0600

    feat: complete 925. long pressed name

--- 925. Long Pressed Name/main.js
+++ 925. Long Pressed Name/main.js
@@ -7,3 +7,21 @@ You examine the typed characters of the keyboard. Return True if it is possible 
 */
 
 // solution
+
+var isLongPressedName = function (name, typed) {
+    let i = 0;
+    let j = 0;
+
+    while (j < typed.length) {
+        if (i < name.length && name[i] === typed[j]) {
+            i++;
+            j++;
+        } else if (j > 0 && typed[j] === typed[j - 1]) {
+            j++;
+        } else {
+            return false;
+        }
+    }
+
+    return i === name.length;
+};
