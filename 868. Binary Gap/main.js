4c7240cf71a6e4035f76411ddcc2661a906920b3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-22 21:44:44 +0600

    feat: complete 868. binary gap

--- 868. Binary Gap/main.js
+++ 868. Binary Gap/main.js
@@ -7,3 +7,19 @@ Two 1's are adjacent if there are only 0's separating them (possibly no 0's). Th
 */
 
 // solution
+
+var binaryGap = function (n) {
+    let maxDist = 0;
+    n = n.toString(2);
+
+    let prevIdx = -1;
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] === "1") {
+            if (prevIdx != -1) {
+                maxDist = Math.max(maxDist, i - prevIdx);
+            }
+            prevIdx = i;
+        }
+    }
+    return maxDist;
+};
