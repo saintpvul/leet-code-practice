a7e774b51a8e6538206e0da6375a9fa6547e56ba
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-22 21:46:04 +0600

    feat: start 914. x of a kind in a deck of cards

---
+++ 914. X of a Kind in a Deck of Cards/main.js
@@ -0,0 +1,13 @@
+/*
+
+You are given an integer array deck where deck[i] represents the number written on the ith card.
+
+Partition the cards into one or more groups such that:
+
+Each group has exactly x cards where x > 1, and
+All the cards in one group have the same integer written on them.
+Return true if such partition is possible, or false otherwise.
+
+*/
+
+// solution
