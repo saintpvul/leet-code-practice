e88d4eade28f73aa3fccca16897294a0b7159e83
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-22 22:41:59 +0600

    feat: complete 941. valid mountain array

--- 941. Valid Mountain Array/main.js
+++ 941. Valid Mountain Array/main.js
@@ -12,3 +12,25 @@ arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
 */
 
 // solution
+
+var validMountainArray = function (arr) {
+    if (arr.length < 3) {
+        return false;
+    }
+
+    let i = 0;
+
+    while (i < arr.length - 1 && arr[i] < arr[i + 1]) {
+        i++;
+    }
+
+    if (i === 0 || i === arr.length - 1) {
+        return false;
+    }
+
+    while (i < arr.length - 1 && arr[i] > arr[i + 1]) {
+        i++;
+    }
+
+    return i === arr.length - 1;
+};
