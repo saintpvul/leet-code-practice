3011a5a7999325bfef61b89090b1bcce6a122090
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-22 22:42:19 +0600

    feat: complete 976. largest perimeter triangle

--- 976. Largest Perimeter Triangle/main.js
+++ 976. Largest Perimeter Triangle/main.js
@@ -5,3 +5,16 @@ Given an integer array nums, return the largest perimeter of a triangle with a n
 */
 
 // solution
+
+var largestPerimeter = function (nums) {
+    if (nums.length < 3) return 0;
+
+    nums = nums.sort((a, b) => b - a);
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < nums[i + 1] + nums[i + 2]) {
+            return nums[i] + nums[i + 1] + nums[i + 2];
+        }
+    }
+    return 0;
+};
