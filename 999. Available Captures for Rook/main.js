8ad49a53a2beb5dc079d0a61e7fe27e5fdea5c5d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-23 20:38:15 +0600

    feat: complete 999. available captures for rook

--- 999. Available Captures for Rook/main.js
+++ 999. Available Captures for Rook/main.js
@@ -9,3 +9,43 @@ Return the number of available captures for the white rook.
 */
 
 // solution
+
+var numRookCaptures = function (board) {
+    let rookRow, rookCol;
+    let captures = 0;
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if (board[i][j] === "R") {
+                rookRow = i;
+                rookCol = j;
+                break;
+            }
+        }
+    }
+
+    const directions = [
+        [-1, 0],
+        [0, 1],
+        [1, 0],
+        [0, -1],
+    ];
+    for (const [dx, dy] of directions) {
+        let row = rookRow + dx;
+        let col = rookCol + dy;
+
+        while (row >= 0 && row < 8 && col >= 0 && col < 8) {
+            if (board[row][col] === "p") {
+                captures++;
+                break;
+            } else if (board[row][col] === "B") {
+                break;
+            }
+
+            row += dx;
+            col += dy;
+        }
+    }
+
+    return captures;
+};
