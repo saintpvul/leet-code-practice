e4786d168fac7b77fcc129c3568c02fed969180a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-23 20:31:58 +0600

    feat: complete 953. verifuing an alien dictionary

--- 953. Verifying an Alien Dictionary/main.js
+++ 953. Verifying an Alien Dictionary/main.js
@@ -7,3 +7,37 @@ Given a sequence of words written in the alien language, and the order of the al
 */
 
 // solution
+
+var isAlienSorted = function (words, order) {
+    const charMap = new Map();
+
+    const isSorted = (word1, word2, charMap) => {
+        const l1 = word1.length;
+        const l2 = word2.length;
+        const minL = Math.min(l1, l2);
+
+        for (let i = 0; i < minL; i++) {
+            const char1 = word1.charAt(i);
+            const char2 = word2.charAt(i);
+            if (charMap.get(char1) < charMap.get(char2)) {
+                return true;
+            } else if (charMap.get(char1) > charMap.get(char2)) {
+                return false;
+            }
+        }
+
+        return l1 <= l2;
+    };
+
+    for (let i = 0; i < order.length; i++) {
+        charMap.set(order[i], i);
+    }
+
+    for (let i = 1; i < words.length; i++) {
+        if (!isSorted(words[i - 1], words[i], charMap)) {
+            return false;
+        }
+    }
+
+    return true;
+};
