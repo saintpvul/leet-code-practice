0b711264ffdf799173cbb29da1b8bff2dfd1a19e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-25 01:46:33 +0600

    feat: complete 1287. element appearing more than 25% in sorted array

--- 1287. Element Appearing More Than 25% In Sorted Array/main.js
+++ 1287. Element Appearing More Than 25% In Sorted Array/main.js
@@ -5,3 +5,30 @@ Given an integer array sorted in non-decreasing order, there is exactly one inte
 */
 
 // solution
+
+// var findSpecialInteger = function (arr) {
+//     if (arr.length === 1) return arr[0];
+//     let percentage = Math.floor(arr.length * 0.25);
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === arr[i + percentage]) {
+//             return arr[i];
+//         }
+//     }
+// };
+
+var findSpecialInteger = function (arr) {
+    if (arr.length === 1) return arr[0];
+    let percentage = Math.floor(arr.length * 0.25);
+    let counter = {};
+    for (let i = 0; i < arr.length; i++) {
+        const elem = arr[i];
+        if (counter[elem]) {
+            if (counter[elem] >= percentage) {
+                return elem;
+            }
+            counter[elem]++;
+        } else {
+            counter[elem] = 1;
+        }
+    }
+};
