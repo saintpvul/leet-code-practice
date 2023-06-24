af50e393fde445fc17c758c3c01a07212ce250b4
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-24 23:56:48 +0600

    feat: complete 1037. valid boomerang

--- 1037. Valid Boomerang/main.js
+++ 1037. Valid Boomerang/main.js
@@ -7,3 +7,25 @@ A boomerang is a set of three points that are all distinct and not in a straight
 */
 
 // solution
+
+var isBoomerang = function (points) {
+    const [x1, y1] = points[0];
+    const [x2, y2] = points[1];
+    const [x3, y3] = points[2];
+
+    if (
+        (x1 === x2 && y1 === y2) ||
+        (x1 === x3 && y1 === y3) ||
+        (x2 === x3 && y2 === y3)
+    ) {
+        return false;
+    }
+
+    const crossProduct = (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+
+    if (crossProduct === 0) {
+        return false;
+    }
+
+    return true;
+};
