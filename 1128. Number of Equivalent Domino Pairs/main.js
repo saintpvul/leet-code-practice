71cdd771a7e5b1f4b0c56b05415a27e328b0ea8f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-25 01:55:10 +0600

    feat: complete 1128. number of equivalent domino pairs

--- 1128. Number of Equivalent Domino Pairs/main.js
+++ 1128. Number of Equivalent Domino Pairs/main.js
@@ -7,3 +7,37 @@ Return the number of pairs (i, j) for which 0 <= i < j < dominoes.length, and do
 */
 
 // solution
+
+// too slow
+// var numEquivDominoPairs = function (dominoes) {
+//     let pairs = 0;
+//     for (let i = 0; i < dominoes.length - 1; i++) {
+//         let [a, b] = dominoes[i];
+//         for (let j = i + 1; j < dominoes.length; j++) {
+//             let [c, d] = dominoes[j];
+//             if ((a === c && b === d) || (a === d && b === c)) {
+//                 pairs++;
+//             }
+//         }
+//     }
+//     return pairs;
+// };
+
+//using hash table
+var numEquivDominoPairs = function (dominoes) {
+    const countMap = new Map();
+    let pairs = 0;
+
+    for (const domino of dominoes) {
+        const key =
+            domino[0] < domino[1]
+                ? `${domino[0]}-${domino[1]}`
+                : `${domino[1]}-${domino[0]}`;
+        const count = countMap.get(key) || 0;
+
+        pairs += count;
+        countMap.set(key, count + 1);
+    }
+
+    return pairs;
+};
