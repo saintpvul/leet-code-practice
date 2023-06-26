0a536db0dbfacfe7890b5253f9b769cf277ad40b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-27 01:40:01 +0600

    feat: complete 1185. day of the week

--- 1185. Day of the Week/main.js
+++ 1185. Day of the Week/main.js
@@ -9,3 +9,18 @@ Return the answer as one of the following values {"Sunday", "Monday", "Tuesday",
 */
 
 // solution
+
+var dayOfTheWeek = function (day, month, year) {
+    const weekdays = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    const date = new Date(year, month - 1, day);
+    const dayOfWeek = date.getDay();
+    return weekdays[dayOfWeek];
+};
