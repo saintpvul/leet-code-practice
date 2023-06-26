1e49a224fa1b5ffbec21e293ea001ecbbf00cea0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-26 23:57:38 +0600

    feat: complete 1360. number of days between two dates

--- 1360. Number of Days Between Two Dates/main.js
+++ 1360. Number of Days Between Two Dates/main.js
@@ -7,3 +7,15 @@ The two dates are given as strings, their format is YYYY-MM-DD as shown in the e
 */
 
 // solution
+
+var daysBetweenDates = function (date1, date2) {
+    const oneDay = 1000 * 60 * 60 * 24;
+
+    const timestamp1 = new Date(date1).getTime();
+    const timestamp2 = new Date(date2).getTime();
+
+    const diffInMilliseconds = Math.abs(timestamp2 - timestamp1);
+    const diffInDays = Math.floor(diffInMilliseconds / oneDay);
+
+    return diffInDays;
+};
