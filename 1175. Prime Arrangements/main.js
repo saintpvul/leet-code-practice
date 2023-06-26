e14cd429a9f5191ee131af9676012c8a704a788d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-27 02:44:52 +0600

    feat: complete 1175. prime arrangements

--- 1175. Prime Arrangements/main.js
+++ 1175. Prime Arrangements/main.js
@@ -9,3 +9,39 @@ Since the answer may be large, return the answer modulo 10^9 + 7.
 */
 
 // solution
+
+var numPrimeArrangements = function (n) {
+    const kMod = 1e9 + 7;
+
+    function isPrime(num) {
+        if (num < 2) {
+            return false;
+        }
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function factorial(num) {
+        let result = BigInt(1);
+        for (let i = 2; i <= num; i++) {
+            result = (result * BigInt(i)) % BigInt(kMod);
+        }
+        return result;
+    }
+
+    let primeCount = 0;
+    for (let i = 1; i <= n; i++) {
+        if (isPrime(i)) {
+            primeCount++;
+        }
+    }
+
+    const primeFactorial = factorial(primeCount);
+    const nonPrimeFactorial = factorial(n - primeCount);
+
+    return Number((primeFactorial * nonPrimeFactorial) % BigInt(kMod));
+};
