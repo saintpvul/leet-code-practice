85ca5dfc6192782e20a685f23724603b6b25f06b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-27 01:47:40 +0600

    feat: complete 1550. three consecutive odds

--- 1550. Three Consecutive Odds/main.js
+++ 1550. Three Consecutive Odds/main.js
@@ -5,3 +5,12 @@ Given an integer array arr, return true if there are three consecutive odd numbe
 */
 
 // solution
+
+var threeConsecutiveOdds = function (arr) {
+    for (let i = 0; i < arr.length - 2; i++) {
+        if (arr[i] % 2 && arr[i + 1] % 2 && arr[i + 2] % 2) {
+            return true;
+        }
+    }
+    return false;
+};
