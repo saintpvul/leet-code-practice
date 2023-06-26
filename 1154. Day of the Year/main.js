65f565b7f4bfe63544adc6374e56dfda4e7b71d6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-26 23:54:30 +0600

    feat: complete 1154. day of the year

--- 1154. Day of the Year/main.js
+++ 1154. Day of the Year/main.js
@@ -5,3 +5,13 @@ Given a string date representing a Gregorian calendar date formatted as YYYY-MM-
 */
 
 // solution
+
+var dayOfYear = function (date) {
+    const currDate = new Date(date);
+    const startOfYear = new Date(currDate.getFullYear(), 0, 0);
+    const diff = currDate - startOfYear;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const dayOfYear = Math.floor(diff / oneDay);
+
+    return dayOfYear;
+};
