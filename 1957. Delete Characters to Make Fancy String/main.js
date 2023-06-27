2c8596bcb90f5e10406b155bbbc830155efe0c66
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-27 21:03:44 +0600

    feat: complete 1957. delete characters to make fancy string

--- 1957. Delete Characters to Make Fancy String/main.js
+++ 1957. Delete Characters to Make Fancy String/main.js
@@ -9,3 +9,24 @@ Return the final string after the deletion. It can be shown that the answer will
 */
 
 // solution
+
+var makeFancyString = function (s) {
+    let res = "";
+    let count = 1;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            count++;
+        } else {
+            count = 1;
+        }
+
+        if (count < 3) {
+            res += s[i - 1];
+        }
+    }
+
+    res += s[s.length - 1];
+
+    return res;
+};
