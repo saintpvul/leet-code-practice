d6679c9c64710bfa304d359fc88d85d492d21c4b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-29 02:34:46 +0600

    feat: start 1446. consecutive characters

---
+++ 1446. Consecutive Characters/main.js
@@ -0,0 +1,9 @@
+/*
+
+The power of the string is the maximum length of a non-empty substring that contains only one unique character.
+
+Given a string s, return the power of s.
+
+*/
+
+// solution
