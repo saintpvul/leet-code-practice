9f32958559b9eb6dc6c9f61c516ba65d23de9412
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-28 23:50:53 +0600

    feat: complete 1275. find winned on a tic tac toe game

--- 1275. Find Winner on a Tic Tac Toe Game/main.js
+++ 1275. Find Winner on a Tic Tac Toe Game/main.js
@@ -15,3 +15,63 @@ You can assume that moves is valid (i.e., it follows the rules of Tic-Tac-Toe), 
 */
 
 // solution
+
+var tictactoe = function (moves) {
+    const grid = [
+        [" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "],
+    ];
+
+    const checkWinner = (player) => {
+        for (let i = 0; i < 3; i++) {
+            if (
+                grid[i][0] === player &&
+                grid[i][1] === player &&
+                grid[i][2] === player
+            ) {
+                return true;
+            }
+        }
+
+        for (let j = 0; j < 3; j++) {
+            if (
+                grid[0][j] === player &&
+                grid[1][j] === player &&
+                grid[2][j] === player
+            ) {
+                return true;
+            }
+        }
+
+        if (
+            (grid[0][0] === player &&
+                grid[1][1] === player &&
+                grid[2][2] === player) ||
+            (grid[0][2] === player &&
+                grid[1][1] === player &&
+                grid[2][0] === player)
+        ) {
+            return true;
+        }
+
+        return false;
+    };
+
+    for (let i = 0; i < moves.length; i++) {
+        const [row, col] = moves[i];
+        const player = i % 2 === 0 ? "A" : "B";
+
+        grid[row][col] = player;
+
+        if (checkWinner(player)) {
+            return player;
+        }
+    }
+
+    if (moves.length === 9) {
+        return "Draw";
+    }
+
+    return "Pending";
+};
