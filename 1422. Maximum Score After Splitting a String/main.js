a34aaa07f6c9491cc85029433dad060e62d43f0f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-29 02:34:00 +0600

    feat: complete 1422. maximum score after splitting a string

--- 1422. Maximum Score After Splitting a String/main.js
+++ 1422. Maximum Score After Splitting a String/main.js
@@ -7,3 +7,21 @@ The score after splitting a string is the number of zeros in the left substring 
 */
 
 // solution
+
+var maxScore = function (s) {
+    let maxScore = 0;
+
+    let zerosLeft = 0;
+    let onesRight = s.split("1").length - 1;
+
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s[i] === "0") {
+            zerosLeft++;
+        } else {
+            onesRight--;
+        }
+        maxScore = Math.max(maxScore, zerosLeft + onesRight);
+    }
+
+    return maxScore;
+};
