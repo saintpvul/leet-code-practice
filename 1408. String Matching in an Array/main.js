bf27c9e4cf6a003589b0d64f76d9d6f10a0f7a2d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-29 00:34:38 +0600

    feat: start 1408. string matching in an array

---
+++ 1408. String Matching in an Array/main.js
@@ -0,0 +1,9 @@
+/*
+
+Given an array of string words, return all strings in words that is a substring of another word. You can return the answer in any order.
+
+A substring is a contiguous sequence of characters within a string
+
+*/
+
+// solution
