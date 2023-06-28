63928156e02cc17a158b97247aab28ae9f22e777
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-28 22:09:28 +0600

    feat: complete 1323. maximum 69 number

--- 1323. Maximum 69 Number/main.js
+++ 1323. Maximum 69 Number/main.js
@@ -7,3 +7,18 @@ Return the maximum number you can get by changing at most one digit (6 becomes 9
 */
 
 // solution
+
+var maximum69Number = function (num) {
+    let maxNum = num;
+    let digits = num.toString();
+    let currentNum = "";
+    for (let i = 0; i < digits.length; i++) {
+        if (digits[i] === "6") {
+            currentNum = digits.slice(0, i) + "9" + digits.slice(i + 1);
+        } else {
+            currentNum = digits.slice(0, i) + "6" + digits.slice(i + 1);
+        }
+        maxNum = Math.max(maxNum, +currentNum);
+    }
+    return maxNum;
+};
