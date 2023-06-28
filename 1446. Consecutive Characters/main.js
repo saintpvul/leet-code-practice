8189bf3ef83f2d5dccf8236198d0d48a9861b961
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-29 02:52:44 +0600

    feat: complete 1446. consecutive characters

--- 1446. Consecutive Characters/main.js
+++ 1446. Consecutive Characters/main.js
@@ -7,3 +7,18 @@ Given a string s, return the power of s.
 */
 
 // solution
+
+var maxPower = function (s) {
+    let maxPower = 1;
+    let currentPower = 1;
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            currentPower++;
+            maxPower = Math.max(maxPower, currentPower);
+        } else {
+            currentPower = 1;
+        }
+    }
+    return maxPower;
+};
