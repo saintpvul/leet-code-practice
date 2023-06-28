7540aa45ba48c84e72a8829ca14feebe88195fa8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-29 02:59:59 +0600

    feat: complete 1437. check if all 1's are at least length k places away

--- 1437. Check If All 1's Are at Least Length K Places Away/main.js
+++ 1437. Check If All 1's Are at Least Length K Places Away/main.js
@@ -5,3 +5,20 @@ Given an binary array nums and an integer k, return true if all 1's are at least
 */
 
 // solution
+
+var kLengthApart = function (nums, k) {
+    let count = k;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            if (count < k) {
+                return false;
+            }
+            count = 0;
+        } else {
+            count++;
+        }
+    }
+
+    return true;
+};
