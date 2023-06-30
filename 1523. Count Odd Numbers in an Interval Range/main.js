1e830ac222d2df84a5de0710271a6be8ec28ee4f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-30 23:18:49 +0600

    feat: complete 1523. count odd numbers in an interval range

--- 1523. Count Odd Numbers in an Interval Range/main.js
+++ 1523. Count Odd Numbers in an Interval Range/main.js
@@ -5,3 +5,17 @@ Given two non-negative integers low and high. Return the count of odd numbers be
 */
 
 // solution
+
+// var countOdds = function (low, high) {
+//     let oddCounter = 0;
+//     for (let i = low; i <= high; i++) {
+//         if (i % 2) {
+//             oddCounter++;
+//         }
+//     }
+//     return oddCounter;
+// };
+
+var countOdds = function (low, high) {
+    return Math.floor((high + 1) / 2) - Math.floor(low / 2);
+};
