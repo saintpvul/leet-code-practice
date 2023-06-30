65a52ab40f5fdc68108be31746803c92b6c6fab2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-30 23:53:57 +0600

    feat: complete 1544. make the string great

--- 1544. Make The String Great/main.js
+++ 1544. Make The String Great/main.js
@@ -15,3 +15,23 @@ Notice that an empty string is also good.
 */
 
 // solution
+
+var makeGood = function (s) {
+    const stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (stack.length > 0 && isBadPair(stack[stack.length - 1], char)) {
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join("");
+};
+
+function isBadPair(char1, char2) {
+    return char1 !== char2 && char1.toLowerCase() === char2.toLowerCase();
+}
