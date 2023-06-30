a28a193abcbda3b95b8ff4c2042899a175acd520
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-06-30 23:21:35 +0600

    feat: complete 1556. thousand separator

--- 1556. Thousand Separator/main.js
+++ 1556. Thousand Separator/main.js
@@ -5,3 +5,7 @@ Given an integer n, add a dot (".") as the thousands separator and return it in 
 */
 
 // solution
+
+var thousandSeparator = function (n) {
+    return n.toLocaleString().replace(/[,]/gi, ".");
+};
