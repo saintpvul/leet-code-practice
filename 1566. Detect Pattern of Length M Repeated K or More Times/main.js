198cea8e55f624693fc78240db950e44af97d2ef
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-01 00:21:20 +0600

    feat: complete 1566. detect pattern of length m repeated k or more times

--- 1566. Detect Pattern of Length M Repeated K or More Times/main.js
+++ 1566. Detect Pattern of Length M Repeated K or More Times/main.js
@@ -9,3 +9,41 @@ Return true if there exists a pattern of length m that is repeated k or more tim
 */
 
 // solution
+
+var containsPattern = function (arr, m, k) {
+    const n = arr.length;
+
+    for (let i = 0; i + m * k <= n; ++i) {
+        const pattern = arr.slice(i, i + m);
+        let found = true;
+
+        for (let t = 1; t < k; ++t) {
+            const subarray = arr.slice(i + m * t, i + m * (t + 1));
+
+            if (!arraysEqual(pattern, subarray)) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+function arraysEqual(a, b) {
+    if (a.length !== b.length) {
+        return false;
+    }
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
