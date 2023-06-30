526a94f443ce915c41ce6194be3f43786d777efd
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-01 00:35:17 +0600

    feat: complete 1582. speacial positions in a binary matrix

--- 1582. Special Positions in a Binary Matrix/main.js
+++ 1582. Special Positions in a Binary Matrix/main.js
@@ -7,3 +7,38 @@ A position (i, j) is called special if mat[i][j] == 1 and all other elements in 
 */
 
 // solution
+
+var numSpecial = function (mat) {
+    const m = mat.length;
+    const n = mat[0].length;
+
+    let count = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 1) {
+                let isSpecial = true;
+
+                for (let col = 0; col < n; col++) {
+                    if (col !== j && mat[i][col] === 1) {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                for (let row = 0; row < m; row++) {
+                    if (row !== i && mat[row][j] === 1) {
+                        isSpecial = false;
+                        break;
+                    }
+                }
+
+                if (isSpecial) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+};
