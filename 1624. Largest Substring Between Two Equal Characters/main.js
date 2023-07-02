6103d308b63f2e1bd17f2d08583f947db1cf1c7b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-02 23:50:45 +0600

    feat: complete 1624. largest substring between two equal characters

--- 1624. Largest Substring Between Two Equal Characters/main.js
+++ 1624. Largest Substring Between Two Equal Characters/main.js
@@ -7,3 +7,20 @@ A substring is a contiguous sequence of characters within a string.
 */
 
 // solution
+
+var maxLengthBetweenEqualCharacters = function (s) {
+    let maxLength = -1;
+    const charMap = {};
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (charMap.hasOwnProperty(char)) {
+            maxLength = Math.max(maxLength, i - charMap[char] - 1);
+        } else {
+            charMap[char] = i;
+        }
+    }
+
+    return maxLength;
+};
