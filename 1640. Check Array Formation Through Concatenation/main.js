ea8ae41da60e3680f1a700a8bed88daf41ef1ecc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-02 23:41:12 +0600

    feat: complete 1640. check array formation through concatenation

--- 1640. Check Array Formation Through Concatenation/main.js
+++ 1640. Check Array Formation Through Concatenation/main.js
@@ -7,3 +7,23 @@ Return true if it is possible to form the array arr from pieces. Otherwise, retu
 */
 
 // solution
+
+var canFormArray = function (arr, pieces) {
+    const map = new Map();
+    arr.forEach((num, index) => map.set(num, index));
+
+    for (const piece of pieces) {
+        if (!map.has(piece[0])) {
+            return false;
+        }
+
+        const startIndex = map.get(piece[0]);
+        for (let i = 0; i < piece.length; i++) {
+            if (arr[startIndex + i] !== piece[i]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
