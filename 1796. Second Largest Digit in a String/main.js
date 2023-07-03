1fca79a74e3ea0a1a9edd55b26cf103272154a55
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-04 00:29:23 +0600

    feat: complete 1796. second largest digit in a string

--- 1796. Second Largest Digit in a String/main.js
+++ 1796. Second Largest Digit in a String/main.js
@@ -7,3 +7,10 @@ An alphanumeric string is a string consisting of lowercase English letters and d
 */
 
 // solution
+
+var secondHighest = function (s) {
+    let digits = Array.from(
+        new Set(s.slice().replace(/\D/gi, "").split(""))
+    ).sort((a, b) => b - a);
+    return digits[1] ? digits[1] : -1;
+};
