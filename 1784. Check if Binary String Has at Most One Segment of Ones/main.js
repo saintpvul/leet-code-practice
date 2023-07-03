1da62e414bd5336a411cafd6dec5a1156c31570e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-04 00:34:20 +0600

    feat: complete 1784. check if binary string has at most one segment of ones

--- 1784. Check if Binary String Has at Most One Segment of Ones/main.js
+++ 1784. Check if Binary String Has at Most One Segment of Ones/main.js
@@ -1,7 +1,21 @@
 /*
 
-
+Given a binary string s ​​​​​without leading zeros, return true​​​ if s contains at most one contiguous segment of ones. Otherwise, return false.
 
 */
 
 // solution
+
+var checkOnesSegment = function (s) {
+    let segmentFound = false;
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === "1" && s[i - 1] === "0") {
+            if (segmentFound) {
+                return false;
+            }
+        } else if (s[i] === "0" && s[i - 1] === "1") {
+            segmentFound = true;
+        }
+    }
+    return true;
+};
