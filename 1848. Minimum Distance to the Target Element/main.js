87f7e398dd4dc7bae9f624334f7b572f5f9043d8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-05 02:07:17 +0600

    feat: complete 1848. minimum distance to the target element

--- 1848. Minimum Distance to the Target Element/main.js
+++ 1848. Minimum Distance to the Target Element/main.js
@@ -9,3 +9,15 @@ It is guaranteed that target exists in nums.
 */
 
 // solution
+
+var getMinDistance = function (nums, target, start) {
+    let minDistance = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            minDistance = Math.min(minDistance, Math.abs(i - start));
+        }
+    }
+
+    return minDistance;
+};
