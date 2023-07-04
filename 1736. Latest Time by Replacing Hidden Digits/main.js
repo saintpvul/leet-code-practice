e1e6a960fdc15b518d47c35d0a797a4d60476c2c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-04 19:11:33 +0600

    feat: complete 1736. latest time by replacing hidden digits

--- 1736. Latest Time by Replacing Hidden Digits/main.js
+++ 1736. Latest Time by Replacing Hidden Digits/main.js
@@ -9,3 +9,33 @@ Return the latest valid time you can get from time by replacing the hidden digit
 */
 
 // solution
+
+var maximumTime = function (time) {
+    let res = "";
+
+    for (let i = 0; i < time.length; i++) {
+        if (time[i] === "?") {
+            if (i === 0) {
+                if (time[1] === "?" || time[1] < "4") {
+                    res += "2";
+                } else {
+                    res += "1";
+                }
+            } else if (i === 1) {
+                if (res[0] === "2") {
+                    res += "3";
+                } else {
+                    res += "9";
+                }
+            } else if (i === 3) {
+                res += "5";
+            } else if (i === 4) {
+                res += "9";
+            }
+        } else {
+            res += time[i];
+        }
+    }
+
+    return res;
+};
