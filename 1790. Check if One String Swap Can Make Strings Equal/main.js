175d5e01fe217d81bc156d35c23f7d6ae307d8e6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-04 20:01:05 +0600

    feat: complete 1790. check if one string swap can make strings equal

--- 1790. Check if One String Swap Can Make Strings Equal/main.js
+++ 1790. Check if One String Swap Can Make Strings Equal/main.js
@@ -7,3 +7,41 @@ Return true if it is possible to make both strings equal by performing at most o
 */
 
 // solution
+
+var areAlmostEqual = function (s1, s2) {
+    if (s1 === s2) {
+        return true;
+    }
+
+    if (s1.length !== s2.length) {
+        return false;
+    }
+
+    let firstIndex = -1;
+    let secondIndex = -1;
+
+    for (let i = 0; i < s1.length; i++) {
+        if (s1[i] !== s2[i]) {
+            if (firstIndex === -1) {
+                firstIndex = i;
+            } else if (secondIndex === -1) {
+                secondIndex = i;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    if (firstIndex !== -1 && secondIndex !== -1) {
+        let s1Array = s1.split("");
+        let s2Array = s2.split("");
+
+        let temp = s1Array[firstIndex];
+        s1Array[firstIndex] = s1Array[secondIndex];
+        s1Array[secondIndex] = temp;
+
+        return s1Array.join("") === s2Array.join("");
+    }
+
+    return false;
+};
