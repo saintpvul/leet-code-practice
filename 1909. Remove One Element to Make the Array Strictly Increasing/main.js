5d3bde0906a2227636ab75d61d208c6801c392ff
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-05 02:18:30 +0600

    feat: complete 1909. remove one element to make the array strictly increasing

--- 1909. Remove One Element to Make the Array Strictly Increasing/main.js
+++ 1909. Remove One Element to Make the Array Strictly Increasing/main.js
@@ -7,3 +7,22 @@ The array nums is strictly increasing if nums[i - 1] < nums[i] for each index (1
 */
 
 // solution
+
+var canBeIncreasing = function (nums) {
+    let removed = false;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] <= nums[i - 1]) {
+            if (removed) {
+                return false;
+            }
+
+            if (i > 1 && nums[i] <= nums[i - 2]) {
+                nums[i] = nums[i - 1];
+            }
+
+            removed = true;
+        }
+    }
+
+    return true;
+};
