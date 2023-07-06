918ce470d52dbe9131c0139d90a8f510411226ed
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-06 23:58:54 +0600

    feat: start 1903. largest odd number in string

---
+++ 1903. Largest Odd Number in String/main.js
@@ -0,0 +1,9 @@
+/*
+
+You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
+
+A substring is a contiguous sequence of characters within a string.
+
+*/
+
+// solution
