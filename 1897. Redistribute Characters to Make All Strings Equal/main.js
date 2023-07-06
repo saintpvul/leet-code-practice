5792239ac4e316116fb22eaa7f603fba5ddb928e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-06 23:56:31 +0600

    feat: complete 1897. redistibute characters to make all strings equal

--- 1897. Redistribute Characters to Make All Strings Equal/main.js
+++ 1897. Redistribute Characters to Make All Strings Equal/main.js
@@ -1,7 +1,32 @@
 /*
 
+You are given an array of strings words (0-indexed).
 
+In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+
+Return true if you can make every string in words equal using any number of operations, and false otherwise.
 
 */
 
 // solution
+
+var makeEqual = function (words) {
+    if (words.length === 1) return true;
+
+    const charCount = new Map();
+    const wordCount = words.length;
+
+    for (let word of words) {
+        for (let char of word) {
+            charCount.set(char, (charCount.get(char) || 0) + 1);
+        }
+    }
+
+    for (let count of charCount.values()) {
+        if (count % wordCount !== 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
