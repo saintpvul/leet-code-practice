749d2b6d757d1dfc19a0aab4075e99c2c14c7e75
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-07 02:18:04 +0600

    feat: start 1952. three divisors

---
+++ 1952. Three Divisors/main.js
@@ -0,0 +1,9 @@
+/*
+
+Given an integer n, return true if n has exactly three positive divisors. Otherwise, return false.
+
+An integer m is a divisor of n if there exists an integer k such that n = k * m.
+ 
+*/
+
+// solution
