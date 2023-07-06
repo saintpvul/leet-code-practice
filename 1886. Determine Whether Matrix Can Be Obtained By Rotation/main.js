a785816859b7f4a1bbae90aa3924464266c1aff0
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-07 00:57:33 +0600

    feat: complete 1886. determine whether matrix can be obtained by rotation

--- 1886. Determine Whether Matrix Can Be Obtained By Rotation/main.js
+++ 1886. Determine Whether Matrix Can Be Obtained By Rotation/main.js
@@ -5,3 +5,27 @@ Given two n x n binary matrices mat and target, return true if it is possible to
 */
 
 // solution
+
+var findRotation = function (mat, target) {
+    const rotateMatrix = (matrix) => {
+        const n = matrix.length;
+        const rotated = Array.from({ length: n }, () => Array(n));
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                rotated[i][j] = matrix[n - j - 1][i];
+            }
+        }
+
+        return rotated;
+    };
+
+    for (let i = 0; i < 4; i++) {
+        if (mat.toString() === target.toString()) {
+            return true;
+        }
+        mat = rotateMatrix(mat);
+    }
+
+    return false;
+};
