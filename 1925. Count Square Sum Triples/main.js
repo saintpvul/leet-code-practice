87c4419c27863b4b5f899706a45dee8688921dc2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-07 00:39:30 +0600

    feat: complete 1925. count square sum triples

--- 1925. Count Square Sum Triples/main.js
+++ 1925. Count Square Sum Triples/main.js
@@ -7,3 +7,19 @@ Given an integer n, return the number of square triples such that 1 <= a, b, c <
 */
 
 // solution
+
+var countTriples = function (n) {
+    let count = 0;
+
+    for (let a = 1; a <= n; a++) {
+        for (let b = 1; b <= n; b++) {
+            for (let c = 1; c <= n; c++) {
+                if (a * a + b * b === c * c) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+};
