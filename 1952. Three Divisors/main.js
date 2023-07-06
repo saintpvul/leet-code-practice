ff71a5037202c6b0b2dcf2145f8967e770def8bc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-07 02:21:58 +0600

    feat: complete 1952. three divisors

--- 1952. Three Divisors/main.js
+++ 1952. Three Divisors/main.js
@@ -7,3 +7,13 @@ An integer m is a divisor of n if there exists an integer k such that n = k * m.
 */
 
 // solution
+
+var isThree = function (n) {
+    let countDivisors = 0;
+    for (let i = n; i > 0; i--) {
+        if (n % i === 0) {
+            countDivisors++;
+        }
+    }
+    return countDivisors === 3;
+};
