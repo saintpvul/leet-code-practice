4ce50c83eb95710e2b250ba7e41b8857a6387e9e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-08 23:55:55 +0600

    feat: complete 2016. maximum difference between increasing elements

--- 2016. Maximum Difference Between Increasing Elements/main.js
+++ 2016. Maximum Difference Between Increasing Elements/main.js
@@ -7,3 +7,18 @@ Return the maximum difference. If no such i and j exists, return -1.
 */
 
 // solution
+
+var maximumDifference = function (nums) {
+    let maxDiff = -1;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                let currDiff = nums[j] - nums[i];
+                if (currDiff > maxDiff) {
+                    maxDiff = currDiff;
+                }
+            }
+        }
+    }
+    return maxDiff === -1 ? -1 : maxDiff;
+};
