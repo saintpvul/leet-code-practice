08a916b38f72509b5aa5d0c41ffadf9f913a704f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-11 05:34:55 +0600

    feat: complete 2180. count integers with even digit sum

--- 2180. Count Integers With Even Digit Sum/main.js
+++ 2180. Count Integers With Even Digit Sum/main.js
@@ -7,3 +7,17 @@ The digit sum of a positive integer is the sum of all its digits.
 */
 
 //
+
+var countEven = function (num) {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+        let sum = i
+            .toString()
+            .split("")
+            .reduce((s, v) => s + +v, 0);
+        if (sum % 2 === 0) {
+            count++;
+        }
+    }
+    return count;
+};
