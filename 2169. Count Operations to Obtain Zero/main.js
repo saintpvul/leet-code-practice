d69193e12b73d1244cec819a8804b3a39bbbf1a1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-11 05:44:31 +0600

    feat: complete 2169. count operations to obtain zero

--- 2169. Count Operations to Obtain Zero/main.js
+++ 2169. Count Operations to Obtain Zero/main.js
@@ -10,3 +10,17 @@ Return the number of operations required to make either num1 = 0 or num2 = 0.
 */
 
 // solution
+
+var countOperations = function (num1, num2) {
+    let operations = 0;
+    while (num1 > 0 && num2 > 0) {
+        if (num1 >= num2) {
+            num1 -= num2;
+        } else {
+            num2 -= num1;
+        }
+        operations++;
+    }
+
+    return operations;
+};
