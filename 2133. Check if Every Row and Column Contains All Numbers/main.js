73b52fc38e9d4acc43c1dfae55d62dadf17e4ef8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-11 05:00:31 +0600

    feat: complete 2133. check if every row and column contains all numbers

--- 2133. Check if Every Row and Column Contains All Numbers/main.js
+++ 2133. Check if Every Row and Column Contains All Numbers/main.js
@@ -7,3 +7,62 @@ Given an n x n integer matrix matrix, return true if the matrix is valid. Otherw
 */
 
 // solution
+
+// var checkValid = function (matrix) {
+//     let n = matrix[0].length;
+
+//     let colMatrix = [];
+
+//     for (let i = 0; i < n; i++) {
+//         let currRow = [];
+//         for (let j = 0; j < matrix.length; j++) {
+//             currRow.push(matrix[j][i]);
+//         }
+//         colMatrix.push(currRow);
+//     }
+
+//     for (let i = 0; i < matrix.length; i++) {
+//         let numbers = new Map(
+//             Array.from({ length: n }, (_, i) => [i + 1, false])
+//         );
+//         let rowNumbers = new Set(matrix[i]);
+//         let colNumbers = new Set(colMatrix[i]);
+
+//         for (let j = 0; j < n; j++) {
+//             let number = matrix[i][j];
+//             if (numbers.has(number) && !numbers.get(number)) {
+//                 numbers.set(number, true);
+//             }
+//         }
+
+//         for (let j = 1; j <= n; j++) {
+//             if (!rowNumbers.has(j) || !colNumbers.has(j) || !numbers.get(j)) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return true;
+// };
+
+// transposing values
+
+var checkValid = function (matrix) {
+    const n = matrix.length;
+
+    const transposedMatrix = matrix.map((row, i) =>
+        row.map((_, j) => matrix[j][i])
+    );
+
+    for (let i = 0; i < n; i++) {
+        const rowNumbers = new Set(matrix[i]);
+        const colNumbers = new Set(transposedMatrix[i]);
+
+        for (let j = 1; j <= n; j++) {
+            if (!rowNumbers.has(j) || !colNumbers.has(j)) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
