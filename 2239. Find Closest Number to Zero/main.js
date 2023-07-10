692359cb31c19d275064905502e17e25e5d0ec3d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-11 04:42:50 +0600

    feat: complete 2239. find closest number to zero

--- 2239. Find Closest Number to Zero/main.js
+++ 2239. Find Closest Number to Zero/main.js
@@ -5,3 +5,17 @@ Given an integer array nums of size n, return the number with the value closest 
 */
 
 // solution
+
+var findClosestNumber = function (nums) {
+    let closestPos = Infinity;
+    let closestNeg = -Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < closestPos && nums[i] >= 0) {
+            closestPos = nums[i];
+        } else if (nums[i] > closestNeg && nums[i] < 0) {
+            closestNeg = nums[i];
+        }
+    }
+    return closestPos <= Math.abs(closestNeg) ? closestPos : closestNeg;
+};
