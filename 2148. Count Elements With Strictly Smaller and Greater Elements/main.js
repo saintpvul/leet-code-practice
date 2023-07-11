3a7371c53e9461782c33000a41fb91d02cb03d9d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-11 06:08:12 +0600

    feat: complete 2148. count elements with smaller and greater elements

--- 2148. Count Elements With Strictly Smaller and Greater Elements/main.js
+++ 2148. Count Elements With Strictly Smaller and Greater Elements/main.js
@@ -5,3 +5,26 @@ Given an integer array nums, return the number of elements that have both a stri
 */
 
 // solution
+
+var countElements = function (nums) {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        let smaller = false;
+        let greater = false;
+
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                smaller = true;
+            } else if (nums[j] > nums[i]) {
+                greater = true;
+            }
+        }
+
+        if (smaller && greater) {
+            count++;
+        }
+    }
+
+    return count;
+};
