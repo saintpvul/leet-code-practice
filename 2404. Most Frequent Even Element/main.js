e6a1c99745db5619081e32e89fafa18d3545ec9b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-12 23:32:42 +0600

    feat: complete 2404. most frequent even element

--- 2404. Most Frequent Even Element/main.js
+++ 2404. Most Frequent Even Element/main.js
@@ -7,3 +7,26 @@ If there is a tie, return the smallest one. If there is no such element, return 
 */
 
 // solution
+
+var mostFrequentEven = function (nums) {
+    let frequency = {};
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (num % 2 === 0) {
+            frequency[num] = frequency[num] + 1 || 1;
+        }
+    }
+    let mostFrequentOne = -1;
+    let frequencyCount = 0;
+
+    for (let [num, count] of Object.entries(frequency)) {
+        if (frequencyCount === count) {
+            mostFrequentOne = Math.min(num, mostFrequentOne);
+        } else if (count > frequencyCount) {
+            frequencyCount = count;
+            mostFrequentOne = num;
+        }
+    }
+
+    return parseInt(mostFrequentOne);
+};
