f82e0fb2e557b2dadc001bddd7155209fb8671f7
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-12 22:56:38 +0600

    feat: complete 2231. largest number after digit swaps by parity

--- 2231. Largest Number After Digit Swaps by Parity/main.js
+++ 2231. Largest Number After Digit Swaps by Parity/main.js
@@ -7,3 +7,36 @@ Return the largest possible value of num after any number of swaps.
 */
 
 // solution
+
+var largestInteger = function (num) {
+    num = num.toString();
+    let digits = num.split("").map(Number);
+    let evens = digits.filter((digit) => digit % 2 === 0).sort((a, b) => b - a);
+    let odds = digits.filter((digit) => digit % 2 === 1).sort((a, b) => b - a);
+    let res = [];
+
+    let i = 0;
+    let j = 0;
+
+    for (let k = 0; k < digits.length; k++) {
+        if (digits[k] % 2 === 0) {
+            if (j < evens.length) {
+                res.push(evens[j]);
+                j++;
+            } else {
+                res.push(odds[i]);
+                i++;
+            }
+        } else {
+            if (i < odds.length) {
+                res.push(odds[i]);
+                i++;
+            } else {
+                res.push(evens[j]);
+                j++;
+            }
+        }
+    }
+
+    return parseInt(res.join(""));
+};
