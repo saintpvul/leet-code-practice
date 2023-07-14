66cb13c9089f453c2f7312b4a787830222490ece
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-14 13:47:19 +0600

    feat: start 2319. check if matrix is x-matrix

---
+++ 2319. Check if Matrix Is X-Matrix/main.js
@@ -0,0 +1,11 @@
+/*
+
+A square matrix is said to be an X-Matrix if both of the following conditions hold:
+
+All the elements in the diagonals of the matrix are non-zero.
+All other elements are 0.
+Given a 2D integer array grid of size n x n representing a square matrix, return true if grid is an X-Matrix. Otherwise, return false.
+
+*/
+
+// solution
