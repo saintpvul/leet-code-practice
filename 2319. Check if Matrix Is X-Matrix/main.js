86ed9b73960bf5426b7c54aaf1500b30258c450a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-14 14:09:18 +0600

    feat: complete 2319. check if matrix is x-matrix

--- 2319. Check if Matrix Is X-Matrix/main.js
+++ 2319. Check if Matrix Is X-Matrix/main.js
@@ -9,3 +9,20 @@ Given a 2D integer array grid of size n x n representing a square matrix, return
 */
 
 // solution
+
+var checkXMatrix = function (grid) {
+    const n = grid.length;
+
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n; ++j) {
+            if (i === j || i + j === n - 1) {
+                if (!grid[i][j]) {
+                    return false;
+                }
+            } else if (grid[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
