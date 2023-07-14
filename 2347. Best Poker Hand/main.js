174a50a07cfb69753d04a7517635a38154aed057
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-14 14:19:16 +0600

    feat: complete 2347. best poker hand

--- 2347. Best Poker Hand/main.js
+++ 2347. Best Poker Hand/main.js
@@ -15,3 +15,21 @@ Note that the return values are case-sensitive.
 */
 
 // solution
+
+var bestHand = function (ranks, suits) {
+    if (suits.every((v) => v === suits[0])) {
+        return "Flush";
+    }
+    const count = new Array(14).fill(0);
+    let isPair = false;
+    for (const v of ranks) {
+        if (++count[v] === 3) {
+            return "Three of a Kind";
+        }
+        isPair = isPair || count[v] === 2;
+    }
+    if (isPair) {
+        return "Pair";
+    }
+    return "High Card";
+};
