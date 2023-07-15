48464201ecdc05aecbbdc16675a925cdd4241dca
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-15 23:17:10 +0600

    feat: complete 2427. number of common factors

--- 2427. Number of Common Factors/main.js
+++ 2427. Number of Common Factors/main.js
@@ -7,3 +7,14 @@ An integer x is a common factor of a and b if x divides both a and b.
 */
 
 // solution
+
+var commonFactors = function (a, b) {
+    let count = 0;
+
+    for (let i = Math.max(a, b); i > 0; i--) {
+        if (a % i === 0 && b % i === 0) {
+            count++;
+        }
+    }
+    return count;
+};
