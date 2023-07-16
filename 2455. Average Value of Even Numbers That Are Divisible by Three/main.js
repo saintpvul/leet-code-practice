66dd876ada4ece2d4bd18b09c4fe6e9471daab4c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-16 22:38:29 +0600

    feat: complete 2455. average value of even numbers that are divisible by three

--- 2455. Average Value of Even Numbers That Are Divisible by Three/main.js
+++ 2455. Average Value of Even Numbers That Are Divisible by Three/main.js
@@ -7,3 +7,16 @@ Note that the average of n elements is the sum of the n elements divided by n an
 */
 
 // solution
+
+var averageValue = function (nums) {
+    let evensSum = 0;
+    let evensCount = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0 && nums[i] % 3 === 0) {
+            evensSum += nums[i];
+            evensCount++;
+        }
+    }
+    return evensSum > 0 ? Math.floor(evensSum / evensCount) : 0;
+};
