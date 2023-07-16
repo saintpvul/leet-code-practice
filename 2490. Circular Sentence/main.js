a48cb91bf7d18188c2963088f793ddcc81088d1c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-17 00:32:49 +0600

    feat: complete 2490. circular sentence

--- 2490. Circular Sentence/main.js
+++ 2490. Circular Sentence/main.js
@@ -16,3 +16,18 @@ Given a string sentence, return true if it is circular. Otherwise, return false.
 */
 
 // solution
+
+var isCircularSentence = function (sentence) {
+    const words = sentence.split(" ");
+
+    for (let i = 0; i < words.length; i++) {
+        const currentWord = words[i];
+        const nextWord = words[(i + 1) % words.length];
+
+        if (currentWord[currentWord.length - 1] !== nextWord[0]) {
+            return false;
+        }
+    }
+
+    return true;
+};
