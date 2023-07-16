1f9b81b85f2ad1b18b45f4a8f52bdf60d3b46e0c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-17 00:28:52 +0600

    feat: complete 2520. count the digits that divide a number

--- 2520. Count the Digits That Divide a Number/main.js
+++ 2520. Count the Digits That Divide a Number/main.js
@@ -7,3 +7,15 @@ An integer val divides nums if nums % val == 0.
 */
 
 // solution
+
+var countDigits = function (num) {
+    const strNum = num.toString();
+
+    let count = 0;
+    for (let i = 0; i < strNum.length; i++) {
+        if (num % Number(strNum[i]) === 0) {
+            count++;
+        }
+    }
+    return count;
+};
