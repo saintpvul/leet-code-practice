30dc51556eecd6dc17e73638d0d4838691361743
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-17 00:19:37 +0600

    feat: complete 2446. determine if two events have conflict

--- 2446. Determine if Two Events Have Conflict/main.js
+++ 2446. Determine if Two Events Have Conflict/main.js
@@ -13,3 +13,39 @@ Return true if there is a conflict between two events. Otherwise, return false.
 */
 
 // solution
+
+// var haveConflict = function (event1, event2) {
+//     const [startTime1, endTime1] = event1;
+//     const [startTime2, endTime2] = event2;
+
+//     const timeToMinutes = (time) => {
+//         const [hours, minutes] = time.split(":").map(Number);
+//         return hours * 60 + minutes;
+//     };
+
+//     const start1 = timeToMinutes(startTime1);
+//     const end1 = timeToMinutes(endTime1);
+//     const start2 = timeToMinutes(startTime2);
+//     const end2 = timeToMinutes(endTime2);
+
+//     if (
+//         (start1 >= start2 && start1 <= end2) ||
+//         (start2 >= start1 && start2 <= end1) ||
+//         (start1 <= start2 && end1 >= end2) ||
+//         (start2 <= start1 && end2 >= end1)
+//     ) {
+//         return true;
+//     }
+
+//     return false;
+// };
+
+var haveConflict = function (event1, event2) {
+    const [start1, end1] = event1;
+    const [start2, end2] = event2;
+
+    return (
+        (start1 <= start2 && start2 <= end1) ||
+        (start2 <= start1 && start1 <= end2)
+    );
+};
