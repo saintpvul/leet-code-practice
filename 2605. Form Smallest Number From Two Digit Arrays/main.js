f076b636afbdab20c3b2b270e94239a11f46f4a8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-19 15:09:53 +0600

    feat: complete 2605. form smallest number from two digit arrays

--- 2605. Form Smallest Number From Two Digit Arrays/main.js
+++ 2605. Form Smallest Number From Two Digit Arrays/main.js
@@ -5,3 +5,29 @@ Given two arrays of unique digits nums1 and nums2, return the smallest number th
 */
 
 // solution
+
+var minNumber = function (nums1, nums2) {
+    let min1 = Infinity;
+    let min2 = Infinity;
+
+    let equals = new Set();
+    for (let i = 0; i < nums1.length; i++) {
+        min1 = Math.min(nums1[i], min1);
+        if (nums2.includes(nums1[i])) {
+            equals.add(nums1[i]);
+        }
+    }
+    for (let i = 0; i < nums2.length; i++) {
+        min2 = Math.min(nums2[i], min2);
+    }
+    let min3 = equals.size > 0 ? Math.min(...Array.from(equals)) : null;
+
+    if (min3 !== null) return min3;
+    else {
+        if (min1 < min2) {
+            return Number(min1 + "" + min2);
+        } else {
+            return Number(min2 + "" + min1);
+        }
+    }
+};
