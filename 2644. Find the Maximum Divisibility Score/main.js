bf257f3f1f69a947f4b36bb0e14062a839f2ad52
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-19 16:14:55 +0600

    feat: complete 2644. find the maximum divisibility score

--- 2644. Find the Maximum Divisibility Score/main.js
+++ 2644. Find the Maximum Divisibility Score/main.js
@@ -9,3 +9,21 @@ Return the integer divisors[i] with the maximum divisibility score. If there is 
 */
 
 // solution
+
+var maxDivScore = function (nums, divisors) {
+    let maxCount = 0;
+    let maxDiv = Infinity;
+    for (let i = 0; i < divisors.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] % divisors[i] === 0) {
+                count++;
+            }
+        }
+        if (count > maxCount || (count === maxCount && divisors[i] < maxDiv)) {
+            maxCount = count;
+            maxDiv = divisors[i];
+        }
+    }
+    return maxDiv;
+};
