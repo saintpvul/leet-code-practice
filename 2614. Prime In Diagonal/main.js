4e950f6cbe62ab7ab966f89ded1b08393e08d59c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-20 23:38:20 +0600

    feat: complete 2614. prime in diagonal

--- 2614. Prime In Diagonal/main.js
+++ 2614. Prime In Diagonal/main.js
@@ -15,3 +15,29 @@ In the above diagram, one diagonal is [1,5,9] and another diagonal is [3,5,7].
 */
 
 // solution
+
+var diagonalPrime = function (nums) {
+    function isPrime(x) {
+        if (x < 2) {
+            return false;
+        }
+        for (let i = 2; i <= Math.sqrt(x); i++) {
+            if (x % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    const n = nums.length;
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        if (isPrime(nums[i][i])) {
+            ans = Math.max(ans, nums[i][i]);
+        }
+        if (isPrime(nums[i][n - i - 1])) {
+            ans = Math.max(ans, nums[i][n - i - 1]);
+        }
+    }
+    return ans;
+};
