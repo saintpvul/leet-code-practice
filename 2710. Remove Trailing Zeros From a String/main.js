155dec9bbfde76c620b5924a5cb66496f5e5fd9f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-21 21:59:57 +0600

    feat: complete 2710. remove trailing zeros from a string

--- 2710. Remove Trailing Zeros From a String/main.js
+++ 2710. Remove Trailing Zeros From a String/main.js
@@ -5,3 +5,15 @@ Given a positive integer num represented as a string, return the integer num wit
 */
 
 // solution
+
+// var removeTrailingZeros = function (num) {
+//     return num.replace(/0+$/, "");
+// };
+
+var removeTrailingZeros = function (num) {
+    for (let i = num.length - 1; i >= 0; i--) {
+        if (num[i] != "0") {
+            return num.slice(0, i + 1);
+        }
+    }
+};
