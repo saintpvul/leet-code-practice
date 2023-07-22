c548dbfc6f91bbfc277021c5c267566f800f3760
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-22 23:39:20 +0600

    feat: complete 2778. sum of squares of speacial elements

--- 2778. Sum of Squares of Special Elements/main.js
+++ 2778. Sum of Squares of Special Elements/main.js
@@ -9,3 +9,34 @@ Return the sum of the squares of all special elements of nums.
 */
 
 // solution
+
+// var sumOfSquares = function (nums) {
+//     let elements = [];
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums.length % (i + 1) === 0) {
+//             elements.push(nums[i]);
+//         }
+//     }
+
+//     return elements.reduce((s, e) => s + e ** 2, 0);
+// };
+
+// var sumOfSquares = function (nums) {
+//     return nums.reduce(
+//         (s, e, i) => s + (nums.length % (i + 1) === 0 ? e ** 2 : 0),
+//         0
+//     );
+// };
+
+var sumOfSquares = function (nums) {
+    let n = nums.length;
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (n % (i + 1) === 0) {
+            sum += nums[i] ** 2;
+        }
+    }
+    return sum;
+};
