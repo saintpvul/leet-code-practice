f1489490863c0f7fe11280c02a2985ac33cc43eb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-23 23:27:19 +0600

    feat: start 2765. longest alternating subarray

---
+++ 2765. Longest Alternating Subarray/main.js
@@ -0,0 +1,14 @@
+/*
+
+You are given a 0-indexed integer array nums. A subarray s of length m is called alternating if:
+
+m is greater than 1.
+s1 = s0 + 1.
+The 0-indexed subarray s looks like [s0, s1, s0, s1,...,s(m-1) % 2]. In other words, s1 - s0 = 1, s2 - s1 = -1, s3 - s2 = 1, s4 - s3 = -1, and so on up to s[m - 1] - s[m - 2] = (-1)m.
+Return the maximum length of all alternating subarrays present in nums or -1 if no such subarray exists.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+*/
+
+// solution
