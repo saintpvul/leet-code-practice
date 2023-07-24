66602bda71de2cfe5435564a4394e18f4b8243e8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-24 20:29:40 +0600

    feat: complete 12. integer to roman

--- 12. Integer to Roman/main.js
+++ 12. Integer to Roman/main.js
@@ -22,3 +22,32 @@ Given an integer, convert it to a roman numeral.
 */
 
 // solution
+
+var intToRoman = function (num) {
+    const roman = [
+        { value: 1000, numeral: "M" },
+        { value: 900, numeral: "CM" },
+        { value: 500, numeral: "D" },
+        { value: 400, numeral: "CD" },
+        { value: 100, numeral: "C" },
+        { value: 90, numeral: "XC" },
+        { value: 50, numeral: "L" },
+        { value: 40, numeral: "XL" },
+        { value: 10, numeral: "X" },
+        { value: 9, numeral: "IX" },
+        { value: 5, numeral: "V" },
+        { value: 4, numeral: "IV" },
+        { value: 1, numeral: "I" },
+    ];
+
+    let result = "";
+
+    for (const { value, numeral } of roman) {
+        while (num >= value) {
+            result += numeral;
+            num -= value;
+        }
+    }
+
+    return result;
+};
