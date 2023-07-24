4294b4fc74b5c694e2847930db37eb3408323d6c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-24 22:33:51 +0600

    feat: complete 29. divide two integers

--- 29. Divide Two Integers/main.js
+++ 29. Divide Two Integers/main.js
@@ -11,3 +11,41 @@ Note: Assume we are dealing with an environment that could only store integers w
 */
 
 // solution
+
+var divide = function (dividend, divisor) {
+    const MAX_INT = 2 ** 31 - 1;
+    const MIN_INT = -(2 ** 31);
+
+    if (divisor === 0 || (dividend === MIN_INT && divisor === -1)) {
+        return MAX_INT;
+    }
+
+    if (divisor === dividend) {
+        return 1;
+    }
+
+    const sign =
+        (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0) ? -1 : 1;
+
+    let quotient = 0;
+    let absoluteDividend = Math.abs(dividend);
+    let absoluteDivisor = Math.abs(divisor);
+
+    while (absoluteDividend >= absoluteDivisor) {
+        let shift = 0;
+        let shiftedDivisor = absoluteDivisor;
+
+        while (absoluteDividend >= shiftedDivisor) {
+            shift++;
+            shiftedDivisor = absoluteDivisor << shift;
+            if (shiftedDivisor < 0) {
+                break;
+            }
+        }
+
+        quotient += 1 << (shift - 1);
+        absoluteDividend -= absoluteDivisor << (shift - 1);
+    }
+
+    return sign === -1 ? -quotient : quotient;
+};
