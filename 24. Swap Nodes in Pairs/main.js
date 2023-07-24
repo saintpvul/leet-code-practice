223654fe54b2f6de741867c096b2303916f9a9a1
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-24 21:30:30 +0600

    feat: complete 24. swap nodes in pairs

--- 24. Swap Nodes in Pairs/main.js
+++ 24. Swap Nodes in Pairs/main.js
@@ -5,3 +5,15 @@ Given a linked list, swap every two adjacent nodes and return its head. You must
 */
 
 // solution
+
+var swapPairs = function (head) {
+    if (!head || !head.next) {
+        return head;
+    }
+
+    const temp = head.next;
+    head.next = swapPairs(head.next.next);
+    temp.next = head;
+
+    return temp;
+};
