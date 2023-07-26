f190cf5bbe6f7c7acb9c6592b0c10f4adf0d6a25
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-26 23:43:10 +0600

    feat: complete 36. valid sudoku

--- 36. Valid Sudoku/main.js
+++ 36. Valid Sudoku/main.js
@@ -13,3 +13,31 @@ Only the filled cells need to be validated according to the mentioned rules.
 */
 
 // solution
+
+var isValidSudoku = function (board) {
+    const rows = Array.from({ length: 9 }, () => new Set());
+    const cols = Array.from({ length: 9 }, () => new Set());
+    const boxes = Array.from({ length: 9 }, () => new Set());
+
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            const cell = board[i][j];
+
+            if (cell != ".") {
+                const boxIdx = Math.floor(i / 3) * 3 + Math.floor(j / 3);
+                if (
+                    rows[i].has(cell) ||
+                    cols[j].has(cell) ||
+                    boxes[boxIdx].has(cell)
+                ) {
+                    return false;
+                }
+
+                rows[i].add(cell);
+                cols[j].add(cell);
+                boxes[boxIdx].add(cell);
+            }
+        }
+    }
+    return true;
+};
