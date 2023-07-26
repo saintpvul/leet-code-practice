5c2ce4db4f89b6f1022e817f46db5040094d1e89
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-26 21:49:35 +0600

    feat: complete 46. permutations

--- 46. Permutations/main.js
+++ 46. Permutations/main.js
@@ -5,3 +5,27 @@ Given an array nums of distinct integers, return all the possible permutations. 
 */
 
 // solution
+
+var permute = function (nums) {
+    let permutations = [];
+
+    const backtrack = (currentPermutation, remainingNums) => {
+        if (currentPermutation.length === nums.length) {
+            permutations.push([...currentPermutation]);
+            return;
+        }
+
+        for (let i = 0; i < remainingNums.length; i++) {
+            const num = remainingNums[i];
+            currentPermutation.push(num);
+            const newRemaining = remainingNums.filter(
+                (_, index) => index !== i
+            );
+            backtrack(currentPermutation, newRemaining);
+            currentPermutation.pop();
+        }
+    };
+
+    backtrack([], nums);
+    return permutations;
+};
