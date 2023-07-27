43cf749ca0211b93a12c92b4656b08c72a5cb218
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-27 21:25:49 +0600

    feat: complete 40. combination sum II

--- 40. Combination Sum II/main.js
+++ 40. Combination Sum II/main.js
@@ -9,3 +9,32 @@ Note: The solution set must not contain duplicate combinations.
 */
 
 // solution
+
+var combinationSum2 = function (candidates, target) {
+    const res = [];
+    candidates.sort((a, b) => a - b);
+
+    const backtrack = (start, currentCombi, currentSum) => {
+        if (currentSum === target) {
+            res.push([...currentCombi]);
+            return;
+        }
+
+        if (currentSum > target) {
+            return;
+        }
+
+        for (let i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] === candidates[i - 1]) {
+                continue;
+            }
+
+            currentCombi.push(candidates[i]);
+            backtrack(i + 1, currentCombi, currentSum + candidates[i]);
+            currentCombi.pop();
+        }
+    };
+
+    backtrack(0, [], 0);
+    return res;
+};
