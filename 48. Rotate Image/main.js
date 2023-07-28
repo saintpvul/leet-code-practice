b81cade4c250ccafa6ed6d9fee553525e31af0cb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-28 21:12:36 +0600

    feat: complete 48. rotate image

--- 48. Rotate Image/main.js
+++ 48. Rotate Image/main.js
@@ -7,3 +7,17 @@ You have to rotate the image in-place, which means you have to modify the input 
 */
 
 // solution
+
+var rotate = function (matrix) {
+    let n = matrix.length;
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+};
