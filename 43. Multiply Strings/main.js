6c648d363cc73b2d29b9b7f558e778bf7404babb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-28 19:43:36 +0600

    feat: complete 43. multiply strings

--- 43. Multiply Strings/main.js
+++ 43. Multiply Strings/main.js
@@ -7,3 +7,25 @@ Note: You must not use any built-in BigInteger library or convert the inputs to 
 */
 
 // solution
+
+var multiply = function (num1, num2) {
+    const n = num1.length;
+    const m = num2.length;
+    const res = new Array(n + m).fill(0);
+
+    for (let i = n - 1; i >= 0; i--) {
+        for (let j = m - 1; j >= 0; j--) {
+            const prod = parseInt(num1[i]) * parseInt(num2[j]);
+            const sum = prod + res[i + j + 1];
+
+            res[i + j] += Math.floor(sum / 10);
+            res[i + j + 1] = sum % 10;
+        }
+    }
+
+    while (res.length > 1 && res[0] === 0) {
+        res.shift();
+    }
+
+    return res.join("");
+};
