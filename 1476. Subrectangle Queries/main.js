ca8f9e8a9d29200ebb2b71c30cc1592f32b525cc
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-29 23:02:50 +0600

    feat: complete 1476. subrectangle queries

--- 1476. Subrectangle Queries/main.js
+++ 1476. Subrectangle Queries/main.js
@@ -12,3 +12,21 @@ Returns the current value of the coordinate (row,col) from the rectangle.
 */
 
 // solution
+
+class SubrectangleQueries {
+    constructor(rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    updateSubrectangle(row1, col1, row2, col2, newValue) {
+        for (let i = row1; i <= row2; i++) {
+            for (let j = col1; j <= col2; j++) {
+                this.rectangle[i][j] = newValue;
+            }
+        }
+    }
+
+    getValue(row, col) {
+        return this.rectangle[row][col];
+    }
+}
