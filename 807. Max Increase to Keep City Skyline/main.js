257273693212c4c89079b1e39316e8f1efa62495
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-29 23:47:30 +0600

    feat: complete 807. max increase to keep city skyline

--- 807. Max Increase to Keep City Skyline/main.js
+++ 807. Max Increase to Keep City Skyline/main.js
@@ -11,3 +11,27 @@ Return the maximum total sum that the height of the buildings can be increased b
 */
 
 // solution
+
+var maxIncreaseKeepingSkyline = function (grid) {
+    const m = grid.length;
+    const n = grid[0].length;
+    const rows = new Array(m).fill(0);
+    const cols = new Array(n).fill(0);
+
+    let res = 0;
+
+    for (let i = 0; i < m; ++i) {
+        for (let j = 0; j < n; ++j) {
+            rows[i] = Math.max(rows[i], grid[i][j]);
+            cols[j] = Math.max(cols[j], grid[i][j]);
+        }
+    }
+
+    for (let i = 0; i < m; ++i) {
+        for (let j = 0; j < n; ++j) {
+            res += Math.min(rows[i], cols[j]) - grid[i][j];
+        }
+    }
+
+    return res;
+};
