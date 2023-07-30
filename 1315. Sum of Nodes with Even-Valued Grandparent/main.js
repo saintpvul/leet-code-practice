9808e1ba75536b9d4e6b41e9a7f06b033a5bd7d9
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-30 23:58:20 +0600

    feat: complete 1315. sum of nodes with even-values grandparent

--- 1315. Sum of Nodes with Even-Valued Grandparent/main.js
+++ 1315. Sum of Nodes with Even-Valued Grandparent/main.js
@@ -7,3 +7,20 @@ A grandparent of a node is the parent of its parent if it exists.
 */
 
 // solution
+
+var sumEvenGrandparent = function (root) {
+    return dfs(root, null, null);
+};
+
+const dfs = (node, parent, grandparent) => {
+    if (node === null) {
+        return 0;
+    }
+
+    let sum = grandparent && !(grandparent.val % 2) ? node.val : 0;
+
+    sum += dfs(node.left, node, parent);
+    sum += dfs(node.right, node, parent);
+
+    return sum;
+};
