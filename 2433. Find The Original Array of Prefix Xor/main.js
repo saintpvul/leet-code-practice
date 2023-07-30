2f24f69001e58a5ad99d1195666e83cd9507cd0b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-30 23:55:10 +0600

    feat: complete 2433. find the original array of prefix xor

--- 2433. Find The Original Array of Prefix Xor/main.js
+++ 2433. Find The Original Array of Prefix Xor/main.js
@@ -10,3 +10,15 @@ It can be proven that the answer is unique.
 */
 
 // solution
+
+var findArray = function (pref) {
+    let res = [];
+    for (let i = 0; i < pref.length; i++) {
+        if (i === 0) {
+            res.push(pref[i]);
+        } else {
+            res.push(pref[i] ^ pref[i - 1]);
+        }
+    }
+    return res;
+};
