acef07d510d0645e3a168576656200810e03c85c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-31 02:10:11 +0600

    feat: complete 2265. count nodes equal to average of subtree

--- 2265. Count Nodes Equal to Average of Subtree/main.js
+++ 2265. Count Nodes Equal to Average of Subtree/main.js
@@ -10,3 +10,29 @@ A subtree of root is a tree consisting of root and all of its descendants.
 */
 
 // solution
+
+var averageOfSubtree = function (root) {
+    let count = 0;
+
+    const traverse = (node) => {
+        if (node === null) {
+            return [0, 0];
+        }
+
+        const [leftSum, leftCount] = traverse(node.left);
+        const [rightSum, rightCount] = traverse(node.right);
+
+        const subtreeSum = leftSum + rightSum + node.val;
+        const subtreeCount = leftCount + rightCount + 1;
+
+        if (Math.floor(subtreeSum / subtreeCount) === node.val) {
+            count++;
+        }
+
+        return [subtreeSum, subtreeCount];
+    };
+
+    traverse(root);
+
+    return count;
+};
