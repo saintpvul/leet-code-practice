cebeb64d0a957feabc4180de2b295741bdd7164c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-07-31 01:46:05 +0600

    feat: complete 654. maximum binary tree

--- 654. Maximum Binary Tree/main.js
+++ 654. Maximum Binary Tree/main.js
@@ -10,3 +10,17 @@ Return the maximum binary tree built from nums.
 */
 
 // solution
+
+var constructMaximumBinaryTree = function (nums) {
+    if (nums.length === 0) return null;
+
+    const max = Math.max(...nums);
+    const maxIdx = nums.indexOf(max);
+
+    const root = new TreeNode(max);
+
+    root.left = constructMaximumBinaryTree(nums.slice(0, maxIdx));
+    root.right = constructMaximumBinaryTree(nums.slice(maxIdx + 1));
+
+    return root;
+};
