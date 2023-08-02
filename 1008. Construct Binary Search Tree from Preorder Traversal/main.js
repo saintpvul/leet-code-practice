90044090c32163b65e9bf8ab181fee5c4113580e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-03 02:20:49 +0600

    feat: complete 1008. construct binary search tree from preorder traversal

--- 1008. Construct Binary Search Tree from Preorder Traversal/main.js
+++ 1008. Construct Binary Search Tree from Preorder Traversal/main.js
@@ -11,3 +11,22 @@ A preorder traversal of a binary tree displays the value of the node first, then
 */
 
 // solution
+
+var bstFromPreorder = function(preorder) {
+    let idx = 0
+
+    const bstConstructor = (min, max) => {
+        if(idx >= preorder.length || preorder[idx] < min || preorder[idx] > max){
+            return null
+        }
+
+        const node = new TreeNode(preorder[idx])
+        idx++
+        node.left = bstConstructor(min, node.val)
+        node.right = bstConstructor(node.val, max)
+
+        return node
+    }
+
+    return bstConstructor (-Infinity, Infinity)
+};
