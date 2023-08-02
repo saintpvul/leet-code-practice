d54241027e8de559d6c430eb32cad8f28c5c0468
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-02 17:08:23 +0600

    feat: complete 894. all possible full binary trees

--- 894. All Possible Full Binary Trees/main.js
+++ 894. All Possible Full Binary Trees/main.js
@@ -9,3 +9,27 @@ A full binary tree is a binary tree where each node has exactly 0 or 2 children.
 */
 
 // solution
+
+var allPossibleFBT = function(n) {
+     if (n === 1) {
+        return [new TreeNode(0)];
+    }
+
+    const result = [];
+ 
+    for (let i = 1; i < n; i += 2) {
+        const leftTrees = allPossibleFBT(i);
+        const rightTrees = allPossibleFBT(n - i - 1);
+ 
+        for (const leftTree of leftTrees) {
+            for (const rightTree of rightTrees) {
+                const root = new TreeNode(0);
+                root.left = leftTree;
+                root.right = rightTree;
+                result.push(root);
+            }
+        }
+    }
+
+    return result;
+};
