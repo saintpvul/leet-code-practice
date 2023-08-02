d2832d2919bd6b812165491ffae6c813674b27d2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-02 15:08:24 +0600

    feat: complete 1637. widest vertical area between two points containing no points

--- 1637. Widest Vertical Area Between Two Points Containing No Points/main.js
+++ 1637. Widest Vertical Area Between Two Points Containing No Points/main.js
@@ -9,3 +9,16 @@ Note that points on the edge of a vertical area are not considered included in t
 */
 
 // solution
+
+
+var maxWidthOfVerticalArea = function(points) {
+    points.sort((a,b)=> a[0] - b[0])
+    let maxWidth = 0
+
+    for(let i = 0; i < points.length - 1; i++){
+        const diff = points[i + 1][0] - points[i][0]
+        maxWidth = Math.max(maxWidth, diff)
+    }
+
+    return maxWidth
+};
