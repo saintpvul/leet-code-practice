89299218c2719922feefb20de0e4f39ab560df6b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-02 16:37:56 +0600

    feat: complete 1329. sort the matrix diagonally

--- 1329. Sort the Matrix Diagonally/main.js
+++ 1329. Sort the Matrix Diagonally/main.js
@@ -7,3 +7,34 @@ Given an m x n matrix mat of integers, sort each matrix diagonal in ascending or
 */
 
 // solution
+
+
+var diagonalSort = function(mat) {
+    const m = mat.length;
+    const n = mat[0].length;
+ 
+    const sortDiagonal = (row, col) => {
+        const diagonal = [];
+        while (row < m && col < n) {
+            diagonal.push(mat[row][col]);
+            row++;
+            col++;
+        }
+        diagonal.sort((a, b) => a - b);
+        while (row > 0 && col > 0) {
+            row--;
+            col--;
+            mat[row][col] = diagonal.pop();
+        }
+    };
+ 
+    for (let row = 0; row < m; row++) {
+        sortDiagonal(row, 0);
+    }
+ 
+    for (let col = 1; col < n; col++) {
+        sortDiagonal(0, col);
+    }
+
+    return mat;
+};
