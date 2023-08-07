8b9d58b02923afd1050b30578222ca9de83a490b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-07 23:35:19 +0600

    feat: complete 763. partition labels

--- 763. Partition Labels/main.js
+++ 763. Partition Labels/main.js
@@ -9,3 +9,33 @@ Return a list of integers representing the size of these parts.
 */
 
 // solution
+
+var partitionLabels = function (s) {
+    let lettersOccur = new Map();
+    for (let i = 0; i < s.length; i++) {
+        const curr = s[i];
+        if (lettersOccur.has(curr)) {
+            lettersOccur.get(curr).push(i);
+        } else {
+            lettersOccur.set(curr, [i]);
+        }
+    }
+
+    let partitions = [];
+    let start = 0;
+    let end = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const curr = s[i];
+        const currLastPosition =
+            lettersOccur.get(curr)[lettersOccur.get(curr).length - 1];
+        end = Math.max(end, currLastPosition);
+
+        if (i === end) {
+            partitions.push(end - start + 1);
+            start = end + 1;
+        }
+    }
+
+    return partitions;
+};
