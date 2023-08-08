ac4c94d73cd8338065469d139fddc90bd83e87f2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-08 22:14:21 +0600

    feat: complete 2405. optimal partition of string

--- 2405. Optimal Partition of String/main.js
+++ 2405. Optimal Partition of String/main.js
@@ -9,3 +9,23 @@ Note that each character should belong to exactly one substring in a partition.
 */
 
 // solution
+
+var partitionString = function (s) {
+    let count = 0;
+    let i = 0;
+
+    while (i < s.length) {
+        const part = new Set();
+        let j = i;
+
+        while (j < s.length && !part.has(s[j])) {
+            part.add(s[j]);
+            j++;
+        }
+
+        count++;
+        i = j;
+    }
+
+    return count;
+};
