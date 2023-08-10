16ef4e6f6f7b0c2babaafe6dea52d66ff6635c3e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-10 19:30:44 +0600

    feat: complete 1829. maximum xor for each query

--- 1829. Maximum XOR for Each Query/main.js
+++ 1829. Maximum XOR for Each Query/main.js
@@ -9,3 +9,28 @@ Return an array answer, where answer[i] is the answer to the ith query.
 */
 
 // solution
+
+var getMaximumXor = function (nums, maximumBit) {
+    const answer = Array.from({ length: nums.length });
+    let xorTotal = 0;
+
+    for (const num of nums) {
+        xorTotal ^= num;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[nums.length - i - 1];
+        let k = 0;
+
+        for (let j = maximumBit - 1; j >= 0; j--) {
+            if (((xorTotal >> j) & 1) == 0) {
+                k |= 1 << j;
+            }
+        }
+
+        answer[i] = k;
+        xorTotal ^= currentNum;
+    }
+
+    return answer;
+};
