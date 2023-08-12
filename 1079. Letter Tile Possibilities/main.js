0c882f8650dd8eca64a579239d1e686741fd8a4e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-12 17:16:59 +0600

    feat: complete 1079. letter tile possibilities

--- 1079. Letter Tile Possibilities/main.js
+++ 1079. Letter Tile Possibilities/main.js
@@ -7,3 +7,25 @@ Return the number of possible non-empty sequences of letters you can make using 
 */
 
 // solution
+
+var numTilePossibilities = function (tiles) {
+    const count = Array.from({ length: 26 }, () => 0);
+    for (const char of tiles) {
+        count[char.charCodeAt() - "A".charCodeAt(0)]++;
+    }
+    const dfs = (cnt) => {
+        let res = 0;
+
+        for (let i = 0; i < 26; i++) {
+            if (cnt[i] > 0) {
+                res++;
+                cnt[i]--;
+                res += dfs(cnt);
+                cnt[i]++;
+            }
+        }
+        return res;
+    };
+
+    return dfs(count);
+};
