5d5165304fece22b34b8a031037ccff274787541
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-12 17:50:46 +0600

    feat: complete 1026. maximum difference between node and ancestor

--- 1026. Maximum Difference Between Node and Ancestor/main.js
+++ 1026. Maximum Difference Between Node and Ancestor/main.js
@@ -7,3 +7,20 @@ A node a is an ancestor of b if either: any child of a is equal to b or any chil
 */
 
 // solution
+
+var maxAncestorDiff = function (root) {
+    let res = 0;
+
+    const dfs = (node, min, max) => {
+        if (!node) return;
+        res = Math.max(res, Math.abs(node.val - min), Math.abs(node.val - max));
+
+        min = Math.min(min, node.val);
+        max = Math.max(max, node.val);
+        dfs(node.left, min, max);
+        dfs(node.right, min, max);
+    };
+    dfs(root, root.val, root.val);
+
+    return res;
+};
