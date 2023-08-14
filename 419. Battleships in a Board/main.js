fd9c559daeaa7ef4cec4a62d0ad4bd45c9269597
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-14 21:41:19 +0600

    feat: complete 419. battleships in a board

--- 419. Battleships in a Board/main.js
+++ 419. Battleships in a Board/main.js
@@ -7,3 +7,25 @@ Battleships can only be placed horizontally or vertically on board. In other wor
 */
 
 // solution
+
+var countBattleships = function (board) {
+    const m = board.length;
+    const n = board[0].length;
+    let res = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] === ".") {
+                continue;
+            }
+            if (i > 0 && board[i - 1][j] === "X") {
+                continue;
+            }
+            if (j > 0 && board[i][j - 1] === "X") {
+                continue;
+            }
+            res++;
+        }
+    }
+    return res;
+};
