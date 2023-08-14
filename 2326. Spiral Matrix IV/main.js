f026cff189dc174e47bf5416eb38b7c04f0a837f
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-14 22:40:59 +0600

    feat: complete 2326. spiral matrix iv

--- 2326. Spiral Matrix IV/main.js
+++ 2326. Spiral Matrix IV/main.js
@@ -11,3 +11,40 @@ Return the generated matrix.
 */
 
 // solution
+
+// beats 100% of users
+var spiralMatrix = function (m, n, head) {
+    const mat = Array.from({ length: m }, () => Array(n).fill(-1));
+
+    let i = 0,
+        j = 0,
+        k = 0;
+    const dir = [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+    ];
+
+    while (true) {
+        mat[i][j] = head.val;
+        head = head.next;
+
+        if (!head) {
+            break;
+        }
+
+        while (true) {
+            const [x, y] = [i + dir[k][0], j + dir[k][1]];
+
+            if (x < 0 || y < 0 || x >= m || y >= n || mat[x][y] !== -1) {
+                k = (k + 1) % 4;
+            } else {
+                i = x;
+                j = y;
+                break;
+            }
+        }
+    }
+    return mat;
+};
