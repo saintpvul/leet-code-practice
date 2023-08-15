134c0859e7f72eaad321fd47c2436da51033a5e8
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-15 19:09:10 +0600

    feat: start 2428. maximum sum of an hourglass

---
+++ 2428. Maximum Sum of an Hourglass/main.js
@@ -0,0 +1,14 @@
+/*
+
+You are given an m x n integer matrix grid.
+
+We define an hourglass as a part of the matrix with the following form:
+
+
+Return the maximum sum of the elements of an hourglass.
+
+Note that an hourglass cannot be rotated and must be entirely contained within the matrix.
+
+*/
+
+// solution
