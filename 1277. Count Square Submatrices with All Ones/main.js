4f76bbf83b9c5cc4c6436ad0c0c63f5d6e14d9cb
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-15 19:00:04 +0600

    feat: complete 1277. count square submatrices with all ones

--- 1277. Count Square Submatrices with All Ones/main.js
+++ 1277. Count Square Submatrices with All Ones/main.js
@@ -5,3 +5,26 @@ Given a m * n matrix of ones and zeros, return how many square submatrices have 
 */
 
 // solution
+
+var countSquares = function (matrix) {
+    const m = matrix.length;
+    const n = matrix[0].length;
+    let count = 0;
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 1) {
+                if (i > 0 && j > 0) {
+                    matrix[i][j] =
+                        Math.min(
+                            matrix[i - 1][j - 1],
+                            matrix[i - 1][j],
+                            matrix[i][j - 1]
+                        ) + 1;
+                }
+                count += matrix[i][j];
+            }
+        }
+    }
+    return count;
+};
