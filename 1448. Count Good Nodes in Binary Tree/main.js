10ca581a4744348d4cf5a2d5d2613010c791450e
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-17 20:22:28 +0600

    feat: complete 1448. count good nodes in binary tree

--- 1448. Count Good Nodes in Binary Tree/main.js
+++ 1448. Count Good Nodes in Binary Tree/main.js
@@ -7,3 +7,27 @@ Return the number of good nodes in the binary tree.
 */
 
 // solution
+
+var goodNodes = function (root) {
+    let count = 0;
+
+    if (!root) return 0;
+
+    const dfs = (node, prevMax) => {
+        if (!node) {
+            return;
+        }
+
+        if (node.val >= prevMax) {
+            count++;
+        }
+
+        prevMax = Math.max(prevMax, node.val);
+
+        dfs(node.left, prevMax);
+        dfs(node.right, prevMax);
+    };
+
+    dfs(root, root.val);
+    return count;
+};
