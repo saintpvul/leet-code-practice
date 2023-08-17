0b47f331a75914fdb28ce9444e23cf76d23f2d98
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-17 20:53:41 +0600

    feat: complete 2023. number of pairs of strings with concatenation equal to target

--- 2023. Number of Pairs of Strings With Concatenation Equal to Target/main.js
+++ 2023. Number of Pairs of Strings With Concatenation Equal to Target/main.js
@@ -5,3 +5,23 @@ Given an array of digit strings nums and a digit string target, return the numbe
 */
 
 // solution
+
+var numOfPairs = function (nums, target) {
+    let res = 0;
+    const count = new Map();
+
+    for (const num of nums) {
+        const k = num.length;
+
+        if (target.slice(0, k) === num) {
+            res += count.get(target.slice(k)) || 0;
+        }
+        if (target.slice(-k) === num) {
+            res += count.get(target.slice(0, -k)) || 0;
+        }
+
+        count.set(num, (count.get(num) || 0) + 1);
+    }
+
+    return res;
+};
