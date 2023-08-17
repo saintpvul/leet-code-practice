47efaaded4814677a091f653bef3a50e0b2d3c8c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-17 20:46:06 +0600

    feat: complete 1286. iterator for combination

--- 1286. Iterator for Combination/main.js
+++ 1286. Iterator for Combination/main.js
@@ -9,3 +9,37 @@ hasNext() Returns true if and only if there exists a next combination.
 */
 
 // solution
+
+class CombinationIterator {
+    constructor(characters, combinationLength) {
+        this.combinations = [];
+        this.generateCombinations(characters, combinationLength, 0, "");
+        this.currentIndex = 0;
+    }
+
+    generateCombinations(characters, length, startIndex, currentCombination) {
+        if (length === 0) {
+            this.combinations.push(currentCombination);
+            return;
+        }
+
+        for (let i = startIndex; i <= characters.length - length; i++) {
+            this.generateCombinations(
+                characters,
+                length - 1,
+                i + 1,
+                currentCombination + characters[i]
+            );
+        }
+    }
+
+    next() {
+        const result = this.combinations[this.currentIndex];
+        this.currentIndex++;
+        return result;
+    }
+
+    hasNext() {
+        return this.currentIndex < this.combinations.length;
+    }
+}
