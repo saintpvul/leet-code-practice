3e03062fd62ff4a48e9f8e8dabf4a2a51e07aae2
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-22 00:00:13 +0600

    feat: complete 1806. minimum number of operations to reinitialize a permutation

--- 1806. Minimum Number of Operations to Reinitialize a Permutation/main.js
+++ 1806. Minimum Number of Operations to Reinitialize a Permutation/main.js
@@ -13,3 +13,32 @@ Return the minimum non-zero number of operations you need to perform on perm to 
 */
 
 // solution
+
+var reinitializePermutation = function (n) {
+    let perm = new Array(n);
+    for (let i = 0; i < n; i++) {
+        perm[i] = i;
+    }
+
+    let arr = new Array(n);
+    let ops = 0;
+
+    while (true) {
+        for (let i = 0; i < n; i++) {
+            if (i % 2 === 0) {
+                arr[i] = perm[i / 2];
+            } else {
+                arr[i] = perm[n / 2 + Math.floor((i - 1) / 2)];
+            }
+        }
+        ops++;
+
+        if (arr.every((value, index) => value === index)) {
+            break;
+        }
+
+        [perm, arr] = [arr, perm];
+    }
+
+    return ops;
+};
