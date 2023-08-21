e77c1191858545575e1bde5701c2b539ecb55c68
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-21 23:55:36 +0600

    feat: complete 814. binary tree pruning

--- 814. Binary Tree Pruning/main.js
+++ 814. Binary Tree Pruning/main.js
@@ -7,3 +7,28 @@ A subtree of a node node is node plus every node that is a descendant of node.
 */
 
 // solution
+
+var pruneTree = function (root) {
+    if (!root) return null;
+
+    const contains = (node) => {
+        if (!node) return false;
+
+        const left = contains(node.left);
+        const right = contains(node.right);
+
+        if (!left) {
+            node.left = null;
+        }
+        if (!right) {
+            node.right = null;
+        }
+
+        return node.val === 1 || left || right;
+    };
+    if (!contains(root)) {
+        return null;
+    }
+
+    return root;
+};
