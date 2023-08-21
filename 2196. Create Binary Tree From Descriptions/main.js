374401d6f236a8caaf3398f2dc80fa6a9f39310a
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-21 23:58:48 +0600

    feat: complete 2196. create binary tree from descriptions

--- 2196. Create Binary Tree From Descriptions/main.js
+++ 2196. Create Binary Tree From Descriptions/main.js
@@ -11,3 +11,26 @@ The test cases will be generated such that the binary tree is valid.
 */
 
 // solution
+
+var createBinaryTree = function (descriptions) {
+    const children = new Set();
+    const valToNode = {};
+
+    for (const [p, c, isLeft] of descriptions) {
+        const parent = valToNode[p] || new TreeNode(p);
+        const child = valToNode[c] || new TreeNode(c);
+        valToNode[p] = parent;
+        valToNode[c] = child;
+        if (isLeft) {
+            parent.left = child;
+        } else {
+            parent.right = child;
+        }
+        children.add(c);
+    }
+
+    const root = [...Object.keys(valToNode)].find(
+        (node) => !children.has(parseInt(node))
+    );
+    return valToNode[root];
+};
