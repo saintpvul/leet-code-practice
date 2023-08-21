6bd27b0014eb37a1c6f551f46663c6687fc28218
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-22 01:44:45 +0600

    feat: complete 1043. partition array for maximum sum

--- 1043. Partition Array for Maximum Sum/main.js
+++ 1043. Partition Array for Maximum Sum/main.js
@@ -7,3 +7,17 @@ Return the largest sum of the given array after partitioning. Test cases are gen
 */
 
 // solution
+
+var maxSumAfterPartitioning = function (arr, k) {
+    const n = arr.length;
+    const dp = Array.from({ length: n }, () => 0);
+
+    for (let i = 0; i < n; i++) {
+        let max = 0;
+        for (let j = 1; j <= k && i - j + 1 >= 0; j++) {
+            max = Math.max(max, arr[i - j + 1]);
+            dp[i] = Math.max(dp[i], (i >= j ? dp[i - j] : 0) + max * j);
+        }
+    }
+    return dp[n - 1];
+};
