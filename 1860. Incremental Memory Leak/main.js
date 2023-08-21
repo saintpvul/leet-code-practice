da96de2bc9c58add943d28ed1b8fd3aff0c4328c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-22 02:03:15 +0600

    feat: complete 1860. incremental memory leak

--- 1860. Incremental Memory Leak/main.js
+++ 1860. Incremental Memory Leak/main.js
@@ -9,3 +9,16 @@ Return an array containing [crashTime, memory1crash, memory2crash], where crashT
 */
 
 // solution
+
+var memLeak = function (memory1, memory2) {
+    let time = 1;
+    while (memory1 >= time || memory2 >= time) {
+        if (memory1 >= memory2) {
+            memory1 -= time;
+        } else {
+            memory2 -= time;
+        }
+        time++;
+    }
+    return [time, memory1, memory2];
+};
