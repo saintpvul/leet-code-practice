6d56f44a175b34edec09dac223c4cb09f343ceb5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-23 20:31:43 +0600

    feat: start 1222. queens that can attack the king

---
+++ 1222. Queens That Can Attack the King/main.js
@@ -0,0 +1,11 @@
+/*
+
+On a 0-indexed 8 x 8 chessboard, there can be multiple black queens ad one white king.
+
+You are given a 2D integer array queens where queens[i] = [xQueeni, yQueeni] represents the position of the ith black queen on the chessboard. You are also given an integer array king of length 2 where king = [xKing, yKing] represents the position of the white king.
+
+Return the coordinates of the black queens that can directly attack the king. You may return the answer in any order.
+
+*/
+
+// solution
