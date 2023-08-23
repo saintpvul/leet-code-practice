d64f6c2c2701ce54d05af9eead7698cb3789e32c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-24 00:09:08 +0600

    feat: complete 1222. queens that can attack the king

--- 1222. Queens That Can Attack the King/main.js
+++ 1222. Queens That Can Attack the King/main.js
@@ -9,3 +9,33 @@ Return the coordinates of the black queens that can directly attack the king. Yo
 */
 
 // solution
+
+var queensAttacktheKing = function (queens, king) {
+    const queensSet = new Set(queens.map((q) => q[0] * 8 + q[1]));
+    const dirs = [
+        [-1, -1],
+        [-1, 0],
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+    ];
+
+    const res = [];
+    for (const [dx, dy] of dirs) {
+        let x = king[0] + dx;
+        let y = king[1] + dy;
+
+        while (x >= 0 && x < 8 && y >= 0 && y < 8) {
+            if (queensSet.has(x * 8 + y)) {
+                res.push([x, y]);
+                break;
+            }
+            x += dx;
+            y += dy;
+        }
+    }
+    return res;
+};
