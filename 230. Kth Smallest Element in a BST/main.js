b614d4f5dd327e3bc38bb9cb346dd4646350d1ba
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-25 01:17:09 +0600

    feat: complete 230. kth smallest element in a bst

--- 230. Kth Smallest Element in a BST/main.js
+++ 230. Kth Smallest Element in a BST/main.js
@@ -5,3 +5,25 @@ Given the root of a binary search tree, and an integer k, return the kth smalles
 */
 
 // solution
+
+var kthSmallest = function (root, k) {
+    let result = null;
+    let count = 0;
+
+    const traverse = (node) => {
+        if (!node) return;
+
+        traverse(node.left);
+
+        count++;
+        if (count === k) {
+            result = node.val;
+            return;
+        }
+
+        traverse(node.right);
+    };
+
+    traverse(root);
+    return result;
+};
