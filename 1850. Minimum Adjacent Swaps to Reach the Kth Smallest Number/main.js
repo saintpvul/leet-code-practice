c6326bb1d3117a7e5c9de539face5be0a24c2330
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-25 01:48:54 +0600

    feat: complete 1850. minimum adjacent swaps to reach the kth smallest number

--- 1850. Minimum Adjacent Swaps to Reach the Kth Smallest Number/main.js
+++ 1850. Minimum Adjacent Swaps to Reach the Kth Smallest Number/main.js
@@ -16,3 +16,63 @@ The tests are generated in such a way that kth smallest wonderful integer exists
 */
 
 // solution
+
+var getMinSwaps = function (num, k) {
+    let copy = num;
+
+    while (k > 0) {
+        copy = nextPermutation(copy);
+        k--;
+    }
+
+    let res = 0;
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] === copy[i]) continue;
+
+        let j = i + 1;
+        for (; j < copy.length; j++) {
+            if (num[i] === copy[j]) {
+                break;
+            }
+        }
+
+        res += j - i;
+        copy =
+            copy.substring(0, i) +
+            copy[j] +
+            copy.substring(i, j) +
+            copy.substring(j + 1);
+    }
+
+    return res;
+};
+
+var nextPermutation = function (str) {
+    const arr = str.split("");
+    let i = arr.length - 2;
+
+    while (i >= 0 && arr[i] >= arr[i + 1]) {
+        i--;
+    }
+
+    if (i >= 0) {
+        let j = arr.length - 1;
+        while (arr[j] <= arr[i]) {
+            j--;
+        }
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+
+    reverse(arr, i + 1);
+    return arr.join("");
+};
+
+var reverse = function (arr, start) {
+    let left = start;
+    let right = arr.length - 1;
+    while (left < right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--;
+    }
+};
