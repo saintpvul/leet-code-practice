848e286e2e15ef7522b511c74fed4df251a46f92
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-25 01:50:38 +0600

    feat: complete 1638. count substrings that differ by one character

--- 1638. Count Substrings That Differ by One Character/main.js
+++ 1638. Count Substrings That Differ by One Character/main.js
@@ -11,3 +11,27 @@ A substring is a contiguous sequence of characters within a string.
 */
 
 // solution
+
+.
+var countSubstrings = function(s, t) {
+    let count = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        for (let j = 0; j < t.length; j++) {
+            let diffCount = 0;
+            for (let k = 0; i + k < s.length && j + k < t.length; k++) {
+                if (s[i + k] !== t[j + k]) {
+                    diffCount++;
+                }
+                if (diffCount === 1) {
+                    count++;
+                }
+                if (diffCount > 1) {
+                    break;
+                }
+            }
+        }
+    }
+
+    return count;
+};
