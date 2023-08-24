15871af09f58b2403b0dee8fd3aa7c4da369bcea
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-25 01:42:23 +0600

    feat: complete 889. construct binary tree from preorder and postorder traversal

--- 889. Construct Binary Tree from Preorder and Postorder Traversal/main.js
+++ 889. Construct Binary Tree from Preorder and Postorder Traversal/main.js
@@ -7,3 +7,41 @@ If there exist multiple answers, you can return any of them.
 */
 
 // solution
+
+var constructFromPrePost = function (preorder, postorder) {
+    const postToIndex = {};
+    for (let i = 0; i < postorder.length; i++) {
+        postToIndex[postorder[i]] = i;
+    }
+
+    const buildTree = (preStart, preEnd, postStart, postEnd) => {
+        if (preStart > preEnd) {
+            return null;
+        }
+        if (preStart === preEnd) {
+            return new TreeNode(preorder[preStart]);
+        }
+
+        const rootVal = preorder[preStart];
+        const leftRootVal = preorder[preStart + 1];
+        const leftRootPostIndex = postToIndex[leftRootVal];
+        const leftSize = leftRootPostIndex - postStart + 1;
+
+        const root = new TreeNode(rootVal);
+        root.left = buildTree(
+            preStart + 1,
+            preStart + leftSize,
+            postStart,
+            leftRootPostIndex
+        );
+        root.right = buildTree(
+            preStart + leftSize + 1,
+            preEnd,
+            leftRootPostIndex + 1,
+            postEnd - 1
+        );
+        return root;
+    };
+
+    return buildTree(0, preorder.length - 1, 0, postorder.length - 1);
+};
