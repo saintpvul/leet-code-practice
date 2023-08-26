1f2db1726050d8c92ca5a88f1421c9c57d25b046
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-26 23:37:55 +0600

    feat: complete 429. n-ary tree level order traversal

--- 429. N-ary Tree Level Order Traversal/main.js
+++ 429. N-ary Tree Level Order Traversal/main.js
@@ -7,3 +7,27 @@ Nary-Tree input serialization is represented in their level order traversal, eac
 */
 
 // solution
+
+var levelOrder = function (root) {
+    if (!root) return [];
+
+    const res = [];
+    const que = [root];
+
+    while (que.length > 0) {
+        const lvl = [];
+        const lvlSize = que.length;
+
+        for (let i = 0; i < lvlSize; i++) {
+            const node = que.shift();
+            lvl.push(node.val);
+
+            for (const child of node.children) {
+                que.push(child);
+            }
+        }
+        res.push(lvl);
+    }
+
+    return res;
+};
