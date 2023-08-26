d18f8e090ac70d5425cc55b3623c27487b2ba11d
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-26 23:23:05 +0600

    feat: complete 451. sort characters by frequency

--- 451. Sort Characters By Frequency/main.js
+++ 451. Sort Characters By Frequency/main.js
@@ -7,3 +7,22 @@ Return the sorted string. If there are multiple answers, return any of them.
 */
 
 // solution
+
+var frequencySort = function (s) {
+    const frequency = new Map();
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+        if (frequency.has(char)) {
+            frequency.set(char, frequency.get(char) + 1);
+        } else {
+            frequency.set(char, 1);
+        }
+    }
+    const sorted = [...frequency.entries()].sort((a, b) => b[1] - a[1]);
+    let res = "";
+    for (const [key, val] of sorted) {
+        res += key.repeat(val);
+    }
+    return res;
+};
