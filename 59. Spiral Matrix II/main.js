2cb11bc66aa320fed5ce5179213b76c7594b0bc3
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-27 14:50:19 +0600

    feat: complete 59. spiral matrix ii

--- 59. Spiral Matrix II/main.js
+++ 59. Spiral Matrix II/main.js
@@ -5,3 +5,46 @@ Given a positive integer n, generate an n x n matrix filled with elements from 1
 */
 
 // solution
+
+const matrix = Array.from({ length: n }, () => new Array(n).fill(0));
+let num = 1;
+let row = 0,
+    col = 0;
+let dir = 0;
+
+while (num <= n * n) {
+    matrix[row][col] = num;
+    num++;
+
+    if (dir === 0) {
+        if (col + 1 < n && matrix[row][col + 1] === 0) {
+            col++;
+        } else {
+            dir = 1;
+            row++;
+        }
+    } else if (dir === 1) {
+        if (row + 1 < n && matrix[row + 1][col] === 0) {
+            row++;
+        } else {
+            dir = 2;
+            col--;
+        }
+    } else if (dir === 2) {
+        if (col - 1 >= 0 && matrix[row][col - 1] === 0) {
+            col--;
+        } else {
+            dir = 3;
+            row--;
+        }
+    } else if (dir === 3) {
+        if (row - 1 >= 0 && matrix[row - 1][col] === 0) {
+            row--;
+        } else {
+            dir = 0;
+            col++;
+        }
+    }
+}
+
+return matrix;
