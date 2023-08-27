3d9ed0aa2e961077f248f4ee454e49a20334c629
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-27 15:02:42 +0600

    feat: complete 877. stone game

--- 877. Stone Game/main.js
+++ 877. Stone Game/main.js
@@ -11,3 +11,30 @@ Assuming Alice and Bob play optimally, return true if Alice wins the game, or fa
 */
 
 // solution
+
+// too slow
+// var stoneGame = function (piles) {
+//     const n = piles.length;
+//     const dp = new Array(n).fill(null).map(() => new Array(n).fill(0));
+
+//     for (let i = 0; i < n; i++) {
+//         dp[i][i] = piles[i];
+//     }
+
+//     for (let l = 2; l <= n; l++) {
+//         for (let i = 0; i <= n - l; i++) {
+//             const j = i + l - 1;
+//             dp[i][j] = Math.max(
+//                 piles[i] - dp[i + 1][j],
+//                 piles[j] - dp[i][j - 1]
+//             );
+//         }
+//     }
+//     return dp[0][n - 1] > 0;
+// };
+
+// the problem in all cases requires true as result
+
+var stoneGame = function (piles) {
+    return true;
+};
