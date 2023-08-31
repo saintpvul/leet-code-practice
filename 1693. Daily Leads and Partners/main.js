499230170f7e8ecb2240d99c4dcdb9f77627a3b6
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-08-31 22:40:24 +0600

    feat: start 1693. daily leads and partners

---
+++ 1693. Daily Leads and Partners/main.js
@@ -0,0 +1,26 @@
+/*
+
+Table: DailySales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| date_id     | date    |
+| make_name   | varchar |
+| lead_id     | int     |
+| partner_id  | int     |
++-------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+This table contains the date and the name of the product sold and the IDs of the lead and partner it was sold to.
+The name consists of only lowercase English letters.
+ 
+
+For each date_id and make_name, find the number of distinct lead_id's and distinct partner_id's.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+*/
+
+// solution
