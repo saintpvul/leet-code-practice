f499e892e06a738c7db56d5a45366fbc2dec2276
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-09-01 23:24:20 +0600

    feat: complete 1238. curcular permutation in binary representation

--- 1238. Circular Permutation in Binary Representation/main.js
+++ 1238. Circular Permutation in Binary Representation/main.js
@@ -9,3 +9,45 @@ p[0] and p[2^n -1] must also differ by only one bit in their binary representati
 */
 
 // solution
+
+var circularPermutation = function (n, start) {
+    const result = [start];
+    const visited = new Set([start]);
+
+    function differByOneBit(a, b) {
+        const xor = a ^ b;
+        return (xor & (xor - 1)) === 0;
+    }
+
+    while (result.length < 2 ** n) {
+        let found = false;
+
+        for (let i = 0; i < n; i++) {
+            const neighbor = result[result.length - 1] ^ (1 << i);
+
+            if (
+                !visited.has(neighbor) &&
+                differByOneBit(result[result.length - 1], neighbor)
+            ) {
+                result.push(neighbor);
+                visited.add(neighbor);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            for (let i = 0; i < n; i++) {
+                const neighbor = result[result.length - 1] ^ (1 << i);
+
+                if (!visited.has(neighbor)) {
+                    result.push(neighbor);
+                    visited.add(neighbor);
+                    break;
+                }
+            }
+        }
+    }
+
+    return result;
+};
