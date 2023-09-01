2fac0f0ef243e540db962ce77dbf4a0ddf52ce4b
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-09-01 23:31:42 +0600

    feat: complete 2824. count pairs whoose sum is less than target

--- 2824. Count Pairs Whose Sum is Less than Target/main.js
+++ 2824. Count Pairs Whose Sum is Less than Target/main.js
@@ -5,3 +5,15 @@ Given a 0-indexed integer array nums of length n and an integer target, return t
 */
 
 // solution
+
+var countPairs = function (nums, target) {
+    let pairs = 0;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                pairs++;
+            }
+        }
+    }
+    return pairs;
+};
