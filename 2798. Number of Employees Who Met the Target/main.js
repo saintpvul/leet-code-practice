84a107d063698950cccec42dd94c3b953fa8ae4c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-09-01 23:27:39 +0600

    feat: complete 2798. number of employees who met the target

--- 2798. Number of Employees Who Met the Target/main.js
+++ 2798. Number of Employees Who Met the Target/main.js
@@ -11,3 +11,11 @@ Return the integer denoting the number of employees who worked at least target h
 */
 
 // solution
+
+var numberOfEmployeesWhoMetTarget = function (hours, target) {
+    let count = 0;
+    for (let i = 0; i < hours.length; i++) {
+        if (hours[i] >= target) count++;
+    }
+    return count;
+};
