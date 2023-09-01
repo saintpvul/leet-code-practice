bdcbd3d1d301369a7d562fd8569e7ed3ad78aef5
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-09-01 23:28:25 +0600

    feat: start 2824. count pairs whoose sum is less than target

---
+++ 2824. Count Pairs Whose Sum is Less than Target/main.js
@@ -0,0 +1,7 @@
+/*
+
+Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+
+*/
+
+// solution
