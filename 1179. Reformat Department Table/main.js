9286f1fc523a63382fffcf5fbcc02ae4c99fb15c
saintpvul <103897555+saintpvul@users.noreply.github.com>
2023-09-03 23:28:44 +0600

    feat: start 1179. reformat department table

---
+++ 1179. Reformat Department Table/main.js
@@ -0,0 +1,23 @@
+/*
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| revenue     | int     |
+| month       | varchar |
++-------------+---------+
+In SQL,(id, month) is the primary key of this table.
+The table has information about the revenue of each department per month.
+The month has values in ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"].
+ 
+
+Reformat the table such that there is a department id column and a revenue column for each month.
+
+Return the result table in any order.
+
+*/
+
+// solution
